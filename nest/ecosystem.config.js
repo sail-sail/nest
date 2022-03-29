f2d3fc3c523bcb03881f00746e64ac6f32639bf2
Sail <151263555@qq.com>
2022-03-29 09:13:42 +0800

    config

--- nest/ecosystem.config.js
+++ nest/ecosystem.config.js
@@ -2,7 +2,10 @@ module.exports = {
   apps: [{
     name: "nest",
     script: "index.js",
-    args: "--enable-source-maps --max_old_space_size=2048",
+    args: [
+      "--enable-source-maps",
+      "--max_old_space_size=2048",
+    ],
     instances: 1,
     autorestart: true,
     watch: false,
