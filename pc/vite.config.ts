e3a1ea61c2bce9e66d27d0ae7578224d82338f7f
Sail <151263555@qq.com>
2022-04-08 15:47:33 +0800

    vite-plugin-pwa

--- pc/vite.config.ts
+++ pc/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 // import ViteRsw from "vite-plugin-rsw";
 
 // https://vitejs.dev/config/
@@ -17,6 +18,32 @@ export default defineConfig({
       reactivityTransform: /^((?!node_modules).)*$/,
     }),
     vueJsx(),
+    VitePWA({
+      manifest: {
+        name: "管理系统",
+        short_name: "管理系统",
+        description: "管理系统",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ]
+      },
+    }),
     // ViteRsw({
     //   cli: "pnpm",
     //   root: "./",
