1f0c758039d22d55f044db8a6b5935f694ef42c3
Sail <151263555@qq.com>
2022-04-14 09:20:27 +0800

    randomUUID

--- nest/src/common/util/uuid.ts
+++ nest/src/common/util/uuid.ts
@@ -1,5 +1,10 @@
 import { randomUUID } from "crypto";
 
+let randomUUID2 = randomUUID;
+if (!randomUUID2) {
+  randomUUID2 = require("uuid").v4;
+}
+
 export function shortUuidV4() {
-  return Buffer.from(randomUUID().replace(/-/gm, ""), "hex").toString("base64").substring(0, 22);
+  return Buffer.from(randomUUID2().replace(/-/gm, ""), "hex").toString("base64").substring(0, 22);
 }
