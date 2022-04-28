416800df152f2c412b9af2fa3620ec8d6d5745a0
Sail <151263555@qq.com>
2022-04-28 10:24:17 +0800

    coderemove

--- codegen/src/lib/coderemove.ts
+++ codegen/src/lib/coderemove.ts
@@ -1,9 +1,10 @@
 import { resolve } from "path";
 import { Context } from "./information_schema";
-import { remove } from "fs-extra";
+import { rm } from "fs/promises";
 
 const out = resolve(`${ __dirname }/../../__out__/`).replace(/\\/gm, "/");
 const projectPh = resolve(`${ __dirname }/../../../`).replace(/\\/gm, "/");
+const buildPh = resolve(`${ __dirname }/../../../build/`).replace(/\\/gm, "/");
 
 async function dropTable(
   context: Context,
@@ -17,22 +18,28 @@ async function dropTable(
 
 export async function coderemove(context: Context, table_name: string) {
   console.log(`删除: ${ out }/nest/src/${ table_name }/`);
-  await remove(`${ out }/nest/src/${ table_name }/`);
+  await rm(`${ out }/nest/src/${ table_name }/`, { force: true, recursive: true });
   
   console.log(`删除: ${ out }/nest/src/${ table_name }2/`);
-  await remove(`${ out }/nest/src/${ table_name }2/`);
+  await rm(`${ out }/nest/src/${ table_name }2/`, { force: true, recursive: true });
   
   console.log(`删除: ${ out }/pc/src/views/${ table_name }/`);
-  await remove(`${ out }/pc/src/views/${ table_name }/`);
+  await rm(`${ out }/pc/src/views/${ table_name }/`, { force: true, recursive: true });
   
   console.log(`删除: ${ projectPh }/nest/src/${ table_name }/`);
-  await remove(`${ projectPh }/nest/src/${ table_name }/`);
+  await rm(`${ projectPh }/nest/src/${ table_name }/`, { force: true, recursive: true });
   
   console.log(`删除: ${ projectPh }/nest/src/${ table_name }2/`);
-  await remove(`${ projectPh }/nest/src/${ table_name }2/`);
+  await rm(`${ projectPh }/nest/src/${ table_name }2/`, { force: true, recursive: true });
   
   console.log(`删除: ${ projectPh }/pc/src/views/${ table_name }/`);
-  await remove(`${ projectPh }/pc/src/views/${ table_name }/`);
+  await rm(`${ projectPh }/pc/src/views/${ table_name }/`, { force: true, recursive: true });
+  
+  console.log(`删除: ${ buildPh }/nest/src/${ table_name }/`);
+  await rm(`${ buildPh }/nest/src/${ table_name }/`, { force: true, recursive: true });
+  
+  console.log(`删除: ${ buildPh }/nest/src/${ table_name }2/`);
+  await rm(`${ buildPh }/nest/src/${ table_name }2/`, { force: true, recursive: true });
   
   console.log(`删除表: ${ table_name }`);
   await dropTable(context, table_name);
