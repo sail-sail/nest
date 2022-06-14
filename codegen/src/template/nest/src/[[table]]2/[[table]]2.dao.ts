580ea6eb34d3b1b08d7f31568ce7b70d01310c92
Sail <151263555@qq.com>
2022-06-14 17:32:06 +0800

    nest

--- codegen/src/template/nest/src/[[table]]2/[[table]]2.dao.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class <#=tableUp#>2Dao {
-  
-  constructor(
-  ) { }
-  
-}
--- nest/src/background_task2/background_task2.dao.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class Background_task2Dao {
-  
-  constructor(
-  ) { }
-  
-}
