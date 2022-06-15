28756a6af7b3bf836ad0eb1aa1dab66ea1b8cdb4
Sail <151263555@qq.com>
2022-06-15 09:44:52 +0800

    wx_usr_id

--- deno/lib/auth/auth.constants.ts
+++ deno/lib/auth/auth.constants.ts
@@ -10,6 +10,7 @@ export const AUTHORIZATION = "Authorization";
 
 export interface AuthModel extends JWTPayload {
   id: string,
+  wx_usr_id?: string,
 }
 
 async function createHash(algorithm: DigestAlgorithm, buffer: Uint8Array): Promise<ArrayBuffer> {
@@ -19,8 +20,9 @@ async function createHash(algorithm: DigestAlgorithm, buffer: Uint8Array): Promi
 
 export async function getPassword(str: string): Promise<string> {
   if (!str) return "";
-  let hash = await createHash("SHA-256", new TextEncoder().encode(str + SECRET_KEY));
+  const textEncoder = new TextEncoder();
+  let hash = await createHash("SHA-256", textEncoder.encode(str + SECRET_KEY));
   const bashStr = encode(hash);
-  hash = await createHash("SHA-256", new TextEncoder().encode((bashStr)));
+  hash = await createHash("SHA-256", textEncoder.encode(bashStr));
   return encode(hash).substring(0, 43);
 }
