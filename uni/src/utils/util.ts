ec85d7ea8eb4bc3e0213d1a85630314503d0d5de
Sail <151263555@qq.com>
2022-09-13 17:13:26 +0800

    isEmpty

--- uni/src/utils/util.ts
+++ uni/src/utils/util.ts
@@ -1,8 +1,11 @@
 
-export function isEmpty(str: string) {
-  if (str === undefined || str === null || str === "" || String(str).trim() === "") return true;
+export function isEmpty(str: any): boolean {
+  if (str === undefined || str === null || str === "" || str === 0) return true;
+  const type = typeof(str);
+  if (type === "string" && str.trim() === "") return true;
+  if (Array.isArray(type) && str.length === 0) return true;
   return false;
-};
+}
 
 let UID = Date.now();
 
