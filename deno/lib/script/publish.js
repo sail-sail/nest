f3adaad0905375ce17103c5be451dc5858480f7a
Sail <151263555@qq.com>
2022-12-11 13:46:54 +0800

    publish

--- deno/lib/script/publish.js
+++ deno/lib/script/publish.js
@@ -1,5 +1,6 @@
 const SSH2Promise = require("ssh2-promise");
 const fs = require("fs/promises");
+const { randomUUID } = require("node:crypto");
 const ecosystem = require(`${ __dirname }/../../ecosystem.config.js`);
 const publish_cnf = require(`${ __dirname }/publish_cnf.js`);
 
@@ -16,6 +17,8 @@ console.log({ ...sshConfig, password: "" });
 
 const buildPath = `${ __dirname }/../../../build/`;
 const publishPath = `${ publishBase }/${ projectName }/`;
+const uuid = randomUUID();
+const publishPathTmp = `${ publishBase }/${ projectName }_tmp_${ uuid }/`;
 
 console.log(publishPath);
 
@@ -27,36 +30,12 @@ console.log(publishPath);
   let data;
   
   try {
-    data = await ssh.exec(`rm -rf ${ publishPath }/docs`);
+    const cmd = `mkdir ${ publishPathTmp }`;
+    console.log(cmd);
+    data = await ssh.exec(cmd);
     console.log(data);
   } catch (err) {
     console.error(err.message);
-  }
-  
-  try {
-    data = await ssh.exec(`rm -rf ${ publishPath }/pc`);
-    console.log(data);
-  } catch (err) {
-    console.error(err.message);
-  }
-  
-  try {
-    data = await ssh.exec(`pm2 stop ${ projectName }`);
-  } catch (err) {
-    console.error(err.message);
-  }
-  
-  try {
-    data = await ssh.exec(`rm -rf ${ publishPath }/deno`);
-    console.log(data);
-  } catch (err) {
-    console.error(err.message);
-  }
-  
-  try {
-    await sftp.mkdir(`${ publishPath }`);
-  } catch (err) {
-    console.log(err);
   }
   
   const treeDir = async function(dir) {
@@ -66,25 +45,81 @@ console.log(publishPath);
       const stats = await fs.stat(`${ buildPath }/${ dir }/${ file }`);
       if (stats.isDirectory()) {
         try {
-          await sftp.mkdir(`${ publishPath }/${ dir }/${ file }`);
+          await sftp.mkdir(`${ publishPathTmp }/${ dir }/${ file }`);
         } catch (errTmp) {
           console.error(errTmp.message);
         }
         await treeDir(`/${ dir }/${ file }`);
       } else {
-        await sftp.fastPut(`${ buildPath }/${ dir }/${ file }`, `${ publishPath }/${ dir }/${ file }`);
+        await sftp.fastPut(`${ buildPath }/${ dir }/${ file }`, `${ publishPathTmp }/${ dir }/${ file }`);
       }
     }
   };
   await treeDir("");
   
-  await ssh.exec(`chmod -R 755 ${ publishPath }/deno/${ projectName }`);
+  try {
+    const cmd = `rm -rf ${ publishPath }/docs`;
+    data = await ssh.exec(cmd);
+    console.log(data);
+  } catch (err) {
+    console.error(err.message);
+  }
   
-  data = await ssh.exec(`cd ${ publishPath }/deno/ && pm2 start`);
-  console.log(data);
+  try {
+    const cmd = `rm -rf ${ publishPath }/pc`;
+    data = await ssh.exec(cmd);
+    console.log(data);
+  } catch (err) {
+    console.error(err.message);
+  }
   
-  data = await ssh.exec(`pm2 ls`);
-  console.log(data);
+  try {
+    const cmd = `pm2 stop ${ projectName }`;
+    data = await ssh.exec(cmd);
+  } catch (err) {
+    console.error(err.message);
+  }
+  
+  try {
+    const cmd = `rm -rf ${ publishPath }/deno`;
+    data = await ssh.exec(cmd);
+    console.log(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+  
+  try {
+    const cmd = `mkdir ${ publishPath }`;
+    console.log(cmd);
+    await sftp.mkdir(`${ publishPath }`);
+  } catch (err) {
+    console.log(err);
+  }
+  
+  try {
+    const cmd = `mv -f ${ publishPathTmp }/* ${ publishPath }/ && rm ${ publishPathTmp }`;
+    console.log(cmd);
+    data = await ssh.exec(cmd);
+    console.log(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+  
+  {
+    const cmd = `chmod -R 755 ${ publishPath }/deno/${ projectName }`;
+    console.log(cmd);
+    await ssh.exec(cmd);
+  }
+  
+  {
+    const cmd = `cd ${ publishPath }/deno/ && pm2 start`;
+    console.log(cmd);
+    data = await ssh.exec(cmd);
+  }
+  
+  if (data) {
+    console.log(data);
+  }
   
   await ssh.close();
 })();
