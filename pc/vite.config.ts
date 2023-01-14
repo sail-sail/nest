0cbbbdeff59ec32ee267849ef7864b4970f56009
Sail <151263555@qq.com>
2023-01-14 16:21:46 +0800

    pwa

--- pc/vite.config.ts
+++ pc/vite.config.ts
@@ -154,22 +154,13 @@ export default defineConfig({
     }),
     Inspector(),
     VitePWA({
+      registerType: "autoUpdate",
       manifest: {
         name: "管理系统",
         short_name: "管理系统",
         description: "管理系统",
         theme_color: "#ffffff",
         icons: [
-          {
-            src: "favicon.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "favicon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
           {
             src: "favicon.png",
             sizes: "512x512",
