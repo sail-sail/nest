3b1ad22112f874d8d2d104d9e2ea5f248fa992d3
Sail <151263555@qq.com>
2023-02-07 09:03:37 +0800

    getDictbiz 业务字典

--- deno/src/dictbiz_detail/dictbiz_detail.graphql.ts
+++ deno/src/dictbiz_detail/dictbiz_detail.graphql.ts
@@ -5,20 +5,7 @@ import {
 } from "./dictbiz_detail.resolver.ts";
 
 defineGraphql(dictbiz_detailResolvers, /* GraphQL */`
-
-  type GetDict {
-    "id"
-    id: String!
-    "字典编码"
-    code: String!
-    "数据类型"
-    type: String!
-    "名称"
-    lbl: String!
-    "值"
-    val: String!
-  }
-
+  
   type Query {
     "获取业务字典列表"
     getDictbiz(codes: [String!]!): [[GetDict!]!]!
