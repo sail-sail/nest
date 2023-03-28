1c22d2f6abd3301e0de1b4ee3978223b9416296d
Sail <151263555@qq.com>
2023-03-28 22:52:30 +0800

    rust

---
+++ rust/src/common/context.rs
@@ -0,0 +1,5 @@
+use async_graphql::{
+  connection::{query, Connection, Edge},
+  Context as GqlContext, Enum, Error, Interface, Object, OutputType, Result,
+};
+
---
+++ rust/src/main.rs
@@ -0,0 +1,66 @@
+mod common;
+
+use async_graphql::{
+  http::{playground_source, GraphQLPlaygroundConfig},
+  EmptyMutation, EmptySubscription, Request, Response, Schema,
+  Object, Context as GqlContext,
+};
+use poem::{
+  get, post, handler,
+  listener::TcpListener,
+  web::{Data, Html, Json},
+  EndpointExt, IntoResponse, Route, Server,
+};
+
+struct QueryRoot;
+
+type QueryRootSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+#[Object]
+impl QueryRoot {
+  
+  async fn hello<'a>(
+    &self,
+    // ctx: &GqlContext<'a>,
+  ) -> &str {
+    return "aaa".into();
+  }
+  
+}
+
+#[handler]
+async fn graphql_handler(schema: Data<&QueryRootSchema>, req: Json<Request>) -> Json<Response> {
+  Json(schema.execute(req.0).await)
+}
+
+#[handler]
+fn graphql_playground() -> impl IntoResponse {
+  Html(playground_source(GraphQLPlaygroundConfig::new("/").with_header("a", "bbb")))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+  if std::env::var_os("RUST_LOG").is_none() {
+    std::env::set_var("RUST_LOG", "poem=debug");
+  }
+  tracing_subscriber::fmt::init();
+
+  let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+    .finish();
+  
+  if cfg!(debug_assertions) {
+    // println!("{}", &schema.sdl());
+    println!("{}", "---------------------------");
+  }
+
+  let app = Route::new()
+    .at("/", get(graphql_playground))
+    .at("/graphql", post(graphql_handler))
+    .data(schema);
+
+  println!("Playground: http://localhost:3000");
+
+  Server::new(TcpListener::bind("127.0.0.1:3000"))
+    .run(app)
+    .await
+}
