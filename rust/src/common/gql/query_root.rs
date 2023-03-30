686546e76558dde2d701006367163b182033bfc3
Sail <151263555@qq.com>
2023-03-31 02:26:13 +0800

    sqlx execute 执行查询

--- rust/src/common/gql/query_root.rs
+++ rust/src/common/gql/query_root.rs
@@ -4,6 +4,7 @@ use async_graphql::{
   Context, Enum, Interface, Object, OutputType,
   EmptyMutation, EmptySubscription, Request, Response, Schema, ErrorExtensions, FieldResult,
 };
+use tracing::info;
 
 use crate::common::context::ReqContext;
 
@@ -18,7 +19,11 @@ impl Query {
     &self,
     gql_ctx: &Context<'_>,
   ) -> Result<String> {
-    let ctx = ReqContext::new(gql_ctx.to_owned());
+    let mut ctx = ReqContext::new(gql_ctx.to_owned());
+    let mut vec: Vec<i32> = Vec::new();
+    vec.push(1);
+    let res = ctx.execute_with("select a from usr where username=?", vec).await?;
+    info!("{:?}", res);
     // tokio::time::sleep(std::time::Duration::from_millis(500)).await;
     Ok(ctx.req_id.to_string())
   }
