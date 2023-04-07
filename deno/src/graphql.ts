fe060272c15e79fc9dfd28e5ce405aae117d79a9
Sail <151263555@qq.com>
2023-04-07 16:34:34 +0800

    mod graphql.ts

--- deno/src/graphql.ts
+++ deno/src/graphql.ts
@@ -1,9 +1,1 @@
-import "/src/base/dept/dept.graphql.ts";
-import "/src/base/menu/menu.graphql.ts";
-import "/src/base/tenant/tenant.graphql.ts";
-import "/src/base/usr/usr.graphql.ts";
-import "/src/base/dict_detail/dict_detail.graphql.ts";
-import "/src/base/dictbiz_detail/dictbiz_detail.graphql.ts";
-import "/src/base/i18n/i18n.graphql.ts";
-import "/src/base/options/options.graphql.ts";
-import "/src/base/lang/lang.graphql.ts";
+import "/src/base/graphql.ts";
