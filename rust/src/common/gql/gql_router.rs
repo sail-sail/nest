bcabf1101eefd2bce29efddb2863c03f08e0510d
Sail <151263555@qq.com>
2023-04-11 04:36:02 +0800

    CtxImpl

--- rust/src/common/gql/gql_router.rs
+++ rust/src/common/gql/gql_router.rs
@@ -1,11 +1,13 @@
 use poem::{
-  handler, IntoResponse,
+  handler, IntoResponse, Response,
   web::{Data, Html, Json},
 };
 
+use poem::http::{StatusCode, header};
+
 use async_graphql::{
+  Request,
   http::{playground_source, GraphQLPlaygroundConfig},
-  Request, Response,
 };
 
 use crate::common::auth::auth_model::{AUTHORIZATION, AuthToken};
@@ -17,7 +19,7 @@ pub async fn graphql_handler(
   schema: Data<&QuerySchema>,
   data: Json<Request>,
   req: &poem::Request,
-) -> Json<Response> {
+) -> Response {
   let mut gql_req = data.0;
   match req.header(AUTHORIZATION).map(ToString::to_string) {
     None => { },
@@ -25,7 +27,23 @@ pub async fn graphql_handler(
       gql_req = gql_req.data::<AuthToken>(auth_token);
     },
   }
-  Json(schema.execute(gql_req).await)
+  let gql_res = schema.execute(gql_req).await;
+  let data = match serde_json::to_vec(&gql_res) {
+    Ok(data) => data,
+    Err(err) => {
+      return Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(err.to_string())
+    }
+  };
+  let mut response = Response::builder()
+    .header(header::CONTENT_TYPE, "application/json; charset=utf-8")
+    .body(data);
+  let headers = response.headers_mut();
+  for (key, value) in gql_res.http_headers.iter() {
+    headers.insert(key, value.to_owned());
+  }
+  response
 }
 
 #[cfg(debug_assertions)]
--- rust/src/gen/base/usr/usr_resolver.rs
+++ rust/src/gen/base/usr/usr_resolver.rs
@@ -1,10 +1,7 @@
 
 use anyhow::Result;
-use poem::http::HeaderName;
-
 use async_graphql::{Context, Object};
 
-use crate::common::auth::auth_model::{AUTHORIZATION, AuthToken};
 use crate::common::context::{CtxImpl, Ctx};
 
 use crate::gen::base::usr::usr_model::UsrModel;
@@ -18,21 +15,9 @@ impl UsrQuery {
   
   async fn hello<'a>(
     &self,
-    gql_ctx: &Context<'a>,
+    ctx: &Context<'a>,
   ) -> Result<Vec<UsrModel>> {
-    let mut ctx = CtxImpl::new()
-      .set_auth_token(
-        gql_ctx.data_opt::<AuthToken>()
-          .map(ToString::to_string)
-      )
-      .set_is_tran(true)
-      ;
-    match ctx.verify_auth_token()? {
-      None => { },
-      Some(auth_token2) => {
-        gql_ctx.insert_http_header(AUTHORIZATION, auth_token2.parse::<HeaderName>()?);
-      },
-    }
+    let mut ctx = CtxImpl::with_tran(&ctx).auth()?;
     let res = usr_service::hello(&mut ctx).await;
     ctx.ok(res).await
   }
