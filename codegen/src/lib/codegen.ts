e1bbbb8dcb307cef132d8685d25079bd7e67cc6d
Sail <151263555@qq.com>
2023-04-18 15:05:09 +0800

    codegen

--- codegen/src/lib/codegen.ts
+++ codegen/src/lib/codegen.ts
@@ -291,8 +291,9 @@ export async function gitDiffOut() {
   // 覆盖xlsx文件
   await copyXlsx(out);
   const diffFile = "__test__.diff";
-  const diffStr = `git diff --full-index ./* > ${projectPh}/${ diffFile }`;
+  const diffStr = `git diff --full-index > ${projectPh}/${ diffFile }`;
   console.log(diffStr);
+  shelljs.cd(projectPh);
   shelljs.exec(diffStr);
   let str = await readFile(`${ projectPh }/${ diffFile }`, "utf8");
   if (!isEmpty0(str)) {
