8f45e0185fb0e4ae4f81282dbde2501af45be9af
Sail <151263555@qq.com>
2023-04-19 09:37:55 +0800

    find_all

---
+++ codegen/src/template/rust/src/gen/[[mod]]/[[table]]/[[table]]_resolver.rs
@@ -0,0 +1,70 @@
+<#
+const hasOrderBy = columns.some((column) => column.COLUMN_NAME === 'order_by');
+const hasPassword = columns.some((column) => column.isPassword);
+const hasLocked = columns.some((column) => column.COLUMN_NAME === "is_locked");
+const hasDeptId = columns.some((column) => column.COLUMN_NAME === "dept_id");
+const hasVersion = columns.some((column) => column.COLUMN_NAME === "version");
+const Table_Up = tableUp.split("_").map(function(item) {
+  return item.substring(0, 1).toUpperCase() + item.substring(1);
+}).join("_");
+const tableUP = tableUp.split("_").map(function(item) {
+  return item.substring(0, 1).toUpperCase() + item.substring(1);
+}).join("");
+const hasDict = columns.some((column) => {
+  if (column.ignoreCodegen) {
+    return false;
+  }
+  const column_name = column.COLUMN_NAME;
+  if (column_name === "id") {
+    return false;
+  }
+  return column.dict;
+});
+const hasDictbiz = columns.some((column) => {
+  if (column.ignoreCodegen) {
+    return false;
+  }
+  const column_name = column.COLUMN_NAME;
+  if (column_name === "id") {
+    return false;
+  }
+  return column.dictbiz;
+});
+#>use anyhow::Result;
+use async_graphql::{Context, Object};
+
+use crate::common::context::{CtxImpl, Ctx};
+use crate::common::gql::model::{PageInput, SortInput};
+
+use super::<#=table#>_model::{<#=tableUP#>Model, <#=tableUP#>Search};
+use super::<#=table#>_service;
+
+
+#[derive(Default)]
+pub struct <#=tableUP#>Resolver;
+
+#[Object]
+impl <#=tableUP#>Resolver {
+  
+  /// 根据搜索条件和分页查找数据
+  async fn find_all_<#=table#><'a>(
+    &self,
+    ctx: &Context<'a>,
+    search: Option<<#=tableUP#>Search>,
+    page: Option<PageInput>,
+    sort: Option<Vec<SortInput>>,
+  ) -> Result<Vec<<#=tableUP#>Model>> {
+    let mut ctx = CtxImpl::with_tran(&ctx).auth()?;
+    
+    let res = <#=table#>_service::find_all(
+      &mut ctx,
+      search,
+      page,
+      sort,
+      None,
+    ).await;
+    
+    ctx.ok(res).await
+  }
+  
+}
