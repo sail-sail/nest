8e5c4d9977c9c990a22636557bab724208240cf3
Sail <151263555@qq.com>
2023-05-15 16:06:04 +0800

    get_login_tenants

--- rust/src/src/base/tenant/tenant_resolver.rs
+++ rust/src/src/base/tenant/tenant_resolver.rs
@@ -13,13 +13,13 @@ pub struct TenantQuery;
 impl TenantQuery {
   
   /// 根据 当前网址的域名+端口 获取 租户列表
-  async fn get_login_teants<'a>(
+  async fn get_login_tenants<'a>(
     &self,
     ctx: &Context<'a>,
     host: String,
   ) -> Result<Vec<TenantModel>> {
     let mut ctx = CtxImpl::new(ctx);
-    let res = tenant_service::get_login_teants(&mut ctx, host).await?;
+    let res = tenant_service::get_login_tenants(&mut ctx, host).await?;
     Ok(res)
   }
   
