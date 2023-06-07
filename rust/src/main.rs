8bf9e9809a00bd12265f8dbae1e8dc1c95449195
Sail <151263555@qq.com>
2023-06-07 16:10:08 +0800

    tracing-appender

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -40,7 +40,38 @@ async fn main() -> Result<(), std::io::Error> {
   if std::env::var_os("RUST_LOG").is_none() {
     std::env::set_var("RUST_LOG", "server=info");
   }
-  tracing_subscriber::fmt::init();
+  
+  let log_path = std::env::var_os("log_path");
+  
+  #[cfg(debug_assertions)]
+  {
+    if log_path.is_none() {
+      tracing_subscriber::fmt::init();
+    } else {
+      let log_path = log_path.unwrap();
+      let file_appender = tracing_appender::rolling::daily(log_path, "server.log");
+      let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+      tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .init();
+    }
+  }
+  
+  #[cfg(release_assertions)]
+  {
+    if log_path.is_none() {
+      tracing_subscriber::fmt.with_ansi(false).init();
+    } else {
+      let log_path = log_path.unwrap();
+      let file_appender = tracing_appender::rolling::daily(log_path, "server.log");
+      let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+      tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_ansi(false)
+        .init();
+    }
+  }
+  
   
   // oss
   tokio::spawn(async move {
@@ -59,24 +90,38 @@ async fn main() -> Result<(), std::io::Error> {
   
   #[cfg(debug_assertions)]
   {
-    let file = "src/common/gql/schema.graphql";
+    let file_path = "src/common/gql/schema.graphql";
     let schema = schema.sdl();
-    let old_schema = {
-      if std::path::Path::new(file).exists() {
-        std::fs::read_to_string(file).unwrap()
-      } else {
-        "".to_string()
-      }
-    };
-    if old_schema != schema {
-      info!("write graphql schema");
-      match std::fs::write(file, &schema) {
-        Ok(_) => {},
-        Err(e) => {
-          info!("write graphql schema error: {}", e);
-        },
+    let mut is_equal = true;
+    let file = std::fs::File::open(file_path).unwrap();
+    let metadata = file.metadata().unwrap();
+    let size = metadata.len();
+    if size != schema.len() as u64 {
+      is_equal = false;
+    } else {
+      let old_schema = {
+        if std::path::Path::new(file_path).exists() {
+          std::fs::read_to_string(file_path).unwrap()
+        } else {
+          "".to_string()
+        }
+      };
+      if old_schema != schema {
+        is_equal = false;
       }
     }
+    if !is_equal {
+      use std::io::Write;
+      let mut writer = std::io::BufWriter::new(std::fs::File::create(file_path).unwrap());
+      writer.write(schema.as_bytes()).unwrap();
+    }
+    std::process::Command::new("node")
+      .arg("node_modules/@graphql-codegen/cli/cjs/bin.js")
+      .arg("--config")
+      .arg("src/common/script/graphql_codegen_config.yml",)
+      .arg("--watch")
+      .spawn()
+      .unwrap();
   }
   
   let app = {
