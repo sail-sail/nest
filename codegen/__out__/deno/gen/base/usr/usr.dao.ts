23bf195d40fff471a1fbabae7abd9b6e28a51291
Sail <151263555@qq.com>
2023-06-19 21:31:46 +0800

    updateById: id cannot be empty

--- codegen/__out__/deno/gen/base/usr/usr.dao.ts
+++ codegen/__out__/deno/gen/base/usr/usr.dao.ts
@@ -886,8 +886,11 @@ export async function updateById(
   const table = "base_usr";
   const method = "updateById";
   
-  if (!id || !model) {
-    return id;
+  if (!id) {
+    throw new Error("updateById: id cannot be empty");
+  }
+  if (!model) {
+    throw new Error("updateById: model cannot be empty");
   }
   
   const [
--- deno/gen/base/options/options.dao.ts
+++ deno/gen/base/options/options.dao.ts
@@ -794,8 +794,11 @@ export async function updateById(
   const table = "base_options";
   const method = "updateById";
   
-  if (!id || !model) {
-    return id;
+  if (!id) {
+    throw new Error("updateById: id cannot be empty");
+  }
+  if (!model) {
+    throw new Error("updateById: model cannot be empty");
   }
   
   const [
