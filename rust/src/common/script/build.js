d24e1f8c48f8d0f0f3c450c881c344a4921d7d56
Sail <151263555@qq.com>
2023-06-27 09:26:56 +0800

    incremental 增量编译

--- rust/src/common/script/build.js
+++ rust/src/common/script/build.js
@@ -5,11 +5,8 @@ const {
   copy,
   remove,
   mkdir,
-  readFile,
-  writeFile,
+  move,
 } = require("fs-extra");
-
-const SSH2Promise = require("ssh2-promise");
 
 const argv = minimist(process.argv.slice(2));
 
@@ -41,66 +38,23 @@ async function pc() {
   });
 }
 
-// async function compile_temp() {
-//   console.log("compile");
-//   let cmd = `"E:/Program Files (x86)/VMware/VMware Workstation/vmware.exe" -x -- "E:/softwear/Virtual Machines/CentOS7/CentOS7.vmx"`;
-//   child_process.exec(cmd, function(err, stdout, stderr) {
-//     if (err) {
-//       console.log("\n" + stderr);
-//     } else {
-//       console.log("\n" + stdout);
-//     }
-//   });
-//   const sshConfig = {
-//     host: "192.168.74.128",
-//     username: "root",
-//     password: "abc123",
-//     port: 22,
-//     reconnect: true,
-//     reconnectTries: 30,
-//     reconnectDelay: 2000,
-//   };
-//   const publishPath = "/data/nest/";
-//   const ssh = new SSH2Promise(sshConfig);
-//   await ssh.connect();
-//   const sftp = ssh.sftp();
-//   let data = "";
-//   try {
-//     let cmd = "";
-//     cmd += `cd ${ publishPath }`;
-//     cmd += " ; cd ./rust";
-//     cmd += " ; git pull";
-//     cmd += " ; git checkout rust";
-//     cmd += " ; git pull";
-//     cmd += " ; cargo build --release";
-//     data = await ssh.exec(cmd);
-//   } catch (err) {
-//     console.log(err);
-//   }
-//   await mkdir(`${ buildDir }/rust/rust`, { recursive: true });
-//   await sftp.fastGet(`${ publishPath }/rust/target/release/server`, `${ buildDir }/rust/rust/server`);
-//   await ssh.close();
-//   if (data) {
-//     console.log(data);
-//   }
-// }
-
 async function compile() {
   console.log("compile");
   const cwd = `${ projectDir }/rust/`;
   let cmd = "";
   cmd += `wsl -e bash -lic `;
   cmd += `"`;
-  cmd += `rustup update`;
-  cmd += ` && rustup target add x86_64-unknown-linux-musl`;
-  cmd += ` && cargo build --release --target=x86_64-unknown-linux-musl`;
+  // cmd += `rustup update`;
+  // cmd += ` && rustup target add x86_64-unknown-linux-musl`;
+  // cmd += ` && cargo build --release --target=x86_64-unknown-linux-musl`;
+  cmd += `cargo build --release --target=x86_64-unknown-linux-musl`;
   cmd += `"`;
   child_process.execSync(cmd, {
     cwd,
     stdio: "inherit",
   });
   await mkdir(`${ buildDir }/rust`, { recursive: true });
-  await copy(`${ cwd }/target/x86_64-unknown-linux-musl/release/server`, `${ buildDir }/rust/server`);
+  await move(`${ cwd }/target/x86_64-unknown-linux-musl/release/server`, `${ buildDir }/rust/server`);
 }
 
 async function publish() {
@@ -116,7 +70,7 @@ async function publish() {
     await remove(buildDir);
     await mkdir(buildDir, { recursive: true });
     try {
-      await remove(buildDir + "/../deno");
+      await remove(buildDir + "/deno");
     } catch (err) {
     }
     await copyEnv();
