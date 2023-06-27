ceda53ab9cacb2c81b26d6ffa097a11bc867796f
Sail <151263555@qq.com>
2023-06-27 08:21:05 +0800

    compile wsl

--- rust/src/common/script/build.js
+++ rust/src/common/script/build.js
@@ -41,63 +41,74 @@ async function pc() {
   });
 }
 
-async function compile_temp() {
-  console.log("compile");
-  let cmd = `"E:/Program Files (x86)/VMware/VMware Workstation/vmware.exe" -x -- "E:/softwear/Virtual Machines/CentOS7/CentOS7.vmx"`;
-  child_process.exec(cmd, function(err, stdout, stderr) {
-    if (err) {
-      console.log("\n" + stderr);
-    } else {
-      console.log("\n" + stdout);
-    }
-  });
-  const sshConfig = {
-    host: "192.168.74.128",
-    username: "root",
-    password: "abc123",
-    port: 22,
-    reconnect: true,
-    reconnectTries: 30,
-    reconnectDelay: 2000,
-  };
-  const publishPath = "/data/nest/";
-  const ssh = new SSH2Promise(sshConfig);
-  await ssh.connect();
-  const sftp = ssh.sftp();
-  let data = "";
-  try {
-    let cmd = "";
-    cmd += `cd ${ publishPath }`;
-    cmd += " ; cd ./rust";
-    cmd += " ; git pull";
-    cmd += " ; git checkout rust";
-    cmd += " ; git pull";
-    cmd += " ; cargo build --release";
-    data = await ssh.exec(cmd);
-  } catch (err) {
-    console.log(err);
-  }
-  await mkdir(`${ buildDir }/rust/rust`, { recursive: true });
-  await sftp.fastGet(`${ publishPath }/rust/target/release/server`, `${ buildDir }/rust/rust/server`);
-  await ssh.close();
-  if (data) {
-    console.log(data);
-  }
-}
+// async function compile_temp() {
+//   console.log("compile");
+//   let cmd = `"E:/Program Files (x86)/VMware/VMware Workstation/vmware.exe" -x -- "E:/softwear/Virtual Machines/CentOS7/CentOS7.vmx"`;
+//   child_process.exec(cmd, function(err, stdout, stderr) {
+//     if (err) {
+//       console.log("\n" + stderr);
+//     } else {
+//       console.log("\n" + stdout);
+//     }
+//   });
+//   const sshConfig = {
+//     host: "192.168.74.128",
+//     username: "root",
+//     password: "abc123",
+//     port: 22,
+//     reconnect: true,
+//     reconnectTries: 30,
+//     reconnectDelay: 2000,
+//   };
+//   const publishPath = "/data/nest/";
+//   const ssh = new SSH2Promise(sshConfig);
+//   await ssh.connect();
+//   const sftp = ssh.sftp();
+//   let data = "";
+//   try {
+//     let cmd = "";
+//     cmd += `cd ${ publishPath }`;
+//     cmd += " ; cd ./rust";
+//     cmd += " ; git pull";
+//     cmd += " ; git checkout rust";
+//     cmd += " ; git pull";
+//     cmd += " ; cargo build --release";
+//     data = await ssh.exec(cmd);
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   await mkdir(`${ buildDir }/rust/rust`, { recursive: true });
+//   await sftp.fastGet(`${ publishPath }/rust/target/release/server`, `${ buildDir }/rust/rust/server`);
+//   await ssh.close();
+//   if (data) {
+//     console.log(data);
+//   }
+// }
 
 async function compile() {
   console.log("compile");
   const cwd = `${ projectDir }/rust/`;
-  // let cmd = "";
-  // cmd += "wsl source /etc/profile";
-  // cmd += " && rustup target add x86_64-unknown-linux-musl";
-  // cmd += " && cargo build --release --target=x86_64-unknown-linux-musl";
-  // child_process.execSync(cmd, {
-  //   cwd,
-  //   stdio: "inherit",
-  // });
+  let cmd = "";
+  cmd += `wsl -e bash -lic `;
+  cmd += `"`;
+  cmd += `rustup update`;
+  cmd += ` && rustup target add x86_64-unknown-linux-musl`;
+  cmd += ` && cargo build --release --target=x86_64-unknown-linux-musl`;
+  cmd += `"`;
+  child_process.execSync(cmd, {
+    cwd,
+    stdio: "inherit",
+  });
   await mkdir(`${ buildDir }/rust`, { recursive: true });
   await copy(`${ cwd }/target/x86_64-unknown-linux-musl/release/server`, `${ buildDir }/rust/server`);
+}
+
+async function publish() {
+  console.log("publish");
+  child_process.execSync("npm run publish", {
+    cwd: `${ projectDir }/rust`,
+    stdio: "inherit",
+  });
 }
 
 (async function() {
@@ -111,6 +122,7 @@ async function compile() {
     await copyEnv();
     await gqlgen();
     await compile();
+    await publish();
     await pc();
     process.exit(0);
   }
