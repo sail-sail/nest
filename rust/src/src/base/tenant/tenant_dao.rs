bce0499726ad24ef309dd195dff7b17ebca2cd80
Sail <151263555@qq.com>
2023-06-30 18:21:15 +0800

    filter_menu_ids_by_tenant

---
+++ rust/src/src/base/tenant/tenant_dao.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use crate::common::context::Ctx;
+
+use crate::gen::base::tenant::tenant_dao;
+
+/// 当前租户拥有的菜单
+pub async fn get_menu_ids_by_tenant<'a>(
+  ctx: &mut impl Ctx<'a>,
+) -> Result<Vec<String>> {
+  let tenant_id = ctx.get_auth_tenant_id();
+  if tenant_id.is_none() {
+    return Ok(vec![]);
+  }
+  let tenant_id = tenant_id.unwrap();
+  let tenant_model = tenant_dao::find_by_id(
+    ctx,
+    tenant_id,
+    None,
+  ).await?;
+  if tenant_model.is_none() {
+    return Ok(vec![]);
+  }
+  let tenant_model = tenant_model.unwrap();
+  let menu_ids = tenant_model.menu_ids;
+  Ok(menu_ids)
+}
+
+pub async fn filter_menu_ids_by_tenant<'a>(
+  ctx: &mut impl Ctx<'a>,
+  menu_ids: Vec<String>,
+) -> Result<Vec<String>> {
+  let tenant_menu_ids = get_menu_ids_by_tenant(ctx).await?;
+  let mut menu_ids2 = vec![];
+  for menu_id in menu_ids {
+    if tenant_menu_ids.contains(&menu_id) {
+      menu_ids2.push(menu_id);
+    }
+  }
+  Ok(menu_ids2)
+}
