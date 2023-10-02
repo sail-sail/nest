2ec8a5387b93fffa07e8900ae37183685276f11f
Sail <151263555@qq.com>
2023-10-03 03:28:47 +0800

    is_encrypt

--- codegen/src/tables/tables.ts
+++ codegen/src/tables/tables.ts
@@ -273,7 +273,6 @@ export default defineConfig({
       cache: true,
       uniques: [
         [ "parent_id", "lbl" ],
-        [ "route_path" ],
       ],
       defaultSort: {
         prop: "order_by",
