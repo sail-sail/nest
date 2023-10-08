be85da9a565deb8f6f2135f6f1167da268676faa
Sail <151263555@qq.com>
2023-10-08 15:43:33 +0800

    isMonth 导入

--- codegen/__out__/pc/src/views/base/i18n/Api.ts
+++ codegen/__out__/pc/src/views/base/i18n/Api.ts
@@ -599,6 +599,8 @@ export async function importModels(
       break;
     }
     
+    percentage.value = Math.floor((i + 1) / models.length * 100);
+    
     const item = models[i];
     
     opt = opt || { };
@@ -617,7 +619,6 @@ export async function importModels(
       failErrMsgs.push(await nsAsync(`第 {0} 行导入失败: {1}`, i + 1, err));
     }
     
-    percentage.value = Math.floor((i + 1) / models.length * 100);
   }
   
   return showUploadMsg(succNum, failNum, failErrMsgs);
