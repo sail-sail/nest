e07dc409b66b34db1791dd7f13c00431ca0d3d10
Sail <151263555@qq.com>
2023-10-09 09:27:42 +0800

    isMonth date

--- pc/src/views/hrm/payslip/List.vue
+++ pc/src/views/hrm/payslip/List.vue
@@ -1386,7 +1386,7 @@ async function onImportExcel() {
           "individual_tax": "string",
           "self_pay": "string",
           "net_pay": "string",
-          "is_locked_lbl": "string",
+          "is_locked_lbl": "number",
           "rem": "string",
         },
       },
