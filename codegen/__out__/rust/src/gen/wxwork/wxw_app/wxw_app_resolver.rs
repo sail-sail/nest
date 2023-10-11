107a85c16507f0dffef3fb16b122e614ff9dd021
Sail <151263555@qq.com>
2023-10-11 19:47:26 +0800

    setIdByLbl

--- codegen/__out__/rust/src/gen/wxwork/wxw_app/wxw_app_resolver.rs
+++ codegen/__out__/rust/src/gen/wxwork/wxw_app/wxw_app_resolver.rs
@@ -85,6 +85,11 @@ pub async fn create<'a>(
   options: Option<Options>,
 ) -> Result<String> {
   
+  let input = wxw_app_service::set_id_by_lbl(
+    ctx,
+    input,
+  ).await?;
+  
   use_permit(
     ctx,
     "/wxwork/wxw_app".to_owned(),
@@ -127,6 +132,11 @@ pub async fn update_by_id<'a>(
   input: WxwAppInput,
   options: Option<Options>,
 ) -> Result<String> {
+  
+  let input = wxw_app_service::set_id_by_lbl(
+    ctx,
+    input,
+  ).await?;
   
   use_permit(
     ctx,
