0ebc2d776b24d5c5bbae3ea8a522dc817caf0d92
Sail <151263555@qq.com>
2023-10-15 23:14:27 +0800

    uuid 碰撞检测

--- codegen/__out__/deno/gen/wxwork/wxw_app_token/wxw_app_token.dao.ts
+++ codegen/__out__/deno/gen/wxwork/wxw_app_token/wxw_app_token.dao.ts
@@ -621,6 +621,10 @@ export async function create(
   const table = "wxwork_wxw_app_token";
   const method = "create";
   
+  if (input.id) {
+    throw new Error(`Can not set id when create in dao: ${ table }`);
+  }
+  
   await setIdByLbl(input);
   
   const oldModels = await findByUnique(input, options);
@@ -642,8 +646,13 @@ export async function create(
     }
   }
   
-  if (!input.id) {
+  while (true) {
     input.id = shortUuidV4();
+    const isExist = await existById(input.id);
+    if (!isExist) {
+      break;
+    }
+    ctx.error(`id: ${ input.id } has collided when create in table: ${ table }`);
   }
   
   const args = new QueryArgs();
