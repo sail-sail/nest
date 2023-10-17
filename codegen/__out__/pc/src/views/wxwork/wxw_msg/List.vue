f073c4c515f78ca3d78f937fe1402f6e359597f0
Sail <151263555@qq.com>
2023-10-17 18:18:12 +0800

    showBuildIn

--- codegen/__out__/pc/src/views/wxwork/wxw_msg/List.vue
+++ codegen/__out__/pc/src/views/wxwork/wxw_msg/List.vue
@@ -871,7 +871,10 @@ async function dataGrid(
 }
 
 function getDataSearch() {
-  let search2 = {
+  if (showBuildIn) {
+    Object.assign(search, builtInSearch);
+  }
+  const search2 = {
     ...search,
     idsChecked: undefined,
   };
@@ -1147,7 +1150,9 @@ async function initFrame() {
 watch(
   () => builtInSearch,
   async function() {
-    Object.assign(search, builtInSearch);
+    if (deepCompare(builtInSearch, search)) {
+      return;
+    }
     await dataGrid(true);
   },
   {
