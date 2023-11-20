1564ebb5f9c5089cc200884c05832c8516bdce85
Sail <151263555@qq.com>
2023-11-20 15:07:52 +0800

    用户 order_by

--- pc/src/views/cron/cron_job/Detail.vue
+++ pc/src/views/cron/cron_job/Detail.vue
@@ -313,18 +313,11 @@ watchEffect(async () => {
         message: `${ await nsAsync("请输入") } ${ n("时区") }`,
       },
     ],
-    // 锁定
-    is_locked: [
+    // 排序
+    order_by: [
       {
         required: true,
-        message: `${ await nsAsync("请输入") } ${ n("锁定") }`,
-      },
-    ],
-    // 启用
-    is_enabled: [
-      {
-        required: true,
-        message: `${ await nsAsync("请输入") } ${ n("启用") }`,
+        message: `${ await nsAsync("请输入") } ${ n("排序") }`,
       },
     ],
   };
