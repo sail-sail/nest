a3c711e374030686ebbeb903223157b84c96a44d
Sail <151263555@qq.com>
2023-12-01 16:13:16 +0800

    Fix git command arguments

--- rust/build.rs
+++ rust/build.rs
@@ -4,7 +4,7 @@ use std::env;
 fn main() {
   if env::var("PROFILE").unwrap() == "release" {
     let output = Command::new("git")
-      .args(&["log", "-n", "1", "--pretty=format:%h"])
+      .args(["log", "-n", "1", "--pretty=format:%h"])
       .output();
     if let Ok(output) = output {
       let git_hash = String::from_utf8(output.stdout).unwrap();
