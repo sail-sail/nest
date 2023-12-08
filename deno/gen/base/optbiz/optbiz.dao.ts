ea10b47a4b17744715756261cdbec4e0b194d8ab
Sail <151263555@qq.com>
2023-12-08 09:49:32 +0800

    完善Api注释

--- deno/gen/base/optbiz/optbiz.dao.ts
+++ deno/gen/base/optbiz/optbiz.dao.ts
@@ -274,7 +274,7 @@ export async function findCount(
 }
 
 /**
- * 根据搜索条件和分页查找数据
+ * 根据搜索条件和分页查找业务选项列表
  * @param {OptbizSearch} search? 搜索条件
  * @param {SortInput|SortInput[]} sort? 排序
  */
@@ -438,7 +438,7 @@ export async function setIdByLbl(
 }
 
 /**
- * 获取字段对应的名称
+ * 获取业务选项字段注释
  */
 export async function getFieldComments(): Promise<OptbizFieldComment> {
   const n = initN(route_path);
@@ -467,7 +467,7 @@ export async function getFieldComments(): Promise<OptbizFieldComment> {
 }
 
 /**
- * 通过唯一约束获得数据列表
+ * 通过唯一约束获得业务选项列表
  * @param {OptbizInput} search0
  */
 export async function findByUnique(
@@ -526,7 +526,7 @@ export function equalsByUnique(
 }
 
 /**
- * 通过唯一约束检查数据是否已经存在
+ * 通过唯一约束检查业务选项是否已经存在
  * @param {OptbizInput} input
  * @param {OptbizModel} oldModel
  * @param {UniqueType} uniqueType
@@ -565,7 +565,7 @@ export async function checkByUnique(
 }
 
 /**
- * 根据条件查找第一条数据
+ * 根据条件查找第一个业务选项
  * @param {OptbizSearch} search?
  */
 export async function findOne(
@@ -584,7 +584,7 @@ export async function findOne(
 }
 
 /**
- * 根据id查找数据
+ * 根据id查找业务选项
  * @param {OptbizId} id
  */
 export async function findById(
@@ -600,7 +600,7 @@ export async function findById(
 }
 
 /**
- * 根据搜索条件判断数据是否存在
+ * 根据搜索条件判断业务选项是否存在
  * @param {OptbizSearch} search?
  */
 export async function exist(
@@ -614,7 +614,7 @@ export async function exist(
 }
 
 /**
- * 根据id判断数据是否存在
+ * 根据id判断业务选项是否存在
  * @param {OptbizId} id
  */
 export async function existById(
@@ -654,7 +654,7 @@ export async function existById(
   return result;
 }
 
-/** 校验记录是否启用 */
+/** 校验业务选项是否启用 */
 export async function validateIsEnabled(
   model: OptbizModel,
 ) {
@@ -663,7 +663,7 @@ export async function validateIsEnabled(
   }
 }
 
-/** 校验记录是否存在 */
+/** 校验业务选项是否存在 */
 export async function validateOption(
   model?: OptbizModel,
 ) {
@@ -674,7 +674,7 @@ export async function validateOption(
 }
 
 /**
- * 增加和修改时校验输入
+ * 业务选项增加和修改时校验输入
  * @param input 
  */
 export async function validate(
@@ -734,7 +734,7 @@ export async function validate(
 }
 
 /**
- * 创建数据
+ * 创建业务选项
  * @param {OptbizInput} input
  * @param {({
  *   uniqueType?: UniqueType,
@@ -929,7 +929,7 @@ export async function delCache() {
 }
 
 /**
- * 根据id修改租户id
+ * 业务选项根据id修改租户id
  * @param {OptbizId} id
  * @param {TenantId} tenant_id
  * @param {{
@@ -968,7 +968,7 @@ export async function updateTenantById(
 }
 
 /**
- * 根据 id 获取版本号
+ * 根据 id 获取业务选项版本号
  */
 export async function getVersionById(
   id: OptbizId,
@@ -982,7 +982,7 @@ export async function getVersionById(
 }
 
 /**
- * 根据id修改一行数据
+ * 根据id修改业务选项
  * @param {OptbizId} id
  * @param {OptbizInput} input
  * @param {({
@@ -1185,8 +1185,8 @@ export async function deleteByIds(
 }
 
 /**
- * 根据 ID 查找是否已启用
- * 记录不存在则返回 undefined
+ * 根据 ID 查找业务选项是否已启用
+ * 不存在则返回 undefined
  * @param {OptbizId} id
  * @return {Promise<0 | 1 | undefined>}
  */
@@ -1254,9 +1254,9 @@ export async function enableByIds(
 }
 
 /**
- * 根据 ID 查找是否已锁定
- * 已锁定的记录不能修改和删除
- * 记录不存在则返回 undefined
+ * 根据 ID 查找业务选项是否已锁定
+ * 已锁定的不能修改和删除
+ * 不存在则返回 undefined
  * @param {OptbizId} id
  * @return {Promise<0 | 1 | undefined>}
  */
@@ -1324,7 +1324,7 @@ export async function lockByIds(
 }
 
 /**
- * 根据 ids 还原数据
+ * 根据 ids 还原业务选项
  * @param {OptbizId[]} ids
  * @return {Promise<number>}
  */
@@ -1383,7 +1383,7 @@ export async function revertByIds(
 }
 
 /**
- * 根据 ids 彻底删除数据
+ * 根据 ids 彻底删除业务选项
  * @param {OptbizId[]} ids
  * @return {Promise<number>}
  */
@@ -1438,7 +1438,7 @@ export async function forceDeleteByIds(
 }
   
 /**
- * 查找 order_by 字段的最大值
+ * 查找 业务选项 order_by 字段的最大值
  * @return {Promise<number>}
  */
 export async function findLastOrderBy(
