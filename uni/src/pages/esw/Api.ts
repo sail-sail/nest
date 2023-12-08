8fb8a04f0be255ac95dfa01f6a9cb87e6c7c6c89
Sail <151263555@qq.com>
2023-12-08 16:34:44 +0800

    移动端 首页 产品类别

---
+++ uni/src/pages/esw/Api.ts
@@ -0,0 +1,105 @@
+import type {
+  WxappConfigSearch,
+  WxappConfigModel as WxappConfigModel0,
+} from "#/types";
+
+type WxappConfigModel = WxappConfigModel0 & {
+  img_urls: string[];
+};
+
+/**
+ * 根据搜索条件查找第一个小程序配置
+ * @export findOne
+ * @param {WxappConfigSearch} search?
+ * @param {Sort[]} sort?
+ * @param {GqlOpt} opt?
+ */
+export async function findOne(
+  search?: WxappConfigSearch,
+  sort?: Sort[],
+  opt?: GqlOpt,
+) {
+  const data: {
+    findOneWxappConfig: WxappConfigModel | undefined;
+  } = await query({
+    query: /* GraphQL */ `
+      query($search: WxappConfigSearch, $sort: [SortInput!]) {
+        findOneWxappConfig(search: $search, sort: $sort) {
+          id
+          img
+          lbl
+        }
+      }
+    `,
+    variables: {
+      search,
+      sort,
+    },
+  }, opt);
+  const model = data.findOneWxappConfig;
+  if (model) {
+    model.img_urls = model.img_urls || [ ];
+    for (const img_id of model.img.split(",")) {
+      if (!img_id) {
+        continue;
+      }
+      const img_url = getImgUrl({
+        id: img_id,
+        width: 750,
+      });
+      model.img_urls.push(img_url);
+    }
+  }
+  return model;
+}
+
+/**
+ * 根据搜索条件查找产品类别列表
+ * @export findAll
+ * @param {PtTypeSearch} search?
+ * @param {PageInput} page
+ * @param {Sort[]} sort?
+ * @param {GqlOpt} opt?
+ */
+export async function findAllPtType(
+  search?: PtTypeSearch,
+  page?: PageInput,
+  sort?: Sort[],
+  opt?: GqlOpt,
+) {
+  const data: {
+    findAllPtType: Query["findAllPtType"];
+  } = await query({
+    query: /* GraphQL */ `
+      query($search: PtTypeSearch, $page: PageInput, $sort: [SortInput!]) {
+        findAllPtType(search: $search, page: $page, sort: $sort) {
+          id
+          img
+          lbl
+          is_recommend
+        }
+      }
+    `,
+    variables: {
+      search,
+      page,
+      sort,
+    },
+  }, opt);
+  const res = data.findAllPtType;
+  for (let i = 0; i < res.length; i++) {
+    const model = res[i];
+    model.img_urls = model.img_urls || [ ];
+    for (const img_id of model.img.split(",")) {
+      if (!img_id) {
+        continue;
+      }
+      const img_url = getImgUrl({
+        id: img_id,
+        width: 750,
+      });
+      model.img_urls.push(img_url);
+    }
+  }
+  return res;
+}
