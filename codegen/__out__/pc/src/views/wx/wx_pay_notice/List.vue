154c756e8378054386dd1435d4a53e2e3ef1ed06
Sail <151263555@qq.com>
2023-12-18 14:38:07 +0800

    selectInput focus

--- codegen/__out__/pc/src/views/wx/wx_pay_notice/List.vue
+++ codegen/__out__/pc/src/views/wx/wx_pay_notice/List.vue
@@ -693,6 +693,7 @@ const props = defineProps<{
   showBuildIn?: string;
   isPagination?: string;
   isLocked?: string;
+  isFocus?: string;
   ids?: string[]; //ids
   selectedIds?: WxPayNoticeId[]; //已选择行的id列表
   isMultiple?: Boolean; //是否多选
@@ -733,6 +734,7 @@ const builtInSearchType: { [key: string]: string } = {
   showBuildIn: "0|1",
   isPagination: "0|1",
   isLocked: "0|1",
+  isFocus: "0|1",
   ids: "string[]",
   trade_type: "string[]",
   trade_type_lbl: "string[]",
@@ -756,6 +758,7 @@ const propsNotInSearch: string[] = [
   "showBuildIn",
   "isPagination",
   "isLocked",
+  "isFocus",
 ];
 
 /** 内置搜索条件 */
@@ -780,6 +783,8 @@ const showBuildIn = $computed(() => props.showBuildIn === "1");
 const isPagination = $computed(() => !props.isPagination || props.isPagination === "1");
 /** 是否只读模式 */
 const isLocked = $computed(() => props.isLocked === "1");
+/** 是否 focus, 默认为 true */
+const isFocus = $computed(() => props.isFocus !== "0");
 
 /** 分页功能 */
 let {
@@ -1290,10 +1295,27 @@ async function initI18nsEfc() {
   ]);
 }
 
-async function initFrame() {
-  if (!usrStore.authorization) {
+async function focus() {
+  if (!inited || !tableRef || !tableRef.$el) {
     return;
   }
+  tableRef.$el.focus();
+}
+
+watch(
+  () => [
+    props.isFocus,
+    inited,
+  ],
+  () => {
+    if (!inited || !isFocus || !tableRef || !tableRef.$el) {
+      return;
+    }
+    tableRef.$el.focus();
+  },
+);
+
+async function initFrame() {
   await Promise.all([
     initI18nsEfc(),
     dataGrid(true),
@@ -1326,5 +1348,6 @@ initFrame();
 
 defineExpose({
   refresh: onRefresh,
+  focus,
 });
 </script>
