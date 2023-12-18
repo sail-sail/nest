7980bdb037ae7e0703da8766bed7ba7b74fd8194
Sail <151263555@qq.com>
2023-12-18 14:37:45 +0800

    selectInput focus

--- pc/src/views/base/field_permit/List.vue
+++ pc/src/views/base/field_permit/List.vue
@@ -720,6 +720,7 @@ const props = defineProps<{
   showBuildIn?: string;
   isPagination?: string;
   isLocked?: string;
+  isFocus?: string;
   ids?: string[]; //ids
   selectedIds?: FieldPermitId[]; //已选择行的id列表
   isMultiple?: Boolean; //是否多选
@@ -740,6 +741,7 @@ const builtInSearchType: { [key: string]: string } = {
   showBuildIn: "0|1",
   isPagination: "0|1",
   isLocked: "0|1",
+  isFocus: "0|1",
   ids: "string[]",
   menu_id: "string[]",
   menu_id_lbl: "string[]",
@@ -757,6 +759,7 @@ const propsNotInSearch: string[] = [
   "showBuildIn",
   "isPagination",
   "isLocked",
+  "isFocus",
 ];
 
 /** 内置搜索条件 */
@@ -781,6 +784,8 @@ const showBuildIn = $computed(() => props.showBuildIn === "1");
 const isPagination = $computed(() => !props.isPagination || props.isPagination === "1");
 /** 是否只读模式 */
 const isLocked = $computed(() => props.isLocked === "1");
+/** 是否 focus, 默认为 true */
+const isFocus = $computed(() => props.isFocus !== "0");
 
 /** 分页功能 */
 let {
@@ -1471,10 +1476,27 @@ async function initI18nsEfc() {
   ]);
 }
 
-async function initFrame() {
-  if (!usrStore.authorization) {
+async function focus() {
+  if (!inited || !tableRef || !tableRef.$el) {
     return;
   }
+  tableRef.$el.focus();
+}
+
+watch(
+  () => [
+    props.isFocus,
+    inited,
+  ],
+  () => {
+    if (!inited || !isFocus || !tableRef || !tableRef.$el) {
+      return;
+    }
+    tableRef.$el.focus();
+  },
+);
+
+async function initFrame() {
   await Promise.all([
     initI18nsEfc(),
     dataGrid(true),
@@ -1507,5 +1529,6 @@ initFrame();
 
 defineExpose({
   refresh: onRefresh,
+  focus,
 });
 </script>
