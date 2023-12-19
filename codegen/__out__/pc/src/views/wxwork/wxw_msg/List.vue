80e8ba832572528c7947a253f287e8437e265672
Sail <151263555@qq.com>
2023-12-19 12:11:47 +0800

    row-dblclick: SelectInput 弹窗选择时, 双击行变为选中, 回车变为选中

--- codegen/__out__/pc/src/views/wxwork/wxw_msg/List.vue
+++ codegen/__out__/pc/src/views/wxwork/wxw_msg/List.vue
@@ -389,7 +389,7 @@
         @row-click="onRow"
         @sort-change="onSortChange"
         @header-dragend="headerDragend"
-        @row-dblclick="openView"
+        @row-dblclick="onRowDblclick"
         @keydown.escape="onEmptySelected"
         @keydown.delete="onDeleteByIds"
         @keydown.enter="onRowEnter"
@@ -582,26 +582,15 @@ const permit = permitStore.getPermit("/wxwork/wxw_msg");
 let inited = $ref(false);
 
 const emit = defineEmits<{
-  selectedIdsChg: [
-    WxwMsgId[],
-  ],
-  add: [
-    WxwMsgId[],
-  ],
-  edit: [
-    WxwMsgId[],
-  ],
-  remove: [
-    number,
-  ],
-  revert: [
-    number,
-  ],
+  selectedIdsChg: [ WxwMsgId[] ],
+  add: [ WxwMsgId[] ],
+  edit: [ WxwMsgId[] ],
+  remove: [ number ],
+  revert: [ number ],
   refresh: [ ],
   beforeSearchReset: [ ],
-  rowEnter: [
-    KeyboardEvent,
-  ],
+  rowEnter: [ KeyboardEvent? ],
+  rowDblclick: [ WxwMsgModel ],
 }>();
 
 /** 表格 */
@@ -1050,6 +1039,17 @@ async function onRowEnter(e: KeyboardEvent) {
   } else {
     await openView();
   }
+}
+
+/** 双击行 */
+async function onRowDblclick(
+  row: WxwMsgModel,
+) {
+  if (props.selectedIds != null) {
+    emit("rowDblclick", row);
+    return;
+  }
+  await openView();
 }
 
 /** 打开查看 */
