31c66c9b4159762f2d7d0e13e0fd9a15611fecf1
Sail <151263555@qq.com>
2024-01-15 16:54:38 +0800

    onInsert

--- pc/src/views/wx/wx_pay_notice/Detail.vue
+++ pc/src/views/wx/wx_pay_notice/Detail.vue
@@ -4,10 +4,8 @@
   :before-close="beforeClose"
   @keydown.page-down="onPageDown"
   @keydown.page-up="onPageUp"
-  @keydown.insert="onInsert"
   @keydown.ctrl.arrow-down="onPageDown"
   @keydown.ctrl.arrow-up="onPageUp"
-  @keydown.ctrl.i="onInsert"
 >
   <template #extra_header>
     <div
@@ -417,7 +415,6 @@ const {
   initSysI18ns,
 } = useI18n("/wx/wx_pay_notice");
 
-const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 
 const permit = permitStore.getPermit("/wx/wx_pay_notice");
@@ -581,13 +578,6 @@ async function showDialog(
   }
   inited = true;
   return await dialogRes.dialogPrm;
-}
-
-/** 键盘按 Insert */
-async function onInsert() {
-  isReadonly = !isReadonly;
-  await nextTick();
-  customDialogRef?.focus();
 }
 
 /** 重置 */
