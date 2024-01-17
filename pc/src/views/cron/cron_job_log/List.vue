b93929e9b1de86ae637a259bfb081af79a02fe1c
Sail <151263555@qq.com>
2024-01-17 17:46:17 +0800

    onDirty

--- pc/src/views/cron/cron_job_log/List.vue
+++ pc/src/views/cron/cron_job_log/List.vue
@@ -503,7 +503,7 @@ const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 const dirtyStore = useDirtyStore();
 
-const clearDirty = dirtyStore.onDirty(onRefresh);
+const clearDirty = dirtyStore.onDirty(onRefresh, pageName);
 
 const permit = permitStore.getPermit("/cron/cron_job_log");
 
