dd1e117ce694ec86f4808ff608cd17cd89f1e5e3
Sail <151263555@qq.com>
2024-01-17 17:48:05 +0800

    onDirty

--- pc/src/views/wxwork/wxw_msg/List.vue
+++ pc/src/views/wxwork/wxw_msg/List.vue
@@ -576,7 +576,7 @@ const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 const dirtyStore = useDirtyStore();
 
-const clearDirty = dirtyStore.onDirty(onRefresh);
+const clearDirty = dirtyStore.onDirty(onRefresh, pageName);
 
 const permit = permitStore.getPermit("/wxwork/wxw_msg");
 
