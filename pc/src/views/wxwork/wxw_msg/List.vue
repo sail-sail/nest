c66719ccc3e5cd39ba2cefffc184fef112e7aca6
Sail <151263555@qq.com>
2024-01-18 08:49:26 +0800

    tableSelectable

--- pc/src/views/wxwork/wxw_msg/List.vue
+++ pc/src/views/wxwork/wxw_msg/List.vue
@@ -609,12 +609,14 @@ let search = $ref(initSearch());
 
 /** 回收站 */
 async function recycleChg() {
+  tableFocus();
   selectedIds = [ ];
   await dataGrid(true);
 }
 
 /** 搜索 */
 async function onSearch() {
+  tableFocus();
   await dataGrid(true);
 }
 
@@ -629,6 +631,7 @@ let isSearchReset = $ref(false);
 
 /** 重置搜索 */
 async function onSearchReset() {
+  tableFocus();
   isSearchReset = true;
   search = initSearch();
   idsChecked = 0;
@@ -641,11 +644,13 @@ async function onSearchReset() {
 
 /** 清空搜索框事件 */
 async function onSearchClear() {
+  tableFocus();
   await dataGrid(true);
 }
 
 /** 点击已选择 */
 async function onIdsChecked() {
+  tableFocus();
   await dataGrid(true);
 }
 
@@ -775,6 +780,7 @@ function resetSelectedIds() {
 
 /** 取消已选择筛选 */
 async function onEmptySelected() {
+  tableFocus();
   resetSelectedIds();
   if (idsChecked === 1) {
     idsChecked = 0;
@@ -1056,6 +1062,7 @@ async function onRowDblclick(
 
 /** 打开查看 */
 async function openView() {
+  tableFocus();
   if (!detailRef) {
     return;
   }
@@ -1122,6 +1129,7 @@ async function onDeleteByIds() {
 
 /** 点击彻底删除 */
 async function onForceDeleteByIds() {
+  tableFocus();
   if (isLocked) {
     return;
   }
