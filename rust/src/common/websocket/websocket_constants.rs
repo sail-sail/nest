1d3c7790ccd23f493991d2358bc0276ae5ffa351
Sail <151263555@qq.com>
2024-01-30 16:17:13 +0800

    websocket

---
+++ rust/src/common/websocket/websocket_constants.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+use futures_util::stream::SplitSink;
+use tokio::sync::Mutex;
+use lazy_static::lazy_static;
+use std::sync::Arc;
+
+use poem::web::websocket::{Message, WebSocketStream};
+
+type Callback = Box<dyn Fn(String) + Send + Sync>;
+
+lazy_static! {
+  pub static ref CALLBACKS_MAP: Mutex<HashMap<String, Vec<Callback>>> = Mutex::new(HashMap::new());
+  pub static ref SOCKET_SINK_MAP: Mutex<HashMap<String, Arc<Mutex<SplitSink<WebSocketStream, Message>>>>> = Mutex::new(HashMap::new());
+  pub static ref CLIENT_ID_TOPICS_MAP: Mutex<HashMap<String, Vec<String>>> = Mutex::new(HashMap::new());
+}
