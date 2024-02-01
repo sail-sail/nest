514dfd9a2720e54537e78ab4cd405160617e6315
Sail <151263555@qq.com>
2024-02-01 14:55:01 +0800

    findOneModel

--- codegen/__out__/pc/src/views/cron/job/Detail.vue
+++ codegen/__out__/pc/src/views/cron/job/Detail.vue
@@ -358,6 +358,8 @@ let readonlyWatchStop: WatchStopHandle | undefined = undefined;
 
 let customDialogRef = $ref<InstanceType<typeof CustomDialog>>();
 
+let findOneModel = findOne;
+
 /** 打开对话框 */
 async function showDialog(
   arg?: {
@@ -371,6 +373,7 @@ async function showDialog(
       ids?: JobId[];
       is_deleted?: number | null;
     };
+    findOne?: typeof findOne;
     action: DialogAction;
   },
 ) {
@@ -391,6 +394,9 @@ async function showDialog(
   isReadonly = false;
   isLocked = false;
   is_deleted = model?.is_deleted ?? 0;
+  if (arg?.findOne) {
+    findOneModel = arg.findOne;
+  }
   if (readonlyWatchStop) {
     readonlyWatchStop();
   }
@@ -438,7 +444,7 @@ async function showDialog(
       data,
       order_by,
     ] = await Promise.all([
-      findOne({
+      findOneModel({
         id: model.id,
         is_deleted,
       }),
@@ -547,7 +553,7 @@ async function onRefresh() {
   if (!dialogModel.id) {
     return;
   }
-  const data = await findOne({
+  const data = await findOneModel({
     id: dialogModel.id,
     is_deleted,
   });
@@ -748,7 +754,7 @@ async function onSaveAndCopy() {
     data,
     order_by,
   ] = await Promise.all([
-    findOne({
+    findOneModel({
       id,
       is_deleted,
     }),
