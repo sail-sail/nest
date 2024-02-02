dfae09cc439460134ed3fbc40c6ae1af8f677387
Sail <151263555@qq.com>
2024-02-02 18:08:02 +0800

    oldIsLocked

--- codegen/__out__/pc/src/views/cron/cron_job/Detail.vue
+++ codegen/__out__/pc/src/views/cron/cron_job/Detail.vue
@@ -334,6 +334,7 @@ let dialogAction = $ref<DialogAction>("add");
 let dialogTitle = $ref("");
 let oldDialogTitle = "";
 let oldDialogNotice: string | undefined = undefined;
+let oldIsLocked = $ref(false);
 let dialogNotice = $ref("");
 
 let dialogModel: CronJobInput = $ref({
@@ -477,6 +478,7 @@ async function showDialog(
   readonlyWatchStop = watchEffect(function() {
     showBuildIn = toValue(arg?.showBuildIn) ?? showBuildIn;
     isReadonly = toValue(arg?.isReadonly) ?? isReadonly;
+    oldIsLocked = toValue(arg?.isLocked) ?? false;
     
     if (dialogAction === "add") {
       isLocked = false;
