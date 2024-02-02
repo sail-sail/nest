be2d17f2e44dfeff43072bcce24a69091af59aa3
Sail <151263555@qq.com>
2024-02-02 18:08:23 +0800

    oldIsLocked

--- pc/src/views/wxwork/wxw_msg/Detail.vue
+++ pc/src/views/wxwork/wxw_msg/Detail.vue
@@ -286,6 +286,7 @@ let dialogAction = $ref<DialogAction>("add");
 let dialogTitle = $ref("");
 let oldDialogTitle = "";
 let oldDialogNotice: string | undefined = undefined;
+let oldIsLocked = $ref(false);
 let dialogNotice = $ref("");
 
 let dialogModel: WxwMsgInput = $ref({
@@ -384,6 +385,7 @@ async function showDialog(
   readonlyWatchStop = watchEffect(function() {
     showBuildIn = toValue(arg?.showBuildIn) ?? showBuildIn;
     isReadonly = toValue(arg?.isReadonly) ?? isReadonly;
+    oldIsLocked = toValue(arg?.isLocked) ?? false;
     
     if (!permit("edit")) {
       isLocked = true;
