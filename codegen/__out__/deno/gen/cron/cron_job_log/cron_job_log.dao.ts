4f17d04258a7bff66a876f710f3d3ee190ab5a3a
Sail <151263555@qq.com>
2024-02-21 11:17:32 +0800

    此 {0} 已经存在

--- codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.dao.ts
+++ codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.dao.ts
@@ -520,7 +520,7 @@ export async function checkByUnique(
   const isEquals = equalsByUnique(oldModel, input);
   if (isEquals) {
     if (uniqueType === UniqueType.Throw) {
-      throw new UniqueException(await ns("数据已经存在"));
+      throw new UniqueException(await ns("此 {0} 已经存在", await ns("任务执行日志")));
     }
     if (uniqueType === UniqueType.Update) {
       const id: CronJobLogId = await updateById(
@@ -1088,7 +1088,7 @@ export async function revertByIds(
       let models = await findByUnique(input);
       models = models.filter((item) => item.id !== id);
       if (models.length > 0) {
-        throw await ns("数据已经存在");
+        throw await ns("此 {0} 已经存在", await ns("任务执行日志"));
       }
     }
   }
