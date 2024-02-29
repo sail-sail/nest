bb1553e9b85ac284980a67931af5555067c31586
Sail <151263555@qq.com>
2024-02-29 20:56:05 +0800

    onRowDblclick 双击行

--- codegen/__out__/pc/src/views/cron/cron_job/List.vue
+++ codegen/__out__/pc/src/views/cron/cron_job/List.vue
@@ -1570,7 +1570,11 @@ async function onRowEnter(e: KeyboardEvent) {
 /** 双击行 */
 async function onRowDblclick(
   row: CronJobModel,
+  column: TableColumnCtx<CronJobModel>,
 ) {
+  if (column.type === "selection") {
+    return;
+  }
   if (props.selectedIds != null && !isLocked) {
     emit("rowDblclick", row);
     return;
