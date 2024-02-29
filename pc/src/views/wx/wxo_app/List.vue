84fbbd1e0ba60a0c90382cf3088568e05bbc059b
Sail <151263555@qq.com>
2024-02-29 17:39:56 +0800

    isListSelectDialog

--- pc/src/views/wx/wxo_app/List.vue
+++ pc/src/views/wx/wxo_app/List.vue
@@ -795,6 +795,7 @@ const props = defineProps<{
   isLocked?: string;
   isFocus?: string;
   propsNotReset?: string[];
+  isListSelectDialog?: string;
   ids?: string[]; //ids
   selectedIds?: WxoAppId[]; //已选择行的id列表
   isMultiple?: Boolean; //是否多选
@@ -820,6 +821,7 @@ const builtInSearchType: { [key: string]: string } = {
   isPagination: "0|1",
   isLocked: "0|1",
   isFocus: "0|1",
+  isListSelectDialog: "0|1",
   ids: "string[]",
   domain_id: "string[]",
   domain_id_lbl: "string[]",
@@ -842,6 +844,7 @@ const propsNotInSearch: string[] = [
   "isLocked",
   "isFocus",
   "propsNotReset",
+  "isListSelectDialog",
 ];
 
 /** 内置查询条件 */
@@ -868,6 +871,7 @@ const isPagination = $computed(() => !props.isPagination || props.isPagination =
 const isLocked = $computed(() => props.isLocked === "1");
 /** 是否 focus, 默认为 true */
 const isFocus = $computed(() => props.isFocus !== "0");
+const isListSelectDialog = $computed(() => props.isListSelectDialog === "1");
 
 /** 表格 */
 let tableRef = $ref<InstanceType<typeof ElTable>>();
