cb7a319c4c65d90be23ad5e716d6b527989f596e
Sail <151263555@qq.com>
2024-03-12 14:56:53 +0800

    database_debug_sql

--- deno/gen/wxwork/wxw_app_token/wxw_app_token.dao.ts
+++ deno/gen/wxwork/wxw_app_token/wxw_app_token.dao.ts
@@ -153,11 +153,9 @@ async function getFromQuery(
   },
 ) {
   const is_deleted = search?.is_deleted ?? 0;
-  let fromQuery = `
-    wxwork_wxw_app_token t
+  let fromQuery = `wxwork_wxw_app_token t
     left join wxwork_wxw_app wxw_app_id_lbl
-      on wxw_app_id_lbl.id = t.wxw_app_id
-  `;
+      on wxw_app_id_lbl.id = t.wxw_app_id`;
   return fromQuery;
 }
 
@@ -174,6 +172,15 @@ export async function findCount(
   const table = "wxwork_wxw_app_token";
   const method = "findCount";
   
+  let msg = `${ table }.${ method }: `;
+  if (search && Object.keys(search).length > 0) {
+    msg += `search:${ JSON.stringify(search) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
+  
   const args = new QueryArgs();
   let sql = `
     select
@@ -183,19 +190,12 @@ export async function findCount(
         select
           1
         from
-          ${ await getFromQuery(args, search, options) }
-  `;
+          ${ await getFromQuery(args, search, options) }`;
   const whereQuery = await getWhereQuery(args, search, options);
   if (isNotEmpty(whereQuery)) {
-    sql += `
-        where
-          ${ whereQuery }
-    `;
+    sql += ` where ${ whereQuery }`;
   }
-  sql += `
-        group by t.id
-      ) t
-  `;
+  sql += ` group by t.id) t`;
   
   const cacheKey1 = `dao.sql.${ table }`;
   const cacheKey2 = await hash(JSON.stringify({ sql, args }));
@@ -224,6 +224,21 @@ export async function findAll(
   const table = "wxwork_wxw_app_token";
   const method = "findAll";
   
+  let msg = `${ table }.${ method }: `;
+  if (search && Object.keys(search).length > 0) {
+    msg += `search:${ JSON.stringify(search) } `;
+  }
+  if (page && Object.keys(page).length > 0) {
+    msg += `page:${ JSON.stringify(page) } `;
+  }
+  if (sort && Object.keys(sort).length > 0) {
+    msg += `sort:${ JSON.stringify(sort) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
+  
   const args = new QueryArgs();
   let sql = `
     select t.*
@@ -233,14 +248,9 @@ export async function findAll(
   `;
   const whereQuery = await getWhereQuery(args, search, options);
   if (isNotEmpty(whereQuery)) {
-    sql += `
-    where
-      ${ whereQuery }
-    `;
+    sql += ` where ${ whereQuery }`;
   }
-  sql += `
-    group by t.id
-  `;
+  sql += ` group by t.id`;
   
   // 排序
   if (!sort) {
@@ -545,6 +555,12 @@ export async function existById(
   const table = "wxwork_wxw_app_token";
   const method = "existById";
   
+  let msg = `${ table }.${ method }: `;
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
+  
   if (isEmpty(id as unknown as string)) {
     return false;
   }
@@ -645,6 +661,15 @@ export async function create(
 ): Promise<WxwAppTokenId> {
   const table = "wxwork_wxw_app_token";
   const method = "create";
+  
+  let msg = `${ table }.${ method }: `;
+  if (input) {
+    msg += `input:${ JSON.stringify(input) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
   
   if (input.id) {
     throw new Error(`Can not set id when create in dao: ${ table }`);
@@ -814,6 +839,18 @@ export async function updateTenantById(
   const table = "wxwork_wxw_app_token";
   const method = "updateTenantById";
   
+  let msg = `${ table }.${ method }: `;
+  if (id) {
+    msg += `id:${ id } `;
+  }
+  if (tenant_id) {
+    msg += `tenant_id:${ tenant_id } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
+  
   const tenantExist = await existByIdTenant(tenant_id);
   if (!tenantExist) {
     return 0;
@@ -857,6 +894,18 @@ export async function updateById(
 ): Promise<WxwAppTokenId> {
   const table = "wxwork_wxw_app_token";
   const method = "updateById";
+  
+  let msg = `${ table }.${ method }: `;
+  if (id) {
+    msg += `id:${ id } `;
+  }
+  if (input) {
+    msg += `input:${ JSON.stringify(input) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
   
   if (!id) {
     throw new Error("updateById: id cannot be empty");
@@ -973,6 +1022,15 @@ export async function deleteByIds(
   const table = "wxwork_wxw_app_token";
   const method = "deleteByIds";
   
+  let msg = `${ table }.${ method }: `;
+  if (ids) {
+    msg += `ids:${ JSON.stringify(ids) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
+  
   if (!ids || !ids.length) {
     return 0;
   }
@@ -1020,6 +1078,15 @@ export async function revertByIds(
 ): Promise<number> {
   const table = "wxwork_wxw_app_token";
   const method = "revertByIds";
+  
+  let msg = `${ table }.${ method }: `;
+  if (ids) {
+    msg += `ids:${ JSON.stringify(ids) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
   
   if (!ids || !ids.length) {
     return 0;
@@ -1079,6 +1146,15 @@ export async function forceDeleteByIds(
 ): Promise<number> {
   const table = "wxwork_wxw_app_token";
   const method = "forceDeleteByIds";
+  
+  let msg = `${ table }.${ method }: `;
+  if (ids) {
+    msg += `ids:${ JSON.stringify(ids) } `;
+  }
+  if (options && Object.keys(options).length > 0){
+    msg += `options:${ JSON.stringify(options) } `;
+  }
+  log(msg);
   
   if (!ids || !ids.length) {
     return 0;
