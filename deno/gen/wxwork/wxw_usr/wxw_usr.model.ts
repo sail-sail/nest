8bd6108ffb78d6e83a167f3f738094da91663541
Sail <151263555@qq.com>
2024-03-15 10:27:46 +0800

    hasCreateUsrId

--- deno/gen/wxwork/wxw_usr/wxw_usr.model.ts
+++ deno/gen/wxwork/wxw_usr/wxw_usr.model.ts
@@ -73,6 +73,7 @@ export interface WxwUsrModel extends WxwUsrModelType {
   create_time?: string | null;
   create_time_lbl: string;
   update_usr_id: UsrId;
+  update_usr_id_lbl: string;
   update_time?: string | null;
   update_time_lbl: string;
   tenant_id: TenantId;
