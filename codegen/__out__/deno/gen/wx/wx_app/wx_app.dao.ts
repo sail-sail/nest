233bd2aabf97a453fea041d004026b84c2727488
Sail <151263555@qq.com>
2024-03-27 16:30:59 +0800

    findAll 分页的问题

--- codegen/__out__/deno/gen/wx/wx_app/wx_app.dao.ts
+++ codegen/__out__/deno/gen/wx/wx_app/wx_app.dao.ts
@@ -324,6 +324,7 @@ export async function findAll(
   
   const args = new QueryArgs();
   let sql = `
+    select f.* from (
     select t.*
       ,create_usr_id_lbl.lbl create_usr_id_lbl
       ,update_usr_id_lbl.lbl update_usr_id_lbl
@@ -352,10 +353,12 @@ export async function findAll(
     prop: "order_by",
     order: SortOrderEnum.Asc,
   });
-  sort.push({
-    prop: "create_time",
-    order: SortOrderEnum.Desc,
-  });
+  if (!sort.some((item) => item.prop === "create_time")) {
+    sort.push({
+      prop: "create_time",
+      order: SortOrderEnum.Desc,
+    });
+  }
   for (let i = 0; i < sort.length; i++) {
     const item = sort[i];
     if (i === 0) {
@@ -365,6 +368,7 @@ export async function findAll(
     }
     sql += ` ${ escapeId(item.prop) } ${ escapeDec(item.order) }`;
   }
+  sql += `) f`;
   
   // 分页
   if (page?.pgSize) {
--- deno/gen/wx/wx_pay_notice/wx_pay_notice.dao.ts
+++ deno/gen/wx/wx_pay_notice/wx_pay_notice.dao.ts
@@ -413,6 +413,7 @@ export async function findAll(
   
   const args = new QueryArgs();
   let sql = `
+    select f.* from (
     select t.*
       ,create_usr_id_lbl.lbl create_usr_id_lbl
       ,update_usr_id_lbl.lbl update_usr_id_lbl
@@ -441,10 +442,12 @@ export async function findAll(
     prop: "transaction_id",
     order: SortOrderEnum.Desc,
   });
-  sort.push({
-    prop: "create_time",
-    order: SortOrderEnum.Desc,
-  });
+  if (!sort.some((item) => item.prop === "create_time")) {
+    sort.push({
+      prop: "create_time",
+      order: SortOrderEnum.Desc,
+    });
+  }
   for (let i = 0; i < sort.length; i++) {
     const item = sort[i];
     if (i === 0) {
@@ -454,6 +457,7 @@ export async function findAll(
     }
     sql += ` ${ escapeId(item.prop) } ${ escapeDec(item.order) }`;
   }
+  sql += `) f`;
   
   // 分页
   if (page?.pgSize) {
