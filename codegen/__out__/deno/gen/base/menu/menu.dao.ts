a6a6bc1c80a65294ae16a52e1e9000be98817414
Sail <151263555@qq.com>
2024-04-02 11:17:23 +0800

    操作人, 操作时间

--- codegen/__out__/deno/gen/base/menu/menu.dao.ts
+++ codegen/__out__/deno/gen/base/menu/menu.dao.ts
@@ -1057,7 +1057,6 @@ export async function updateById(
   const table = "base_menu";
   const method = "updateById";
   
-  
   if (options?.debug !== false) {
     let msg = `${ table }.${ method }:`;
     if (id) {
