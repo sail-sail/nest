bc42908adb159a94cfb1c0d9056385ccbd373cb3
Sail <151263555@qq.com>
2024-04-02 10:28:43 +0800

    encrypt Decimal

--- codegen/__out__/rust/src/gen/wxwork/wxw_usr/wxw_usr_model.rs
+++ codegen/__out__/rust/src/gen/wxwork/wxw_usr/wxw_usr_model.rs
@@ -320,7 +320,7 @@ pub struct WxwUsrSearch {
   pub create_usr_id_is_null: Option<bool>,
   /// 创建时间
   #[graphql(skip)]
-  pub create_time: Option<Vec<chrono::NaiveDateTime>>,
+  pub create_time: Option<Vec<Option<chrono::NaiveDateTime>>>,
   /// 更新人
   #[graphql(skip)]
   pub update_usr_id: Option<Vec<UsrId>>,
@@ -329,7 +329,7 @@ pub struct WxwUsrSearch {
   pub update_usr_id_is_null: Option<bool>,
   /// 更新时间
   #[graphql(skip)]
-  pub update_time: Option<Vec<chrono::NaiveDateTime>>,
+  pub update_time: Option<Vec<Option<chrono::NaiveDateTime>>>,
 }
 
 impl std::fmt::Debug for WxwUsrSearch {
@@ -609,11 +609,11 @@ impl From<WxwUsrInput> for WxwUsrSearch {
       // 创建人
       create_usr_id: input.create_usr_id.map(|x| vec![x]),
       // 创建时间
-      create_time: input.create_time.map(|x| vec![x, x]),
+      create_time: input.create_time.map(|x| vec![Some(x), Some(x)]),
       // 更新人
       update_usr_id: input.update_usr_id.map(|x| vec![x]),
       // 更新时间
-      update_time: input.update_time.map(|x| vec![x, x]),
+      update_time: input.update_time.map(|x| vec![Some(x), Some(x)]),
       ..Default::default()
     }
   }
@@ -717,5 +717,11 @@ impl<'r> sqlx::Decode<'r, MySql> for WxwUsrId {
     value: <MySql as sqlx::database::HasValueRef>::ValueRef,
   ) -> Result<Self, sqlx::error::BoxDynError> {
     <&str as sqlx::Decode<MySql>>::decode(value).map(Self::from)
+  }
+}
+
+impl PartialEq<str> for WxwUsrId {
+  fn eq(&self, other: &str) -> bool {
+    self.0 == other
   }
 }
