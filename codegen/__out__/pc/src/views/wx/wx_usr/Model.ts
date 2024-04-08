0a8fcd8f00d6907fd615c9c3cf4ba82701efec30
Sail <151263555@qq.com>
2024-04-08 09:03:33 +0800

    QueryField

--- codegen/__out__/pc/src/views/wx/wx_usr/Model.ts
+++ codegen/__out__/pc/src/views/wx/wx_usr/Model.ts
@@ -16,3 +16,53 @@ export interface WxUsrSearch extends WxUsrSearchType {
 
 export interface WxUsrFieldComment extends WxUsrFieldCommentType {
 }
+
+export const wxUsrFields = [
+  // ID
+  "id",
+  // 名称
+  "lbl",
+  // 用户
+  "usr_id",
+  "usr_id_lbl",
+  // 昵称
+  "nick_name",
+  // 头像
+  "avatar_url",
+  // 手机
+  "mobile",
+  // 小程序用户唯一标识
+  "openid",
+  // 小程序用户统一标识
+  "unionid",
+  // 性别
+  "gender",
+  "gender_lbl",
+  // 城市
+  "city",
+  // 省份
+  "province",
+  // 国家
+  "country",
+  // 语言
+  "language",
+  // 备注
+  "rem",
+  // 创建人
+  "create_usr_id",
+  "create_usr_id_lbl",
+  // 创建时间
+  "create_time",
+  "create_time_lbl",
+  // 更新人
+  "update_usr_id",
+  "update_usr_id_lbl",
+  // 更新时间
+  "update_time",
+  "update_time_lbl",
+  "is_deleted",
+];
+
+export const wxUsrQueryField = `
+  ${ wxUsrFields.join(" ") }
+`;
--- pc/src/views/wx/wxo_usr/Model.ts
+++ pc/src/views/wx/wxo_usr/Model.ts
@@ -16,3 +16,36 @@ export interface WxoUsrSearch extends WxoUsrSearchType {
 
 export interface WxoUsrFieldComment extends WxoUsrFieldCommentType {
 }
+
+export const wxoUsrFields = [
+  // ID
+  "id",
+  // 名称
+  "lbl",
+  // 用户
+  "usr_id",
+  "usr_id_lbl",
+  // 公众号用户唯一标识
+  "openid",
+  // 公众号用户统一标识
+  "unionid",
+  // 备注
+  "rem",
+  // 创建人
+  "create_usr_id",
+  "create_usr_id_lbl",
+  // 创建时间
+  "create_time",
+  "create_time_lbl",
+  // 更新人
+  "update_usr_id",
+  "update_usr_id_lbl",
+  // 更新时间
+  "update_time",
+  "update_time_lbl",
+  "is_deleted",
+];
+
+export const wxoUsrQueryField = `
+  ${ wxoUsrFields.join(" ") }
+`;
