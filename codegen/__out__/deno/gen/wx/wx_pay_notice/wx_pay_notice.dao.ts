d5b8afbb7125a0d561e8f49c616ffa38b2ec964c
Sail <151263555@qq.com>
2024-05-14 13:30:04 +0800

    <#=Table_Up#>Input

--- codegen/__out__/deno/gen/wx/wx_pay_notice/wx_pay_notice.dao.ts
+++ codegen/__out__/deno/gen/wx/wx_pay_notice/wx_pay_notice.dao.ts
@@ -1773,7 +1773,7 @@ export async function revertByIds(
       const input = {
         ...old_model,
         id: undefined,
-      };
+      } as WxPayNoticeInput;
       let models = await findByUnique(input);
       models = models.filter((item) => item.id !== id);
       if (models.length > 0) {
