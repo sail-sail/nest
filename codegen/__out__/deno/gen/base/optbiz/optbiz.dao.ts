5ab7df5e961812334e1e9f4c3c987ee8f32cc1e3
Sail <151263555@qq.com>
2024-05-14 13:26:51 +0800

    <#=Table_Up#>Input

--- codegen/__out__/deno/gen/base/optbiz/optbiz.dao.ts
+++ codegen/__out__/deno/gen/base/optbiz/optbiz.dao.ts
@@ -1558,7 +1558,7 @@ export async function revertByIds(
       const input = {
         ...old_model,
         id: undefined,
-      };
+      } as OptbizInput;
       let models = await findByUnique(input);
       models = models.filter((item) => item.id !== id);
       if (models.length > 0) {
