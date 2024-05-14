0ab85c3cd3ed74affe60ee0aaf49340e4d410fee
Sail <151263555@qq.com>
2024-05-14 13:31:32 +0800

    <#=Table_Up#>Input

--- deno/gen/cron/cron_job_log/cron_job_log.dao.ts
+++ deno/gen/cron/cron_job_log/cron_job_log.dao.ts
@@ -1334,7 +1334,7 @@ export async function revertByIds(
       const input = {
         ...old_model,
         id: undefined,
-      };
+      } as CronJobLogInput;
       let models = await findByUnique(input);
       models = models.filter((item) => item.id !== id);
       if (models.length > 0) {
