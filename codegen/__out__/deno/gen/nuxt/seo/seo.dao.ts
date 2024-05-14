bcdba65110995c5e0c5a3d95f4b11f5972454729
Sail <151263555@qq.com>
2024-05-14 13:32:23 +0800

    <#=Table_Up#>Input

--- codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
+++ codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
@@ -1539,7 +1539,7 @@ export async function revertByIds(
       const input = {
         ...old_model,
         id: undefined,
-      };
+      } as SeoInput;
       let models = await findByUnique(input);
       models = models.filter((item) => item.id !== id);
       if (models.length > 0) {
