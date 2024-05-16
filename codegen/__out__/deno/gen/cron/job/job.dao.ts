71d52360933026b92f44bfca8f03638df1264f2a
Sail <151263555@qq.com>
2024-05-16 11:08:14 +0800

    dao findAll id 为空字符串的问题

--- codegen/__out__/deno/gen/cron/job/job.dao.ts
+++ codegen/__out__/deno/gen/cron/job/job.dao.ts
@@ -269,9 +269,6 @@ export async function findAll(
     log(msg);
   }
   
-  if (search?.id === "") {
-    return [ ];
-  }
   if (search?.ids?.length === 0) {
     return [ ];
   }
@@ -654,9 +651,6 @@ export async function findOne(
     options.debug = false;
   }
   
-  if (search?.id === "") {
-    return;
-  }
   if (search?.ids?.length === 0) {
     return;
   }
@@ -693,10 +687,19 @@ export async function findById(
     options = options || { };
     options.debug = false;
   }
-  if (isEmpty(id as unknown as string)) {
+  
+  if (id == null) {
     return;
   }
-  const model = await findOne({ id }, undefined, options);
+  
+  const model = await findOne(
+    {
+      id,
+    },
+    undefined,
+    options,
+  );
+  
   return model;
 }
 
@@ -750,7 +753,7 @@ export async function existById(
     log(msg);
   }
   
-  if (isEmpty(id as unknown as string)) {
+  if (id == null) {
     return false;
   }
   
@@ -883,7 +886,9 @@ export async function create(
     throw new Error(`input is required in dao: ${ table }`);
   }
   
-  const [ id ] = await _creates([ input ], options);
+  const [
+    id,
+  ] = await _creates([ input ], options);
   
   return id;
 }
@@ -1122,15 +1127,7 @@ export async function updateTenantById(
   }
   
   const args = new QueryArgs();
-  const sql = `
-    update
-      cron_job
-    set
-      update_time = ${ args.push(reqDate()) },
-      tenant_id = ${ args.push(tenant_id) }
-    where
-      id = ${ args.push(id) }
-  `;
+  const sql = `update cron_job set tenant_id=${ args.push(tenant_id) } where id=${ args.push(id) }`;
   const result = await execute(sql, args);
   const num = result.affectedRows;
   
@@ -1606,18 +1603,14 @@ export async function findLastOrderBy(
     log(msg);
   }
   
-  let sql = `
-    select
-      t.order_by order_by
-    from
-      cron_job t`;
+  let sql = `select t.order_by order_by from cron_job t`;
   const whereQuery: string[] = [ ];
   const args = new QueryArgs();
-  whereQuery.push(`t.is_deleted = 0`);
+  whereQuery.push(` t.is_deleted=0`);
   {
     const authModel = await getAuthModel();
     const tenant_id = await getTenant_id(authModel?.id);
-    whereQuery.push(`t.tenant_id = ${ args.push(tenant_id) }`);
+    whereQuery.push(` t.tenant_id=${ args.push(tenant_id) }`);
   }
   if (whereQuery.length > 0) {
     sql += " where " + whereQuery.join(" and ");
--- deno/gen/cron/cron_job_log/cron_job_log.dao.ts
+++ deno/gen/cron/cron_job_log/cron_job_log.dao.ts
@@ -275,9 +275,6 @@ export async function findAll(
     log(msg);
   }
   
-  if (search?.id === "") {
-    return [ ];
-  }
   if (search?.ids?.length === 0) {
     return [ ];
   }
@@ -662,9 +659,6 @@ export async function findOne(
     options.debug = false;
   }
   
-  if (search?.id === "") {
-    return;
-  }
   if (search?.ids?.length === 0) {
     return;
   }
@@ -701,10 +695,19 @@ export async function findById(
     options = options || { };
     options.debug = false;
   }
-  if (isEmpty(id as unknown as string)) {
+  
+  if (id == null) {
     return;
   }
-  const model = await findOne({ id }, undefined, options);
+  
+  const model = await findOne(
+    {
+      id,
+    },
+    undefined,
+    options,
+  );
+  
   return model;
 }
 
@@ -758,7 +761,7 @@ export async function existById(
     log(msg);
   }
   
-  if (isEmpty(id as unknown as string)) {
+  if (id == null) {
     return false;
   }
   
@@ -865,7 +868,9 @@ export async function create(
     throw new Error(`input is required in dao: ${ table }`);
   }
   
-  const [ id ] = await _creates([ input ], options);
+  const [
+    id,
+  ] = await _creates([ input ], options);
   
   return id;
 }
@@ -1088,15 +1093,7 @@ export async function updateTenantById(
   }
   
   const args = new QueryArgs();
-  const sql = `
-    update
-      cron_cron_job_log
-    set
-      update_time = ${ args.push(reqDate()) },
-      tenant_id = ${ args.push(tenant_id) }
-    where
-      id = ${ args.push(id) }
-  `;
+  const sql = `update cron_cron_job_log set tenant_id=${ args.push(tenant_id) } where id=${ args.push(id) }`;
   const result = await execute(sql, args);
   const num = result.affectedRows;
   return num;
