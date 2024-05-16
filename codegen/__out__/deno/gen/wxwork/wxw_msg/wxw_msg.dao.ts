36c9a5a61d87af5c649b33a7ddba0ff2e64163b0
Sail <151263555@qq.com>
2024-05-16 11:06:43 +0800

    dao findAll id 为空字符串的问题

--- codegen/__out__/deno/gen/wxwork/wxw_msg/wxw_msg.dao.ts
+++ codegen/__out__/deno/gen/wxwork/wxw_msg/wxw_msg.dao.ts
@@ -288,9 +288,6 @@ export async function findAll(
     log(msg);
   }
   
-  if (search?.id === "") {
-    return [ ];
-  }
   if (search?.ids?.length === 0) {
     return [ ];
   }
@@ -602,9 +599,6 @@ export async function findOne(
     options.debug = false;
   }
   
-  if (search?.id === "") {
-    return;
-  }
   if (search?.ids?.length === 0) {
     return;
   }
@@ -641,10 +635,19 @@ export async function findById(
     options = options || { };
     options.debug = false;
   }
-  if (isEmpty(id as unknown as string)) {
+  
+  if (id == null) {
     return;
   }
-  const model = await findOne({ id }, undefined, options);
+  
+  const model = await findOne(
+    {
+      id,
+    },
+    undefined,
+    options,
+  );
+  
   return model;
 }
 
@@ -698,7 +701,7 @@ export async function existById(
     log(msg);
   }
   
-  if (isEmpty(id as unknown as string)) {
+  if (id == null) {
     return false;
   }
   
@@ -833,7 +836,9 @@ export async function create(
     throw new Error(`input is required in dao: ${ table }`);
   }
   
-  const [ id ] = await _creates([ input ], options);
+  const [
+    id,
+  ] = await _creates([ input ], options);
   
   return id;
 }
@@ -1071,15 +1076,7 @@ export async function updateTenantById(
   }
   
   const args = new QueryArgs();
-  const sql = `
-    update
-      wxwork_wxw_msg
-    set
-      update_time = ${ args.push(reqDate()) },
-      tenant_id = ${ args.push(tenant_id) }
-    where
-      id = ${ args.push(id) }
-  `;
+  const sql = `update wxwork_wxw_msg set tenant_id=${ args.push(tenant_id) } where id=${ args.push(id) }`;
   const result = await execute(sql, args);
   const num = result.affectedRows;
   return num;
