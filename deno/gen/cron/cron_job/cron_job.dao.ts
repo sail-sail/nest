b95e73b9de85c8efdde012f66fad0991ad7b037d
Sail <151263555@qq.com>
2024-05-21 18:25:14 +0800

    findByIds, noRevert

--- deno/gen/cron/cron_job/cron_job.dao.ts
+++ deno/gen/cron/cron_job/cron_job.dao.ts
@@ -799,6 +799,56 @@ export async function findById(
   return model;
 }
 
+/** 根据 ids 查找定时任务 */
+export async function findByIds(
+  ids: CronJobId[],
+  options?: {
+    debug?: boolean;
+  },
+): Promise<CronJobModel[]> {
+  const table = "cron_cron_job";
+  const method = "findByIds";
+  if (options?.debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (ids) {
+      msg += ` ids:${ ids }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options || { };
+    options.debug = false;
+  }
+  
+  if (!ids || ids.length === 0) {
+    return [ ];
+  }
+  
+  const models = await findAll(
+    {
+      ids,
+    },
+    undefined,
+    undefined,
+    options,
+  );
+  
+  if (models.length !== ids.length) {
+    throw new Error("findByIds: models.length !== ids.length");
+  }
+  
+  const models2 = ids.map((id) => {
+    const model = models.find((item) => item.id === id);
+    if (!model) {
+      throw new Error(`findByIds: id: ${ id } not found`);
+    }
+    return model;
+  });
+  
+  return models2;
+}
+
 /**
  * 根据搜索条件判断定时任务是否存在
  * @param {CronJobSearch} search?
