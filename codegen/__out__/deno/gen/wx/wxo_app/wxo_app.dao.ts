8cce7382b81d938e4c651cef1c09e3ad1f6626fa
Sail <151263555@qq.com>
2024-05-21 18:28:18 +0800

    findByIds, noRevert

--- codegen/__out__/deno/gen/wx/wxo_app/wxo_app.dao.ts
+++ codegen/__out__/deno/gen/wx/wxo_app/wxo_app.dao.ts
@@ -789,6 +789,56 @@ export async function findById(
   return model;
 }
 
+/** 根据 ids 查找公众号设置 */
+export async function findByIds(
+  ids: WxoAppId[],
+  options?: {
+    debug?: boolean;
+  },
+): Promise<WxoAppModel[]> {
+  const table = "wx_wxo_app";
+  const method = "findByIds";
+  if (options?.debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (ids) {
+      msg += ` ids:${ ids }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options || { };
+    options.debug = false;
+  }
+  
+  if (!ids || ids.length === 0) {
+    return [ ];
+  }
+  
+  const models = await findAll(
+    {
+      ids,
+    },
+    undefined,
+    undefined,
+    options,
+  );
+  
+  if (models.length !== ids.length) {
+    throw new Error("findByIds: models.length !== ids.length");
+  }
+  
+  const models2 = ids.map((id) => {
+    const model = models.find((item) => item.id === id);
+    if (!model) {
+      throw new Error(`findByIds: id: ${ id } not found`);
+    }
+    return model;
+  });
+  
+  return models2;
+}
+
 /**
  * 根据搜索条件判断公众号设置是否存在
  * @param {WxoAppSearch} search?
--- codegen/__out__/deno/gen/wx/wxo_app_token/wxo_app_token.dao.ts
+++ codegen/__out__/deno/gen/wx/wxo_app_token/wxo_app_token.dao.ts
@@ -620,6 +620,56 @@ export async function findById(
   return model;
 }
 
+/** 根据 ids 查找小程序接口凭据 */
+export async function findByIds(
+  ids: WxoAppTokenId[],
+  options?: {
+    debug?: boolean;
+  },
+): Promise<WxoAppTokenModel[]> {
+  const table = "wx_wxo_app_token";
+  const method = "findByIds";
+  if (options?.debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (ids) {
+      msg += ` ids:${ ids }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options || { };
+    options.debug = false;
+  }
+  
+  if (!ids || ids.length === 0) {
+    return [ ];
+  }
+  
+  const models = await findAll(
+    {
+      ids,
+    },
+    undefined,
+    undefined,
+    options,
+  );
+  
+  if (models.length !== ids.length) {
+    throw new Error("findByIds: models.length !== ids.length");
+  }
+  
+  const models2 = ids.map((id) => {
+    const model = models.find((item) => item.id === id);
+    if (!model) {
+      throw new Error(`findByIds: id: ${ id } not found`);
+    }
+    return model;
+  });
+  
+  return models2;
+}
+
 /**
  * 根据搜索条件判断小程序接口凭据是否存在
  * @param {WxoAppTokenSearch} search?
