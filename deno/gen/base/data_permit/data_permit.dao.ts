61ad8b326ab9fd58962033a3de8f765f491e7799
Sail <151263555@qq.com>
2024-05-21 15:02:23 +0800

    dao findByIds

--- deno/gen/base/data_permit/data_permit.dao.ts
+++ deno/gen/base/data_permit/data_permit.dao.ts
@@ -710,6 +710,56 @@ export async function findById(
   return model;
 }
 
+/** 根据 ids 查找数据权限 */
+export async function findByIds(
+  ids: DataPermitId[],
+  options?: {
+    debug?: boolean;
+  },
+): Promise<DataPermitModel[]> {
+  const table = "base_data_permit";
+  const method = "findByIds";
+  if (options?.debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (ids) {
+      msg += ` ids:${ ids }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options || { };
+    options.debug = false;
+  }
+  
+  if (!ids || ids.length === 0) {
+    return [ ];
+  }
+  
+  const models = await findAll(
+    {
+      ids,
+    },
+    undefined,
+    undefined,
+    options,
+  );
+  
+  if (models.length !== ids.length) {
+    throw new Error("findByIds: models.length !== ids.length");
+  }
+  
+  const models2 = ids.map((id) => {
+    const model = models.find((item) => item.id === id);
+    if (!model) {
+      throw new Error(`findByIds: id: ${ id } not found`);
+    }
+    return model;
+  });
+  
+  return models2;
+}
+
 /**
  * 根据搜索条件判断数据权限是否存在
  * @param {DataPermitSearch} search?
--- deno/gen/base/optbiz/optbiz.dao.ts
+++ deno/gen/base/optbiz/optbiz.dao.ts
@@ -711,6 +711,56 @@ export async function findById(
   return model;
 }
 
+/** 根据 ids 查找业务选项 */
+export async function findByIds(
+  ids: OptbizId[],
+  options?: {
+    debug?: boolean;
+  },
+): Promise<OptbizModel[]> {
+  const table = "base_optbiz";
+  const method = "findByIds";
+  if (options?.debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (ids) {
+      msg += ` ids:${ ids }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options || { };
+    options.debug = false;
+  }
+  
+  if (!ids || ids.length === 0) {
+    return [ ];
+  }
+  
+  const models = await findAll(
+    {
+      ids,
+    },
+    undefined,
+    undefined,
+    options,
+  );
+  
+  if (models.length !== ids.length) {
+    throw new Error("findByIds: models.length !== ids.length");
+  }
+  
+  const models2 = ids.map((id) => {
+    const model = models.find((item) => item.id === id);
+    if (!model) {
+      throw new Error(`findByIds: id: ${ id } not found`);
+    }
+    return model;
+  });
+  
+  return models2;
+}
+
 /**
  * 根据搜索条件判断业务选项是否存在
  * @param {OptbizSearch} search?
