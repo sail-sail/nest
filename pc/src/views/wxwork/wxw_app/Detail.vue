1e930566618cf6ab83b6ad88671c8206f217be16
Sail <151263555@qq.com>
2024-05-23 15:15:41 +0800

    已经是第一项

--- pc/src/views/wxwork/wxw_app/Detail.vue
+++ pc/src/views/wxwork/wxw_app/Detail.vue
@@ -645,7 +645,7 @@ async function onPageUp(e?: KeyboardEvent) {
   }
   const isSucc = await prevId();
   if (!isSucc) {
-    ElMessage.warning(await nsAsync("已经是第一{0} 了", await nsAsync("企微应用")));
+    ElMessage.warning(await nsAsync("已经是第一项了"));
   }
 }
 
