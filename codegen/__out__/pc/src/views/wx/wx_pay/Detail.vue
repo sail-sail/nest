0bae8987ad233d0ac8b2ade2db860572200fb084
Sail <151263555@qq.com>
2024-05-23 15:16:14 +0800

    @submit.prevent

--- codegen/__out__/pc/src/views/wx/wx_pay/Detail.vue
+++ codegen/__out__/pc/src/views/wx/wx_pay/Detail.vue
@@ -71,6 +71,8 @@
         :model="dialogModel"
         :rules="form_rules"
         :validate-on-rule-change="false"
+        
+        @submit.prevent
       >
         
         <template v-if="(showBuildIn || builtInModel?.lbl == null)">
@@ -637,7 +639,7 @@ async function onPageUp(e?: KeyboardEvent) {
   }
   const isSucc = await prevId();
   if (!isSucc) {
-    ElMessage.warning(await nsAsync("已经是第一个 {0} 了", await nsAsync("微信支付设置")));
+    ElMessage.warning(await nsAsync("已经是第一项了"));
   }
 }
 
