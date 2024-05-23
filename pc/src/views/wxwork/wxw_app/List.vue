ddba70ea5ab5ab66b2ad1f7c2eb3dbb4db82a16c
Sail <151263555@qq.com>
2024-05-23 15:12:12 +0800

    @submit.prevent

--- pc/src/views/wxwork/wxw_app/List.vue
+++ pc/src/views/wxwork/wxw_app/List.vue
@@ -23,7 +23,8 @@
       un-justify-items-end
       un-items-center
       
-      @keyup.enter="onSearch"
+      @submit.prevent
+      @keydown.enter="onSearch"
     >
       
       <template v-if="builtInSearch?.lbl == null && (showBuildIn || builtInSearch?.lbl_like == null)">
@@ -1596,7 +1597,7 @@ async function onDeleteByIds() {
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定删除已选择的 {0} 个 {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
+    await ElMessageBox.confirm(`${ await nsAsync("确定删除已选择的 {0} {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
       confirmButtonText: await nsAsync("确定"),
       cancelButtonText: await nsAsync("取消"),
       type: "warning",
@@ -1610,7 +1611,7 @@ async function onDeleteByIds() {
     selectedIds = [ ];
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
-    ElMessage.success(await nsAsync("删除 {0} 个 {1} 成功", num, await nsAsync("企微应用")));
+    ElMessage.success(await nsAsync("删除 {0} {1} 成功", num, await nsAsync("企微应用")));
     emit("remove", num);
   }
 }
@@ -1630,7 +1631,7 @@ async function onForceDeleteByIds() {
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定 彻底删除 已选择的 {0} 个 {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
+    await ElMessageBox.confirm(`${ await nsAsync("确定 彻底删除 已选择的 {0} {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
       confirmButtonText: await nsAsync("确定"),
       cancelButtonText: await nsAsync("取消"),
       type: "warning",
@@ -1641,7 +1642,7 @@ async function onForceDeleteByIds() {
   const num = await forceDeleteByIds(selectedIds);
   if (num) {
     selectedIds = [ ];
-    ElMessage.success(await nsAsync("彻底删除 {0} 个 {1} 成功", num, await nsAsync("企微应用")));
+    ElMessage.success(await nsAsync("彻底删除 {0} {1} 成功", num, await nsAsync("企微应用")));
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
   }
@@ -1671,9 +1672,9 @@ async function onEnableByIds(is_enabled: 0 | 1) {
   if (num > 0) {
     let msg = "";
     if (is_enabled === 1) {
-      msg = await nsAsync("启用 {0} 个 {1} 成功", num, await nsAsync("企微应用"));
+      msg = await nsAsync("启用 {0} {1} 成功", num, await nsAsync("企微应用"));
     } else {
-      msg = await nsAsync("禁用 {0} 个 {1} 成功", num, await nsAsync("企微应用"));
+      msg = await nsAsync("禁用 {0} {1} 成功", num, await nsAsync("企微应用"));
     }
     ElMessage.success(msg);
     dirtyStore.fireDirty(pageName);
@@ -1705,9 +1706,9 @@ async function onLockByIds(is_locked: 0 | 1) {
   if (num > 0) {
     let msg = "";
     if (is_locked === 1) {
-      msg = await nsAsync("锁定 {0} 个 {1} 成功", num, await nsAsync("企微应用"));
+      msg = await nsAsync("锁定 {0} {1} 成功", num, await nsAsync("企微应用"));
     } else {
-      msg = await nsAsync("解锁 {0} 个 {1} 成功", num, await nsAsync("企微应用"));
+      msg = await nsAsync("解锁 {0} {1} 成功", num, await nsAsync("企微应用"));
     }
     ElMessage.success(msg);
     dirtyStore.fireDirty(pageName);
@@ -1730,7 +1731,7 @@ async function onRevertByIds() {
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定还原已选择的 {0} 个 {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
+    await ElMessageBox.confirm(`${ await nsAsync("确定还原已选择的 {0} {1}", selectedIds.length, await nsAsync("企微应用")) }?`, {
       confirmButtonText: await nsAsync("确定"),
       cancelButtonText: await nsAsync("取消"),
       type: "warning",
@@ -1743,7 +1744,7 @@ async function onRevertByIds() {
     search.is_deleted = 0;
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
-    ElMessage.success(await nsAsync("还原 {0} 个 {1} 成功", num, await nsAsync("企微应用")));
+    ElMessage.success(await nsAsync("还原 {0} {1} 成功", num, await nsAsync("企微应用")));
     emit("revert", num);
   }
 }
