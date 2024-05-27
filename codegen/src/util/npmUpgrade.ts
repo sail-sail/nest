ed0d4f276a666d1ed6170fa71427bf5d99507e51
Sail <151263555@qq.com>
2024-05-27 08:51:12 +0800

    upgrade

--- codegen/src/util/npmUpgrade.ts
+++ codegen/src/util/npmUpgrade.ts
@@ -4,7 +4,7 @@ const parentBranchs = [
 ];
 
 async function exec() {
-  const currentBranch = execSync("git branch --show-current").toString().trim();
+  // const currentBranch = execSync("git branch --show-current").toString().trim();
   let command = "";
   command = "git rev-parse --abbrev-ref @{u}";
   console.log(command);
@@ -14,22 +14,22 @@ async function exec() {
   // console.log(command);
   // execSync(command, { stdio: "inherit" });
   if (parentBranchs.length > 0) {
-    for (let i = 0; i < parentBranchs.length; i++) {
-      const branch = parentBranchs[i];
-      command = `git switch ${ branch }`;
-      console.log(command);
-      execSync(command, { stdio: "inherit" });
-      command = "git rev-parse --abbrev-ref @{u}";
-      console.log(command);
-      let origin = execSync(command).toString().trim();
-      origin = origin.split("/")[0];
-      command = `git pull ${ origin } ${ branch }`;
-      console.log(command);
-      execSync(command, { stdio: "inherit" });
-    }
-    command = `git switch ${ currentBranch }`;
-    console.log(command);
-    execSync(command, { stdio: "inherit" });
+    // for (let i = 0; i < parentBranchs.length; i++) {
+    //   const branch = parentBranchs[i];
+    //   command = `git switch ${ branch }`;
+    //   console.log(command);
+    //   execSync(command, { stdio: "inherit" });
+    //   command = "git rev-parse --abbrev-ref @{u}";
+    //   console.log(command);
+    //   let origin = execSync(command).toString().trim();
+    //   origin = origin.split("/")[0];
+    //   command = `git pull ${ origin } ${ branch }`;
+    //   console.log(command);
+    //   execSync(command, { stdio: "inherit" });
+    // }
+    // command = `git switch ${ currentBranch }`;
+    // console.log(command);
+    // execSync(command, { stdio: "inherit" });
     for (let i = 0; i < parentBranchs.length; i++) {
       const branch = parentBranchs[i];
       command = `git merge ${ branch }`;
