9ec1e55df2dfd904c9be1e6b7fb3b852ddc3bcfa
Sail <151263555@qq.com>
2024-05-31 17:47:30 +0800

    org_id

--- codegen/__out__/deno/gen/wx/wxo_usr/wxo_usr.model.ts
+++ codegen/__out__/deno/gen/wx/wxo_usr/wxo_usr.model.ts
@@ -28,8 +28,8 @@ declare global {
     create_time?: string[];
     /** 更新时间 */
     update_time?: string[];
-    tenant_id?: string | null;
-    org_id?: OrgId | null;
+    tenant_id?: TenantId | null;
+    org_id?: OrgId[] | null;
   }
 
   interface WxoUsrModel extends WxoUsrModelType {
