f66382215533b3aad9ddfc90b44af647e747dbc9
Sail <151263555@qq.com>
2024-06-03 08:05:07 +0800

    creates update_usr_id

--- deno/gen/wx/wx_app_token/wx_app_token.dao.ts
+++ deno/gen/wx/wx_app_token/wx_app_token.dao.ts
@@ -954,15 +954,12 @@ async function _creates(
   
   const args = new QueryArgs();
   let sql = `insert into wx_wx_app_token(id`;
-  if (!silentMode) {
-    sql += ",create_time";
-  }
-  if (!silentMode) {
-    sql += ",create_usr_id";
-  }
-  if (!silentMode) {
-    sql += ",create_usr_id_lbl";
-  }
+  sql += ",create_time";
+  sql += ",update_time";
+  sql += ",create_usr_id";
+  sql += ",create_usr_id_lbl";
+  sql += ",update_usr_id";
+  sql += ",update_usr_id_lbl";
   sql += ",wx_app_id";
   sql += ",access_token";
   sql += ",token_time";
@@ -975,11 +972,22 @@ async function _creates(
       const input = inputs2[i];
       sql += `(${ args.push(input.id) }`;
       if (!silentMode) {
-        if (input.create_time != null) {
+        if (input.create_time != null || input.create_time_save_null) {
           sql += `,${ args.push(input.create_time) }`;
         } else {
           sql += `,${ args.push(reqDate()) }`;
         }
+      } else {
+        if (input.create_time != null || input.create_time_save_null) {
+          sql += `,${ args.push(input.create_time) }`;
+        } else {
+          sql += `,null`;
+        }
+      }
+      if (input.update_time != null || input.update_time_save_null) {
+        sql += `,${ args.push(input.update_time) }`;
+      } else {
+        sql += `,null`;
       }
       if (!silentMode) {
         if (input.create_usr_id == null) {
@@ -1020,6 +1028,27 @@ async function _creates(
           }
           sql += `,${ args.push(usr_lbl) }`;
         }
+      } else {
+        if (input.create_usr_id == null) {
+          sql += ",default";
+        } else {
+          sql += `,${ args.push(input.create_usr_id) }`;
+        }
+        if (input.create_usr_id_lbl == null) {
+          sql += ",default";
+        } else {
+          sql += `,${ args.push(input.create_usr_id_lbl) }`;
+        }
+      }
+      if (input.update_usr_id != null) {
+        sql += `,${ args.push(input.update_usr_id) }`;
+      } else {
+        sql += ",default";
+      }
+      if (input.update_usr_id_lbl != null) {
+        sql += `,${ args.push(input.update_usr_id_lbl) }`;
+      } else {
+        sql += ",default";
       }
       if (input.wx_app_id != null) {
         sql += `,${ args.push(input.wx_app_id) }`;
@@ -1148,7 +1177,7 @@ export async function updateById(
   let updateFldNum = 0;
   if (input.wx_app_id != null) {
     if (input.wx_app_id != oldModel.wx_app_id) {
-      sql += `wx_app_id = ${ args.push(input.wx_app_id) },`;
+      sql += `wx_app_id=${ args.push(input.wx_app_id) },`;
       updateFldNum++;
     }
   }
@@ -1167,6 +1196,23 @@ export async function updateById(
   if (input.expires_in != null) {
     if (input.expires_in != oldModel.expires_in) {
       sql += `expires_in=${ args.push(input.expires_in) },`;
+      updateFldNum++;
+    }
+  }
+  if (isNotEmpty(input.create_usr_id_lbl)) {
+    sql += `create_usr_id_lbl=?,`;
+    args.push(input.create_usr_id_lbl);
+    updateFldNum++;
+  }
+  if (input.create_usr_id != null) {
+    if (input.create_usr_id != oldModel.create_usr_id) {
+      sql += `create_usr_id=${ args.push(input.create_usr_id) },`;
+      updateFldNum++;
+    }
+  }
+  if (input.create_time != null) {
+    if (input.create_time != oldModel.create_time) {
+      sql += `create_time=${ args.push(input.create_time) },`;
       updateFldNum++;
     }
   }
@@ -1208,13 +1254,22 @@ export async function updateById(
           sql += `update_usr_id_lbl=${ args.push(usr_lbl) },`;
         }
       }
+    } else {
+      if (input.update_usr_id != null) {
+        sql += `update_usr_id=${ args.push(input.update_usr_id) },`;
+      }
+      if (input.update_usr_id_lbl != null) {
+        sql += `update_usr_id_lbl=${ args.push(input.update_usr_id_lbl) },`;
+      }
     }
     if (!silentMode) {
-      if (input.update_time) {
+      if (input.update_time != null || input.update_time_save_null) {
         sql += `update_time=${ args.push(input.update_time) },`;
       } else {
         sql += `update_time=${ args.push(reqDate()) },`;
       }
+    } else if (input.update_time != null || input.update_time_save_null) {
+      sql += `update_time=${ args.push(input.update_time) },`;
     }
     if (sql.endsWith(",")) {
       sql = sql.substring(0, sql.length - 1);
