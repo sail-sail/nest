95fe49e94e5e278c8667bda88438b8d58227b0a9
Sail <151263555@qq.com>
2024-06-11 15:31:26 +0800

    updateById UniqueType.Throw 默认值

--- codegen/src/template/deno/gen/[[mod_slash_table]]/[[table]].dao.ts
+++ codegen/src/template/deno/gen/[[mod_slash_table]]/[[table]].dao.ts
@@ -4017,7 +4017,7 @@ export async function updateById(
     let models = await findByUnique(input2, options);
     models = models.filter((item) => item.id !== id);
     if (models.length > 0) {
-      if (!options || options.uniqueType === UniqueType.Throw) {
+      if (!options || !options.uniqueType || options.uniqueType === UniqueType.Throw) {
         throw await ns("此 {0} 已经存在", await ns("<#=table_comment#>"));
       } else if (options.uniqueType === UniqueType.Ignore) {
         return id;
