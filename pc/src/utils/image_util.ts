9d0584e88b612ef02bb119579a570caa8b91d2ee
Sail <151263555@qq.com>
2024-06-11 08:52:20 +0800

    image

--- pc/src/utils/image_util.ts
+++ pc/src/utils/image_util.ts
@@ -3,10 +3,23 @@ import * as jpeg from "@jsquash/jpeg";
 import * as png from "@jsquash/png";
 import resize from "@jsquash/resize";
 
-async function workerFn(file: File) {
+async function workerFn(
+  file: File,
+  opts?: {
+    compress: boolean,
+    maxImageWidth?: number,
+    maxImageHeight?: number,
+  },
+) {
+  
+  const compress = opts?.compress ?? true;
+  
+  if (compress) {
+    return file;
+  }
     
-  const maxImageWidth = 1920;
-  const maxImageHeight = 1080;
+  const maxImageWidth = opts?.maxImageWidth ?? 1920;
+  const maxImageHeight = opts?.maxImageHeight ?? 1080;
   
   let buffer = await file.arrayBuffer();
   let imgData: ImageData;
@@ -46,7 +59,12 @@ async function workerFn(file: File) {
 
 export async function checkImageMaxSize(
   file: File,
+  opts?: {
+    compress: boolean,
+    maxImageWidth?: number,
+    maxImageHeight?: number,
+  },
 ) {
-  const file2 = await workerFn(file);
+  const file2 = await workerFn(file, opts);
   return file2;
 }
