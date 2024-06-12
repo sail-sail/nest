abd6bcb7991b1d8b41ac5a52751aa47829b87084
Sail <151263555@qq.com>
2024-06-12 11:29:11 +0800

    is_creating

--- codegen/__out__/deno/gen/cron/cron_job/cron_job.dao.ts
+++ codegen/__out__/deno/gen/cron/cron_job/cron_job.dao.ts
@@ -2,6 +2,7 @@
 import {
   get_is_debug,
   get_is_silent_mode,
+  get_is_creating,
 } from "/lib/context.ts";
 
 import {
@@ -1387,12 +1388,13 @@ async function _creates(
   
   const is_debug_sql = getParsedEnv("database_debug_sql") === "true";
   
-  await execute(sql, args, {
+  const res = await execute(sql, args, {
     debug: is_debug_sql,
   });
+  const affectedRows = res.affectedRows;
   
-  for (let i = 0; i < inputs2.length; i++) {
-    const input = inputs2[i];
+  if (affectedRows !== inputs2.length) {
+    throw new Error(`affectedRows: ${ affectedRows } != ${ inputs2.length }`);
   }
   
   await delCache();
@@ -1453,13 +1455,13 @@ export async function updateTenantById(
   
   const args = new QueryArgs();
   const sql = `update cron_cron_job set tenant_id=${ args.push(tenant_id) } where id=${ args.push(id) }`;
-  const result = await execute(sql, args);
-  const num = result.affectedRows;
+  const res = await execute(sql, args);
+  const affectedRows = res.affectedRows;
   
   await delCache();
   
   await refreshCronJobs();
-  return num;
+  return affectedRows;
 }
 
 /**
@@ -1481,6 +1483,7 @@ export async function updateById(
     is_debug?: boolean;
     uniqueType?: Exclude<UniqueType, UniqueType.Update>;
     is_silent_mode?: boolean;
+    is_creating?: boolean;
   },
 ): Promise<CronJobId> {
   
@@ -1489,6 +1492,7 @@ export async function updateById(
   
   const is_debug = get_is_debug(options?.is_debug);
   const is_silent_mode = get_is_silent_mode(options?.is_silent_mode);
+  const is_creating = get_is_creating(options?.is_creating);
   
   if (is_debug !== false) {
     let msg = `${ table }.${ method }:`;
@@ -1526,7 +1530,7 @@ export async function updateById(
     let models = await findByUnique(input2, options);
     models = models.filter((item) => item.id !== id);
     if (models.length > 0) {
-      if (!options || options.uniqueType === UniqueType.Throw) {
+      if (!options || !options.uniqueType || options.uniqueType === UniqueType.Throw) {
         throw await ns("此 {0} 已经存在", await ns("定时任务"));
       } else if (options.uniqueType === UniqueType.Ignore) {
         return id;
@@ -1611,7 +1615,7 @@ export async function updateById(
   let sqlSetFldNum = updateFldNum;
   
   if (updateFldNum > 0) {
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       if (input.update_usr_id == null) {
         const authModel = await getAuthModel();
         let usr_id: UsrId | undefined = authModel?.id;
@@ -1654,7 +1658,7 @@ export async function updateById(
         sql += `update_usr_id_lbl=${ args.push(input.update_usr_id_lbl) },`;
       }
     }
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       if (input.update_time != null || input.update_time_save_null) {
         sql += `update_time=${ args.push(input.update_time) },`;
       } else {
@@ -1702,6 +1706,7 @@ export async function deleteByIds(
   options?: {
     is_debug?: boolean;
     is_silent_mode?: boolean;
+    is_creating?: boolean;
   },
 ): Promise<number> {
   
@@ -1710,6 +1715,7 @@ export async function deleteByIds(
   
   const is_debug = get_is_debug(options?.is_debug);
   const is_silent_mode = get_is_silent_mode(options?.is_silent_mode);
+  const is_creating = get_is_creating(options?.is_creating);
   
   if (is_debug !== false) {
     let msg = `${ table }.${ method }:`;
@@ -1730,7 +1736,7 @@ export async function deleteByIds(
   
   await delCache();
   
-  let num = 0;
+  let affectedRows = 0;
   for (let i = 0; i < ids.length; i++) {
     const id = ids[i];
     const oldModel = await findById(id, options);
@@ -1739,7 +1745,7 @@ export async function deleteByIds(
     }
     const args = new QueryArgs();
     let sql = `update cron_cron_job set is_deleted=1`;
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       const authModel = await getAuthModel();
       let usr_id: UsrId | undefined = authModel?.id;
       if (usr_id != null) {
@@ -1747,7 +1753,7 @@ export async function deleteByIds(
       }
       let usr_lbl = "";
       if (usr_id) {
-        const usr_model = await findByIdUsr(usr_id);
+        const usr_model = await findByIdUsr(usr_id, options);
         if (!usr_model) {
           usr_id = undefined;
         } else {
@@ -1760,15 +1766,15 @@ export async function deleteByIds(
       sql += `,delete_time=${ args.push(reqDate()) }`;
     }
     sql += ` where id=${ args.push(id) } limit 1`;
-    const result = await execute(sql, args);
-    num += result.affectedRows;
+    const res = await execute(sql, args);
+    affectedRows += res.affectedRows;
   }
   
   await delCache();
   
   await refreshCronJobs();
   
-  return num;
+  return affectedRows;
 }
 
 /**
--- codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.resolver.ts
+++ codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.resolver.ts
@@ -1,5 +1,6 @@
 import {
-  useContext,
+  set_is_tran,
+  set_is_creating,
 } from "/lib/context.ts";
 
 import type {
@@ -95,9 +96,7 @@ export async function deleteByIdsCronJobLog(
     deleteByIds,
   } = await import("./cron_job_log.service.ts");
   
-  const context = useContext();
-  
-  context.is_tran = true;
+  set_is_tran(true);
   
   await usePermit(
     "/cron/cron_job_log",
@@ -118,9 +117,7 @@ export async function revertByIdsCronJobLog(
     revertByIds,
   } = await import("./cron_job_log.service.ts");
   
-  const context = useContext();
-  
-  context.is_tran = true;
+  set_is_tran(true);
   
   await usePermit(
     "/cron/cron_job_log",
@@ -136,18 +133,17 @@ export async function revertByIdsCronJobLog(
 export async function forceDeleteByIdsCronJobLog(
   ids: CronJobLogId[],
 ): Promise<number> {
-  const context = useContext();
   
-  context.is_tran = true;
+  const {
+    forceDeleteByIds,
+  } = await import("./cron_job_log.service.ts");
+  
+  set_is_tran(true);
   
   await usePermit(
     "/cron/cron_job_log",
     "force_delete",
   );
-  
-  const {
-    forceDeleteByIds,
-  } = await import("./cron_job_log.service.ts");
   const res = await forceDeleteByIds(ids);
   return res;
 }
