86a9dd1832934e0d0bf59523042e7d8e0c4a6025
Sail <151263555@qq.com>
2024-06-12 11:29:29 +0800

    is_creating

--- codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
+++ codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
@@ -2,6 +2,7 @@
 import {
   get_is_debug,
   get_is_silent_mode,
+  get_is_creating,
 } from "/lib/context.ts";
 
 import {
@@ -1308,12 +1309,13 @@ async function _creates(
   
   const is_debug_sql = getParsedEnv("database_debug_sql") === "true";
   
-  await execute(sql, args, {
+  const res = await execute(sql, args, {
     debug: is_debug_sql,
   });
+  const affectedRows = res.affectedRows;
   
-  for (let i = 0; i < inputs2.length; i++) {
-    const input = inputs2[i];
+  if (affectedRows !== inputs2.length) {
+    throw new Error(`affectedRows: ${ affectedRows } != ${ inputs2.length }`);
   }
   
   await delCache();
@@ -1372,11 +1374,11 @@ export async function updateTenantById(
   
   const args = new QueryArgs();
   const sql = `update nuxt_seo set tenant_id=${ args.push(tenant_id) } where id=${ args.push(id) }`;
-  const result = await execute(sql, args);
-  const num = result.affectedRows;
+  const res = await execute(sql, args);
+  const affectedRows = res.affectedRows;
   
   await delCache();
-  return num;
+  return affectedRows;
 }
 
 /**
@@ -1398,6 +1400,7 @@ export async function updateById(
     is_debug?: boolean;
     uniqueType?: Exclude<UniqueType, UniqueType.Update>;
     is_silent_mode?: boolean;
+    is_creating?: boolean;
   },
 ): Promise<SeoId> {
   
@@ -1406,6 +1409,7 @@ export async function updateById(
   
   const is_debug = get_is_debug(options?.is_debug);
   const is_silent_mode = get_is_silent_mode(options?.is_silent_mode);
+  const is_creating = get_is_creating(options?.is_creating);
   
   if (is_debug !== false) {
     let msg = `${ table }.${ method }:`;
@@ -1443,7 +1447,7 @@ export async function updateById(
     let models = await findByUnique(input2, options);
     models = models.filter((item) => item.id !== id);
     if (models.length > 0) {
-      if (!options || options.uniqueType === UniqueType.Throw) {
+      if (!options || !options.uniqueType || options.uniqueType === UniqueType.Throw) {
         throw await ns("此 {0} 已经存在", await ns("SEO优化"));
       } else if (options.uniqueType === UniqueType.Ignore) {
         return id;
@@ -1540,7 +1544,7 @@ export async function updateById(
   let sqlSetFldNum = updateFldNum;
   
   if (updateFldNum > 0) {
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       if (input.update_usr_id == null) {
         const authModel = await getAuthModel();
         let usr_id: UsrId | undefined = authModel?.id;
@@ -1583,7 +1587,7 @@ export async function updateById(
         sql += `update_usr_id_lbl=${ args.push(input.update_usr_id_lbl) },`;
       }
     }
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       if (input.update_time != null || input.update_time_save_null) {
         sql += `update_time=${ args.push(input.update_time) },`;
       } else {
@@ -1629,6 +1633,7 @@ export async function deleteByIds(
   options?: {
     is_debug?: boolean;
     is_silent_mode?: boolean;
+    is_creating?: boolean;
   },
 ): Promise<number> {
   
@@ -1637,6 +1642,7 @@ export async function deleteByIds(
   
   const is_debug = get_is_debug(options?.is_debug);
   const is_silent_mode = get_is_silent_mode(options?.is_silent_mode);
+  const is_creating = get_is_creating(options?.is_creating);
   
   if (is_debug !== false) {
     let msg = `${ table }.${ method }:`;
@@ -1657,7 +1663,7 @@ export async function deleteByIds(
   
   await delCache();
   
-  let num = 0;
+  let affectedRows = 0;
   for (let i = 0; i < ids.length; i++) {
     const id = ids[i];
     const oldModel = await findById(id, options);
@@ -1666,7 +1672,7 @@ export async function deleteByIds(
     }
     const args = new QueryArgs();
     let sql = `update nuxt_seo set is_deleted=1`;
-    if (!is_silent_mode) {
+    if (!is_silent_mode && !is_creating) {
       const authModel = await getAuthModel();
       let usr_id: UsrId | undefined = authModel?.id;
       if (usr_id != null) {
@@ -1674,7 +1680,7 @@ export async function deleteByIds(
       }
       let usr_lbl = "";
       if (usr_id) {
-        const usr_model = await findByIdUsr(usr_id);
+        const usr_model = await findByIdUsr(usr_id, options);
         if (!usr_model) {
           usr_id = undefined;
         } else {
@@ -1687,13 +1693,13 @@ export async function deleteByIds(
       sql += `,delete_time=${ args.push(reqDate()) }`;
     }
     sql += ` where id=${ args.push(id) } limit 1`;
-    const result = await execute(sql, args);
-    num += result.affectedRows;
+    const res = await execute(sql, args);
+    affectedRows += res.affectedRows;
   }
   
   await delCache();
   
-  return num;
+  return affectedRows;
 }
 
 /**
