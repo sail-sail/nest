8a181e37f8f3f21123904e9c35c47b519bf9bd77
Sail <151263555@qq.com>
2024-06-25 17:33:41 +0800

    validateOption

--- codegen/__out__/deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
+++ codegen/__out__/deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
@@ -845,7 +845,9 @@ export async function validateOption(
   model?: BaiduAppTokenModel,
 ) {
   if (!model) {
-    throw `${ await ns("百度接口凭据") } ${ await ns("不存在") }`;
+    const err_msg = `${ await ns("百度接口凭据") } ${ await ns("不存在") }`;
+    error(new Error(err_msg));
+    throw err_msg;
   }
   return model;
 }
@@ -1036,19 +1038,7 @@ async function _creates(
   }
   
   const args = new QueryArgs();
-  let sql = `insert into baidu_baidu_app_token(id`;
-  sql += ",create_time";
-  sql += ",update_time";
-  sql += ",tenant_id";
-  sql += ",create_usr_id";
-  sql += ",create_usr_id_lbl";
-  sql += ",update_usr_id";
-  sql += ",update_usr_id_lbl";
-  sql += ",baidu_app_id";
-  sql += ",access_token";
-  sql += ",token_time";
-  sql += ",expires_in";
-  sql += ")values";
+  let sql = "insert into baidu_baidu_app_token(id,create_time,update_time,tenant_id,create_usr_id,create_usr_id_lbl,update_usr_id,update_usr_id_lbl,baidu_app_id,access_token,token_time,expires_in)values";
   
   const inputs2Arr = splitCreateArr(inputs2);
   for (const inputs2 of inputs2Arr) {
@@ -1507,7 +1497,7 @@ export async function deleteByIds(
     let sql = `update baidu_baidu_app_token set is_deleted=1`;
     if (!is_silent_mode && !is_creating) {
       const authModel = await getAuthModel();
-      let usr_id: UsrId | undefined = authModel?.id;
+      let usr_id = authModel?.id;
       if (usr_id != null) {
         sql += `,delete_usr_id=${ args.push(usr_id) }`;
       }
