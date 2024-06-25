72030b2711a427e231c1513d9b17c594b5020b18
Sail <151263555@qq.com>
2024-06-25 17:33:21 +0800

    validateOption

--- deno/gen/wxwork/wxw_app/wxw_app.dao.ts
+++ deno/gen/wxwork/wxw_app/wxw_app.dao.ts
@@ -980,7 +980,9 @@ export async function validateOption(
   model?: WxwAppModel,
 ) {
   if (!model) {
-    throw `${ await ns("企微应用") } ${ await ns("不存在") }`;
+    const err_msg = `${ await ns("企微应用") } ${ await ns("不存在") }`;
+    error(new Error(err_msg));
+    throw err_msg;
   }
   return model;
 }
@@ -1206,25 +1208,7 @@ async function _creates(
   }
   
   const args = new QueryArgs();
-  let sql = `insert into wxwork_wxw_app(id`;
-  sql += ",create_time";
-  sql += ",update_time";
-  sql += ",tenant_id";
-  sql += ",create_usr_id";
-  sql += ",create_usr_id_lbl";
-  sql += ",update_usr_id";
-  sql += ",update_usr_id_lbl";
-  sql += ",lbl";
-  sql += ",corpid";
-  sql += ",agentid";
-  sql += ",domain_id";
-  sql += ",corpsecret";
-  sql += ",contactsecret";
-  sql += ",is_locked";
-  sql += ",is_enabled";
-  sql += ",order_by";
-  sql += ",rem";
-  sql += ")values";
+  let sql = "insert into wxwork_wxw_app(id,create_time,update_time,tenant_id,create_usr_id,create_usr_id_lbl,update_usr_id,update_usr_id_lbl,lbl,corpid,agentid,domain_id,corpsecret,contactsecret,is_locked,is_enabled,order_by,rem)values";
   
   const inputs2Arr = splitCreateArr(inputs2);
   for (const inputs2 of inputs2Arr) {
@@ -1749,7 +1733,7 @@ export async function deleteByIds(
     let sql = `update wxwork_wxw_app set is_deleted=1`;
     if (!is_silent_mode && !is_creating) {
       const authModel = await getAuthModel();
-      let usr_id: UsrId | undefined = authModel?.id;
+      let usr_id = authModel?.id;
       if (usr_id != null) {
         sql += `,delete_usr_id=${ args.push(usr_id) }`;
       }
