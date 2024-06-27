f565bf1b055260479be310719d28abb33fbdaea9
Sail <151263555@qq.com>
2024-06-27 09:31:46 +0800

    FieldComment

--- codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.dao.ts
+++ codegen/__out__/deno/gen/cron/cron_job_log/cron_job_log.dao.ts
@@ -578,6 +578,12 @@ export async function getFieldComments(): Promise<CronJobLogFieldComment> {
     rem: await n("备注"),
     create_time: await n("创建时间"),
     create_time_lbl: await n("创建时间"),
+    create_usr_id: await n("创建人"),
+    create_usr_id_lbl: await n("创建人"),
+    update_usr_id: await n("更新人"),
+    update_usr_id_lbl: await n("更新人"),
+    update_time: await n("更新时间"),
+    update_time_lbl: await n("更新时间"),
   };
   return fieldComments;
 }
