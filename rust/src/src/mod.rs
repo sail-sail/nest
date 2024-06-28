854616692366619654cec0ce0d3ea699dfd9681e
Sail <151263555@qq.com>
2024-06-28 10:47:49 +0800

    SetTenantAdminPwdInput

--- rust/src/src/mod.rs
+++ rust/src/src/mod.rs
@@ -20,4 +20,5 @@ pub struct SrcQuery(
 pub struct SrcMutation(
   crate::src::base::org::org_graphql::OrgMutation,
   crate::src::base::usr::usr_graphql::UsrMutation,
+  crate::src::base::tenant::tenant_graphql::TenantMutation,
 );
