df62963b1048c4c05cd5459bba71ae28a9217067
Sail <151263555@qq.com>
2024-07-12 17:08:19 +0800

    forceDeleteByIds log

--- deno/gen/wx/wxo_app_token/wxo_app_token.dao.ts
+++ deno/gen/wx/wxo_app_token/wxo_app_token.dao.ts
@@ -46,10 +46,6 @@ import {
   hash,
 } from "/lib/util/string_util.ts";
 
-import {
-  deepCompare,
-} from "/lib/util/object_util.ts";
-
 import * as validators from "/lib/validators/mod.ts";
 
 import { UniqueException } from "/lib/exceptions/unique.execption.ts";
@@ -1380,11 +1376,7 @@ export async function updateById(
   }
   
   if (!is_silent_mode) {
-    const newModel = await findById(id, options);
-    
-    if (!deepCompare(oldModel, newModel)) {
-      log(JSON.stringify(oldModel));
-    }
+    log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
   }
   
   return id;
@@ -1436,6 +1428,9 @@ export async function deleteByIds(
     const oldModel = await findById(id, options);
     if (!oldModel) {
       continue;
+    }
+    if (!is_silent_mode) {
+      log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
     }
     const args = new QueryArgs();
     let sql = `update wx_wxo_app_token set is_deleted=1`;
@@ -1546,12 +1541,14 @@ export async function forceDeleteByIds(
   ids: WxoAppTokenId[],
   options?: {
     is_debug?: boolean;
+    is_silent_mode?: boolean;
   },
 ): Promise<number> {
   
   const table = "wx_wxo_app_token";
   const method = "forceDeleteByIds";
   
+  const is_silent_mode = get_is_silent_mode(options?.is_silent_mode);
   const is_debug = get_is_debug(options?.is_debug);
   
   if (is_debug !== false) {
@@ -1584,7 +1581,9 @@ export async function forceDeleteByIds(
       undefined,
       options,
     );
-    log("forceDeleteByIds:", oldModel);
+    if (oldModel && !is_silent_mode) {
+      log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
+    }
     const args = new QueryArgs();
     const sql = `delete from wx_wxo_app_token where id=${ args.push(id) } and is_deleted = 1 limit 1`;
     const result = await execute(sql, args);
