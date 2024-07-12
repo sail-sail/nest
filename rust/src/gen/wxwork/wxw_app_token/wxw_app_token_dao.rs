65d34a6d5dab97381ffdff5b63641ab6063040ae
Sail <151263555@qq.com>
2024-07-12 16:59:31 +0800

    forceDeleteByIds log

--- rust/src/gen/wxwork/wxw_app_token/wxw_app_token_dao.rs
+++ rust/src/gen/wxwork/wxw_app_token/wxw_app_token_dao.rs
@@ -1624,6 +1624,17 @@ pub async fn update_by_id(
     ).await?;
     return Err(anyhow!(err_msg));
   }
+  let old_model = old_model.unwrap();
+  
+  if !is_silent_mode {
+    info!(
+      "{} {}.{}: {}",
+      get_req_id(),
+      table,
+      method,
+      serde_json::to_string(&old_model)?,
+    );
+  }
   
   {
     let mut input = input.clone();
@@ -1893,6 +1904,16 @@ pub async fn delete_by_ids(
     }
     let old_model = old_model.unwrap();
     
+    if !is_silent_mode {
+      info!(
+        "{} {}.{}: {}",
+        get_req_id(),
+        table,
+        method,
+        serde_json::to_string(&old_model)?,
+      );
+    }
+    
     let mut args = QueryArgs::new();
     
     let mut sql_fields = String::with_capacity(30);
@@ -2071,6 +2092,8 @@ pub async fn force_delete_by_ids(
   
   let is_debug = get_is_debug(options.as_ref());
   
+  let is_silent_mode = get_is_silent_mode(options.as_ref());
+  
   if is_debug {
     let mut msg = format!("{table}.{method}:");
     msg += &format!(" ids: {:?}", &ids);
@@ -2110,7 +2133,15 @@ pub async fn force_delete_by_ids(
     }
     let old_model = old_model.unwrap();
     
-    info!("force_delete_by_ids: {}", serde_json::to_string(&old_model)?);
+    if !is_silent_mode {
+      info!(
+        "{} {}.{}: {}",
+        get_req_id(),
+        table,
+        method,
+        serde_json::to_string(&old_model)?,
+      );
+    }
     
     let mut args = QueryArgs::new();
     
