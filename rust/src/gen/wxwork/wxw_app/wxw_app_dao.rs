8cb88b54a0faa2413651c76b3aa578ce3e718e43
Sail <151263555@qq.com>
2024-07-24 14:58:58 +0800

    revertByIds 级联还原

--- rust/src/gen/wxwork/wxw_app/wxw_app_dao.rs
+++ rust/src/gen/wxwork/wxw_app/wxw_app_dao.rs
@@ -2440,6 +2440,7 @@ pub async fn revert_by_ids(
   
   let options = Options::from(options)
     .set_is_debug(Some(false));
+  let options = options.set_del_cache_key1s(get_cache_tables());
   let options = Some(options);
   
   let mut num = 0;
@@ -2452,31 +2453,30 @@ pub async fn revert_by_ids(
     
     let args: Vec<_> = args.into();
     
-    let options = Options::from(options.clone());
-    
-    let options = options.set_del_cache_key1s(get_cache_tables());
-    
-    let options = Some(options);
-    
-    num += execute(
-      sql,
-      args,
+    let mut old_model = find_one(
+      WxwAppSearch {
+        id: Some(id.clone()),
+        is_deleted: Some(1),
+        ..Default::default()
+      }.into(),
+      None,
       options.clone(),
     ).await?;
     
-    // 检查数据的唯一索引
-    {
-      let old_model = find_by_id(
+    if old_model.is_none() {
+      old_model = find_by_id(
         id.clone(),
         options.clone(),
       ).await?;
-      
-      if old_model.is_none() {
-        continue;
-      }
-      let old_model = old_model.unwrap();
-      
-      let mut input: WxwAppInput = old_model.into();
+    }
+    
+    if old_model.is_none() {
+      continue;
+    }
+    let old_model = old_model.unwrap();
+    
+    {
+      let mut input: WxwAppInput = old_model.clone().into();
       input.id = None;
       
       let models = find_by_unique(
@@ -2485,7 +2485,8 @@ pub async fn revert_by_ids(
         options.clone(),
       ).await?;
       
-      let models: Vec<WxwAppModel> = models.into_iter()
+      let models: Vec<WxwAppModel> = models
+        .into_iter()
         .filter(|item| 
           item.id != id
         )
@@ -2506,6 +2507,12 @@ pub async fn revert_by_ids(
         return Err(anyhow!(err_msg));
       }
     }
+    
+    num += execute(
+      sql,
+      args,
+      options.clone(),
+    ).await?;
     
   }
   
