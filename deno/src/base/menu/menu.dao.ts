79a1b1fe0ce2d5c31494090309cb114eaf2a3a6c
Sail <151263555@qq.com>
2024-07-29 11:40:53 +0800

    菜单国际化 initI18nMenus

--- deno/src/base/menu/menu.dao.ts
+++ deno/src/base/menu/menu.dao.ts
@@ -16,11 +16,19 @@ import {
   validateOption as validateOptionUsr,
 } from "/gen/base/usr/usr.dao.ts";
 
+import {
+  route_path as route_path_menu,
+} from "/gen/base/menu/menu.model.ts";
+
+import {
+  initN,
+} from "/src/base/i18n/i18n.ts";
+
 async function _getMenus(
   parent_id?: MenuId,
 ) {
   const args = new QueryArgs();
-  let sql = /*sql*/ `
+  let sql = `
     select distinct
       t.id,
       t.parent_id,
@@ -31,20 +39,20 @@ async function _getMenus(
     from base_menu t
     inner join base_tenant_menu
       on t.id = base_tenant_menu.menu_id
-      and base_tenant_menu.is_deleted = 0
+      and base_tenant_menu.is_deleted=0
     inner join base_tenant
       on base_tenant_menu.tenant_id = base_tenant.id
-      and base_tenant.is_deleted = 0
-      and base_tenant.is_enabled = 1
+      and base_tenant.is_deleted=0
+      and base_tenant.is_enabled=1
     inner join base_role_menu
       on t.id = base_role_menu.menu_id
-      and base_role_menu.is_deleted = 0
+      and base_role_menu.is_deleted=0
     inner join base_usr_role
-      on base_role_menu.role_id = base_usr_role.role_id
-      and base_usr_role.is_deleted = 0
+      on base_role_menu.role_id=base_usr_role.role_id
+      and base_usr_role.is_deleted=0
     where
-      t.is_deleted = 0
-      and t.is_enabled = 1
+      t.is_deleted=0
+      and t.is_enabled=1
   `;
   const usr_id = await get_usr_id();
   if (!usr_id) {
@@ -52,10 +60,10 @@ async function _getMenus(
   }
   const tenant_id = await getTenant_id(usr_id);
   if (tenant_id) {
-    sql += ` and base_tenant_menu.tenant_id = ${ args.push(tenant_id) }`;
+    sql += ` and base_tenant_menu.tenant_id=${ args.push(tenant_id) }`;
   }
   if (parent_id != null) {
-    sql += ` and t.parent_id = ${ args.push(parent_id) }`;
+    sql += ` and t.parent_id=${ args.push(parent_id) }`;
   }
   const usr_model = await validateOptionUsr(
     await findByIdUsr(
@@ -67,7 +75,7 @@ async function _getMenus(
   );
   const username = usr_model.username;
   if (username !== "admin") {
-    sql += ` and base_usr_role.usr_id = ${ args.push(usr_id) }`;
+    sql += ` and base_usr_role.usr_id=${ args.push(usr_id) }`;
   }
   
   const table = "base_menu";
@@ -101,11 +109,40 @@ async function _getMenus(
   
   if (server_i18n_enable === "false") {
     models = models.filter((item) => item.route_path !== "/base/i18n" && item.route_path !== "/base/lang");
+  } else {
+    await initI18nMenus(models);
   }
   
   models.sort((a, b) => a.order_by - b.order_by);
   
   return models;
+}
+
+/** 菜单国际化 */
+async function initI18nMenus(models: { lbl: string }[]) {
+  const n = initN(route_path_menu);
+  const set = new Set<string>();
+  for (let i = 0; i < models.length; i++) {
+    const item = models[i];
+    set.add(item.lbl);
+  }
+  const arrPrms = new Array<Promise<string>>(set.size);
+  let i = 0;
+  for (const item of set) {
+    arrPrms[i] = n(item);
+    i++;
+  }
+  const arr = await Promise.all(arrPrms);
+  const map = new Map<string, string>();
+  i = 0;
+  for (const item of set) {
+    map.set(item, arr[i]);
+    i++;
+  }
+  for (let i = 0; i < models.length; i++) {
+    const item = models[i];
+    item.lbl = map.get(item.lbl) || item.lbl;
+  }
 }
 
 export async function getMenus() {
