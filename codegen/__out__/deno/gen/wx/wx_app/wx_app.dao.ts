ad71eae5abfb4c00c290bba5a2a3dd4ae16bbb35
Sail <151263555@qq.com>
2024-08-20 09:34:10 +0800

    MARK

--- codegen/__out__/deno/gen/wx/wx_app/wx_app.dao.ts
+++ codegen/__out__/deno/gen/wx/wx_app/wx_app.dao.ts
@@ -206,6 +206,7 @@ async function getFromQuery(
   return fromQuery;
 }
 
+// MARK: findCount
 /** 根据条件查找小程序设置总数 */
 export async function findCount(
   search?: Readonly<WxAppSearch>,
@@ -252,11 +253,8 @@ export async function findCount(
   return result;
 }
 
-/**
- * 根据搜索条件和分页查找小程序设置列表
- * @param {WxAppSearch} search? 搜索条件
- * @param {SortInput|SortInput[]} sort? 排序
- */
+// MARK: findAll
+/** 根据搜索条件和分页查找小程序设置列表 */
 export async function findAll(
   search?: Readonly<WxAppSearch>,
   page?: Readonly<PageInput>,
@@ -468,6 +466,7 @@ export async function findAll(
   return result;
 }
 
+// MARK: setIdByLbl
 /** 根据lbl翻译业务字典, 外键关联id, 日期 */
 export async function setIdByLbl(
   input: WxAppInput,
@@ -508,9 +507,8 @@ export async function setIdByLbl(
   }
 }
 
-/**
- * 获取小程序设置字段注释
- */
+// MARK: getFieldComments
+/** 获取小程序设置字段注释 */
 export async function getFieldComments(): Promise<WxAppFieldComment> {
   const n = initN(route_path);
   const fieldComments: WxAppFieldComment = {
@@ -537,10 +535,8 @@ export async function getFieldComments(): Promise<WxAppFieldComment> {
   return fieldComments;
 }
 
-/**
- * 通过唯一约束获得小程序设置列表
- * @param {WxAppInput} search0
- */
+// MARK: findByUnique
+/** 通过唯一约束获得小程序设置列表 */
 export async function findByUnique(
   search0: Readonly<WxAppInput>,
   options?: {
@@ -656,6 +652,7 @@ export function equalsByUnique(
   return false;
 }
 
+// MARK: checkByUnique
 /** 通过唯一约束检查 小程序设置 是否已经存在 */
 export async function checkByUnique(
   input: Readonly<WxAppInput>,
@@ -693,10 +690,8 @@ export async function checkByUnique(
   return;
 }
 
-/**
- * 根据条件查找第一个小程序设置
- * @param {WxAppSearch} search?
- */
+// MARK: findOne
+/** 根据条件查找第一小程序设置 */
 export async function findOne(
   search?: Readonly<WxAppSearch>,
   sort?: SortInput | SortInput[],
@@ -743,10 +738,8 @@ export async function findOne(
   return model;
 }
 
-/**
- * 根据 id 查找小程序设置
- * @param {WxAppId} id
- */
+// MARK: findById
+/** 根据 id 查找小程序设置 */
 export async function findById(
   id?: WxAppId | null,
   options?: {
@@ -787,6 +780,7 @@ export async function findById(
   return model;
 }
 
+// MARK: findByIds
 /** 根据 ids 查找小程序设置 */
 export async function findByIds(
   ids: WxAppId[],
@@ -841,10 +835,8 @@ export async function findByIds(
   return models2;
 }
 
-/**
- * 根据搜索条件判断小程序设置是否存在
- * @param {WxAppSearch} search?
- */
+// MARK: exist
+/** 根据搜索条件判断小程序设置是否存在 */
 export async function exist(
   search?: Readonly<WxAppSearch>,
   options?: {
@@ -875,10 +867,8 @@ export async function exist(
   return exist;
 }
 
-/**
- * 根据id判断小程序设置是否存在
- * @param {WxAppId} id
- */
+// MARK: existById
+/** 根据id判断小程序设置是否存在 */
 export async function existById(
   id?: Readonly<WxAppId | null>,
   options?: {
@@ -929,6 +919,7 @@ export async function existById(
   return result;
 }
 
+// MARK: validateIsEnabled
 /** 校验小程序设置是否启用 */
 export async function validateIsEnabled(
   model: Readonly<WxAppModel>,
@@ -938,6 +929,7 @@ export async function validateIsEnabled(
   }
 }
 
+// MARK: validateOption
 /** 校验小程序设置是否存在 */
 export async function validateOption(
   model?: WxAppModel,
@@ -950,10 +942,8 @@ export async function validateOption(
   return model;
 }
 
-/**
- * 小程序设置增加和修改时校验输入
- * @param input 
- */
+// MARK: validate
+/** 小程序设置增加和修改时校验输入 */
 export async function validate(
   input: Readonly<WxAppInput>,
 ) {
@@ -1017,6 +1007,7 @@ export async function validate(
   
 }
 
+// MARK: create
 /** 创建 小程序设置 */
 export async function create(
   input: Readonly<WxAppInput>,
@@ -1057,6 +1048,7 @@ export async function create(
   return id;
 }
 
+// MARK: creates
 /** 批量创建 小程序设置 */
 export async function creates(
   inputs: WxAppInput[],
@@ -1314,13 +1306,13 @@ async function _creates(
   return ids2;
 }
 
-/**
- * 删除缓存
- */
+// MARK: delCache
+/** 删除缓存 */
 export async function delCache() {
   await delCacheCtx(`dao.sql.wx_wx_app`);
 }
 
+// MARK: updateTenantById
 /** 小程序设置 根据 id 修改 租户id */
 export async function updateTenantById(
   id: WxAppId,
@@ -1365,6 +1357,7 @@ export async function updateTenantById(
   return affectedRows;
 }
 
+// MARK: updateById
 /** 根据 id 修改 小程序设置 */
 export async function updateById(
   id: WxAppId,
@@ -1579,6 +1572,7 @@ export async function updateById(
   return id;
 }
 
+// MARK: deleteByIds
 /** 根据 ids 删除 小程序设置 */
 export async function deleteByIds(
   ids: WxAppId[],
@@ -1656,6 +1650,7 @@ export async function deleteByIds(
   return affectedRows;
 }
 
+// MARK: getIsEnabledById
 /** 根据 id 查找 小程序设置 是否已启用, 不存在则返回 undefined */
 export async function getIsEnabledById(
   id: WxAppId,
@@ -1676,6 +1671,7 @@ export async function getIsEnabledById(
   return is_enabled;
 }
 
+// MARK: enableByIds
 /** 根据 ids 启用或者禁用 小程序设置 */
 export async function enableByIds(
   ids: WxAppId[],
@@ -1724,6 +1720,7 @@ export async function enableByIds(
   return num;
 }
 
+// MARK: getIsLockedById
 /** 根据 id 查找 小程序设置 是否已锁定, 不存在则返回 undefined, 已锁定的不能修改和删除 */
 export async function getIsLockedById(
   id: WxAppId,
@@ -1744,6 +1741,7 @@ export async function getIsLockedById(
   return is_locked;
 }
 
+// MARK: lockByIds
 /** 根据 ids 锁定或者解锁 小程序设置 */
 export async function lockByIds(
   ids: WxAppId[],
@@ -1790,6 +1788,7 @@ export async function lockByIds(
   return num;
 }
 
+// MARK: revertByIds
 /** 根据 ids 还原 小程序设置 */
 export async function revertByIds(
   ids: WxAppId[],
@@ -1866,6 +1865,7 @@ export async function revertByIds(
   return num;
 }
 
+// MARK: forceDeleteByIds
 /** 根据 ids 彻底删除 小程序设置 */
 export async function forceDeleteByIds(
   ids: WxAppId[],
@@ -1924,7 +1924,8 @@ export async function forceDeleteByIds(
   
   return num;
 }
-  
+
+// MARK: findLastOrderBy
 /** 查找 小程序设置 order_by 字段的最大值 */
 export async function findLastOrderBy(
   options?: {
