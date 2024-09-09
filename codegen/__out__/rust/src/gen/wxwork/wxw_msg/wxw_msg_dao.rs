0f54101904ed95c1f18548d297c3a51e4cb14976
Sail <151263555@qq.com>
2024-09-09 18:30:40 +0800

    field_permit

--- codegen/__out__/rust/src/gen/wxwork/wxw_msg/wxw_msg_dao.rs
+++ codegen/__out__/rust/src/gen/wxwork/wxw_msg/wxw_msg_dao.rs
@@ -1749,6 +1749,7 @@ pub async fn update_tenant_by_id(
 // MARK: update_by_id
 /// 根据 id 修改企微消息
 #[allow(unused_mut)]
+#[allow(unused_variables)]
 pub async fn update_by_id(
   id: WxwMsgId,
   mut input: WxwMsgInput,
