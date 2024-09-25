064fa619c5547414c5dfe6c8f8c59c005060ae5a
Sail <151263555@qq.com>
2024-09-25 15:31:49 +0800

    业务字典不需要国际化

--- rust/src/gen/base/org/org_dao.rs
+++ rust/src/gen/base/org/org_dao.rs
@@ -2044,7 +2044,7 @@ pub async fn delete_by_ids(
     {
       let mut args = QueryArgs::new();
       let sql = "update base_usr_org set is_deleted=1 where org_id=? and is_deleted=0".to_owned();
-      args.push(id.as_ref().into());
+      args.push(id.clone().into());
       let args: Vec<_> = args.into();
       execute(
         sql,
@@ -2272,7 +2272,7 @@ pub async fn revert_by_ids(
     
     let sql = format!("update {table} set is_deleted=0 where id=? limit 1");
     
-    args.push(id.as_ref().into());
+    args.push(id.clone().into());
     
     let args: Vec<_> = args.into();
     
@@ -2410,7 +2410,7 @@ pub async fn force_delete_by_ids(
     
     let sql = format!("delete from {table} where id=? and is_deleted=1 limit 1");
     
-    args.push(id.as_ref().into());
+    args.push(id.clone().into());
     
     let args: Vec<_> = args.into();
     
@@ -2428,7 +2428,7 @@ pub async fn force_delete_by_ids(
     {
       let mut args = QueryArgs::new();
       let sql = "delete from base_usr_org where org_id=?".to_owned();
-      args.push(id.as_ref().into());
+      args.push(id.clone().into());
       let args: Vec<_> = args.into();
       execute(
         sql,
