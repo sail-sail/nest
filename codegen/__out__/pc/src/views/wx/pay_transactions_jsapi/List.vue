243e8748346fc4f6d9ae9659c249cd1c56f2d0aa
Sail <151263555@qq.com>
2024-11-04 09:51:53 +0800

    List 删除 usrStore.onLogin(initFrame)

--- codegen/__out__/pc/src/views/wx/pay_transactions_jsapi/List.vue
+++ codegen/__out__/pc/src/views/wx/pay_transactions_jsapi/List.vue
@@ -87,7 +87,7 @@
           <el-checkbox
             v-if="!isLocked"
             :set="search.is_deleted = search.is_deleted ?? 0"
-            v-model="search.is_deleted as number"
+            v-model="search.is_deleted"
             :false-value="0"
             :true-value="1"
             @change="recycleChg"
@@ -629,7 +629,6 @@ const {
   initSysI18ns
 } = useI18n(pagePath);
 
-const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 const fieldPermitStore = useFieldPermitStore();
 const dirtyStore = useDirtyStore();
@@ -1376,8 +1375,6 @@ watch(
     immediate: true,
   },
 );
-
-usrStore.onLogin(initFrame);
 
 initFrame();
 
--- pc/src/views/wx/wx_pay_notice/List.vue
+++ pc/src/views/wx/wx_pay_notice/List.vue
@@ -100,7 +100,7 @@
           <el-checkbox
             v-if="!isLocked"
             :set="search.is_deleted = search.is_deleted ?? 0"
-            v-model="search.is_deleted as number"
+            v-model="search.is_deleted"
             :false-value="0"
             :true-value="1"
             @change="recycleChg"
@@ -651,7 +651,6 @@ const {
   initSysI18ns
 } = useI18n(pagePath);
 
-const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 const fieldPermitStore = useFieldPermitStore();
 const dirtyStore = useDirtyStore();
@@ -1410,8 +1409,6 @@ watch(
     immediate: true,
   },
 );
-
-usrStore.onLogin(initFrame);
 
 initFrame();
 
