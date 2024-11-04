64aa6a91e14e1dd76a13f3d4021b8a59a9191424
Sail <151263555@qq.com>
2024-11-04 09:48:36 +0800

    List 删除 usrStore.onLogin(initFrame)

--- pc/src/views/wxwork/wxw_app/List.vue
+++ pc/src/views/wxwork/wxw_app/List.vue
@@ -128,7 +128,7 @@
           <el-checkbox
             v-if="!isLocked"
             :set="search.is_deleted = search.is_deleted ?? 0"
-            v-model="search.is_deleted as number"
+            v-model="search.is_deleted"
             :false-value="0"
             :true-value="1"
             @change="recycleChg"
@@ -735,7 +735,6 @@ const {
   initSysI18ns
 } = useI18n(pagePath);
 
-const usrStore = useUsrStore();
 const permitStore = usePermitStore();
 const fieldPermitStore = useFieldPermitStore();
 const dirtyStore = useDirtyStore();
@@ -1823,8 +1822,6 @@ watch(
     immediate: true,
   },
 );
-
-usrStore.onLogin(initFrame);
 
 initFrame();
 
