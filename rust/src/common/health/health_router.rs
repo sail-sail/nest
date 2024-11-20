8bf74e89a0c6eee374e365a73ffc3626835fcc01
Sail <151263555@qq.com>
2024-11-20 18:40:01 +0800

    resful_scope

--- rust/src/common/health/health_router.rs
+++ rust/src/common/health/health_router.rs
@@ -1,16 +1,14 @@
-use std::time::Instant;
+use serde::Deserialize;
 
-use tracing::error;
+use anyhow::Result;
 
 use poem::{
   handler, web::Query, Response,
 };
-use poem::http::StatusCode;
-use serde::Deserialize;
+
+use crate::common::context::Ctx;
 
 use super::health_resful;
-
-static KEY: &str = "lLpR1EKETWSb5x7TR4R32Q";
 
 #[derive(Deserialize)]
 pub struct HealthQuery {
@@ -19,49 +17,15 @@ pub struct HealthQuery {
 
 #[handler]
 pub async fn health(
-   Query(HealthQuery {
+  req: &poem::Request,
+  Query(HealthQuery {
     key,
-   }): Query<HealthQuery>,
-) -> Response {
-  
-  let key = key.unwrap_or_default();
-  
-  if key != KEY {
-    return Response::builder()
-      .status(StatusCode::UNAUTHORIZED)
-      .body("Unauthorized");
-  }
-  
-  let now0 = Instant::now();
-  
-  let res = health_resful::health_check().await;
-  
-  if res.is_err() {
-    let err = res.err().unwrap();
-    error!("{}", err);
-    return Response::builder()
-      .status(StatusCode::INTERNAL_SERVER_ERROR)
-      .body(err.to_string());
-  }
-  
-  let mut response = Response::builder();
-  response = response.header("Content-Type", "application/json");
-  let mut response = response.body("OK");
-  
-  let headers = response.headers_mut();
-  let now1 = Instant::now();
-  let response_time = format!("app;dur={}", now1.saturating_duration_since(now0).as_millis());
-  let response_time = poem::http::header::HeaderValue::from_str(&response_time);
-  let response_time = match response_time {
-    Ok(response_time) => response_time,
-    Err(err) => {
-      error!("{}", err);
-      return Response::builder()
-        .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .body(err.to_string())
-    }
-  };
-  headers.insert("Server-Timing", response_time);
-  
-  response
+  }): Query<HealthQuery>,
+) -> Result<Response> {
+  Ctx::resful_builder(Some(req))
+    .with_auth()?
+    .build()
+    .resful_scope({
+      health_resful::health_check(key)
+    }).await
 }
