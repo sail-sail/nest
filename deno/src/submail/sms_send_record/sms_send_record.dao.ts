a10b55877d69b579f3d7cbfcaf9818eaf4be482b
Sail <151263555@qq.com>
2024-12-07 20:16:09 +0800

    xsendSms 发送模板短信

--- deno/src/submail/sms_send_record/sms_send_record.dao.ts
+++ deno/src/submail/sms_send_record/sms_send_record.dao.ts
@@ -25,6 +25,162 @@ import {
 
 const SMS_URL = "https://api-v4.mysubmail.com/sms/send.json";
 
+const XSMS_URL = "https://api-v4.mysubmail.com/sms/xsend.json";
+
+/**
+ * 发送模板短信
+ * https://www.mysubmail.com/documents/OOVyh
+ */
+export async function xsendSms(
+  send_to: string,
+  project: string,
+  vars: Record<string, string>,
+  tag?: string,
+  is_log = true,
+): Promise<boolean> {
+  if (!send_to) {
+    throw new Error("手机号不能为空");
+  }
+  
+  const [
+    status_dict_models,
+  ] = await getDict([
+    "submail_sms_send_record_status",
+  ]);
+  
+  const sms_app_model = await validateOptionSmsSendRecord(
+    await findOneSmsSendRecord(
+      {
+        is_enabled: [ 1 ],
+      },
+    ),
+  );
+  if (!tag) {
+    tag = undefined;
+  }
+  const appid = sms_app_model.appid;
+  const signature = sms_app_model.appkey;
+  const sms_app_id = sms_app_model.id;
+  const sms_app_id_lbl = sms_app_model.lbl;
+  const is_paused = sms_app_model.is_paused;
+  const tenant_id = sms_app_model.tenant_id;
+  
+  if (is_paused && is_log) {
+    const status = SmsSendRecordStatus.Paused;
+    const status_lbl = status_dict_models.find((item) => item.val === status)?.lbl;
+    await createSmsSendRecord({
+      sms_app_id,
+      sms_app_id_lbl,
+      send_to,
+      send_time: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      content: JSON.stringify({
+        project,
+        vars,
+      }),
+      tag,
+      status,
+      status_lbl,
+      tenant_id,
+    });
+    return false;
+  }
+  
+  let sms_send_record_id: SmsSendRecordId | undefined;
+  
+  if (is_log) {
+    const status = SmsSendRecordStatus.Sending;
+    const status_lbl = status_dict_models.find((item) => item.val === status)?.lbl;
+    sms_send_record_id = await createSmsSendRecord({
+      sms_app_id,
+      sms_app_id_lbl,
+      send_to,
+      content: JSON.stringify({
+        project,
+        vars,
+      }),
+      tag,
+      status,
+      status_lbl,
+      tenant_id,
+    });
+  }
+  
+  let data: {
+    status: "success" | "error";
+    msg?: string;
+  } | undefined;
+  
+  try {
+    const body = {
+      appid,
+      signature,
+      to: send_to,
+      project,
+      vars: JSON.stringify(vars),
+      tag,
+    };
+    const bodyStr = JSON.stringify(body);
+    log("xsendSms.body", bodyStr);
+    const res = await fetch(XSMS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: bodyStr,
+    });
+    const dataStr = await res.text();
+    log("xsendSms.return", dataStr);
+    data = JSON.parse(dataStr);
+  } catch (err) {
+    if (sms_send_record_id) {
+      const send_time = dayjs().format("YYYY-MM-DD HH:mm:ss");
+      const status = SmsSendRecordStatus.Failure;
+      const status_lbl = status_dict_models.find((item) => item.val === status)?.lbl;
+      await updateByIdSmsSendRecord(
+        sms_send_record_id,
+        {
+          status,
+          status_lbl,
+          send_time,
+          msg: err.message,
+        },
+      );
+    }
+    error("xsendSms.error", err);
+    return false;
+  }
+  
+  if (sms_send_record_id && data) {
+    const send_time = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    if (data.status === "success") {
+      const status = SmsSendRecordStatus.Success;
+      const status_lbl = status_dict_models.find((item) => item.val === status)?.lbl;
+      await updateByIdSmsSendRecord(
+        sms_send_record_id,
+        {
+          status,
+          status_lbl,
+          send_time,
+        },
+      );
+      return true;
+    }
+    const status = SmsSendRecordStatus.Failure;
+    const status_lbl = status_dict_models.find((item) => item.val === status)?.lbl;
+    await updateByIdSmsSendRecord(
+      sms_send_record_id,
+      {
+        status,
+        status_lbl,
+        send_time,
+        msg: data.msg,
+      },
+    );
+    return false;
+  }
+  return true;
+}
+
 /**
  * 发送短信
  * https://www.mysubmail.com/documents/FppOR3
