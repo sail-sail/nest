dd7b32d73799036bc86f7a084b8fe79462d43cbb
Sail <151263555@qq.com>
2024-12-11 15:09:17 +0800

    createReturn

--- deno/gen/wshop/card_recharge/card_recharge.dao.ts
+++ deno/gen/wshop/card_recharge/card_recharge.dao.ts
@@ -1015,6 +1015,57 @@ export async function validate(
   
 }
 
+// MARK: createReturn
+/** 创建 会员卡充值记录 并返回 */
+export async function createReturn(
+  input: Readonly<CardRechargeInput>,
+  options?: {
+    is_debug?: boolean;
+    uniqueType?: UniqueType;
+    hasDataPermit?: boolean;
+    is_silent_mode?: boolean;
+  },
+): Promise<CardRechargeModel> {
+  
+  const table = "wshop_card_recharge";
+  const method = "createReturn";
+  
+  const is_debug = get_is_debug(options?.is_debug);
+  
+  if (is_debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (input) {
+      msg += ` input:${ JSON.stringify(input) }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options ?? { };
+    options.is_debug = false;
+  }
+  
+  if (!input) {
+    throw new Error(`input is required in dao: ${ table }`);
+  }
+  
+  const [
+    id,
+  ] = await _creates([ input ], options);
+  
+  const model = await validateOption(
+    await findOne(
+      {
+        id,
+      },
+      undefined,
+      options,
+    ),
+  );
+  
+  return model;
+}
+
 // MARK: create
 /** 创建 会员卡充值记录 */
 export async function create(
@@ -1054,6 +1105,43 @@ export async function create(
   ] = await _creates([ input ], options);
   
   return id;
+}
+
+// MARK: createsReturn
+/** 批量创建 会员卡充值记录 并返回 */
+export async function createsReturn(
+  inputs: CardRechargeInput[],
+  options?: {
+    is_debug?: boolean;
+    uniqueType?: UniqueType;
+    hasDataPermit?: boolean;
+    is_silent_mode?: boolean;
+  },
+): Promise<CardRechargeModel[]> {
+  
+  const table = "wshop_card_recharge";
+  const method = "createsReturn";
+  
+  const is_debug = get_is_debug(options?.is_debug);
+  
+  if (is_debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (inputs) {
+      msg += ` inputs:${ JSON.stringify(inputs) }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options ?? { };
+    options.is_debug = false;
+  }
+  
+  const ids = await _creates(inputs, options);
+  
+  const models = await findByIds(ids, options);
+  
+  return models;
 }
 
 // MARK: creates
@@ -1508,7 +1596,7 @@ export async function updateById(
   }
   
   if (!is_silent_mode) {
-    log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
+    log(`${ table }.${ method }.old_model: ${ JSON.stringify(oldModel) }`);
   }
   
   return id;
@@ -1557,7 +1645,7 @@ export async function deleteByIds(
       continue;
     }
     if (!is_silent_mode) {
-      log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
+      log(`${ table }.${ method }.old_model: ${ JSON.stringify(oldModel) }`);
     }
     const args = new QueryArgs();
     let sql = `update wshop_card_recharge set is_deleted=1`;
