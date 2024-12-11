3bfce03223a4eb399f74e112b6f51f5ba51cc42b
Sail <151263555@qq.com>
2024-12-11 10:49:38 +0800

    createReturn

--- codegen/__out__/deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
+++ codegen/__out__/deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
@@ -942,6 +942,57 @@ export async function validate(
   
 }
 
+// MARK: createReturn
+/** 创建 企微用户 并返回 */
+export async function createReturn(
+  input: Readonly<WxwUsrInput>,
+  options?: {
+    is_debug?: boolean;
+    uniqueType?: UniqueType;
+    hasDataPermit?: boolean;
+    is_silent_mode?: boolean;
+  },
+): Promise<WxwUsrModel> {
+  
+  const table = "wxwork_wxw_usr";
+  const method = "createReturn";
+  
+  const is_debug = get_is_debug(options?.is_debug);
+  
+  if (is_debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (input) {
+      msg += ` input:${ JSON.stringify(input) }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options ?? { };
+    options.is_debug = false;
+  }
+  
+  if (!input) {
+    throw new Error(`input is required in dao: ${ table }`);
+  }
+  
+  const [
+    id,
+  ] = await _creates([ input ], options);
+  
+  const model = await validateOption(
+    await findOne(
+      {
+        id,
+      },
+      undefined,
+      options,
+    ),
+  );
+  
+  return model;
+}
+
 // MARK: create
 /** 创建 企微用户 */
 export async function create(
@@ -981,6 +1032,43 @@ export async function create(
   ] = await _creates([ input ], options);
   
   return id;
+}
+
+// MARK: createsReturn
+/** 批量创建 企微用户 并返回 */
+export async function createsReturn(
+  inputs: WxwUsrInput[],
+  options?: {
+    is_debug?: boolean;
+    uniqueType?: UniqueType;
+    hasDataPermit?: boolean;
+    is_silent_mode?: boolean;
+  },
+): Promise<WxwUsrModel[]> {
+  
+  const table = "wxwork_wxw_usr";
+  const method = "createsReturn";
+  
+  const is_debug = get_is_debug(options?.is_debug);
+  
+  if (is_debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (inputs) {
+      msg += ` inputs:${ JSON.stringify(inputs) }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+    options = options ?? { };
+    options.is_debug = false;
+  }
+  
+  const ids = await _creates(inputs, options);
+  
+  const models = await findByIds(ids, options);
+  
+  return models;
 }
 
 // MARK: creates
@@ -1545,7 +1633,7 @@ export async function updateById(
   }
   
   if (!is_silent_mode) {
-    log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
+    log(`${ table }.${ method }.old_model: ${ JSON.stringify(oldModel) }`);
   }
   
   return id;
@@ -1596,7 +1684,7 @@ export async function deleteByIds(
       continue;
     }
     if (!is_silent_mode) {
-      log(`${ table }.${ method }: ${ JSON.stringify(oldModel) }`);
+      log(`${ table }.${ method }.old_model: ${ JSON.stringify(oldModel) }`);
     }
     const args = new QueryArgs();
     let sql = `update wxwork_wxw_usr set is_deleted=1`;
