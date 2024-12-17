19135190c961bdb85d292cdb3a18aa5699684f18
Sail <151263555@qq.com>
2024-12-17 10:30:42 +0800

    eslint

--- pc/src/views/wx/wxo_app/List.vue
+++ pc/src/views/wx/wxo_app/List.vue
@@ -12,9 +12,9 @@
     un-overflow-auto
   >
     <el-form
+      ref="searchFormRef"
       v-search-form-item-width-auto="inited"
       
-      ref="searchFormRef"
       size="default"
       :model="search"
       inline-message
@@ -75,9 +75,9 @@
         >
           <DictSelect
             :model-value="is_enabled_search[0]"
-            @update:model-value="($event != null && $event !== '') ? is_enabled_search = [ $event ] : is_enabled_search = [ ]"
             code="is_enabled"
             :placeholder="`${ ns('请选择') } ${ n('启用') }`"
+            @update:model-value="($event != null && $event !== '') ? is_enabled_search = [ $event ] : is_enabled_search = [ ]"
             @change="onSearch(false)"
           ></DictSelect>
         </el-form-item>
@@ -127,8 +127,8 @@
           
           <el-checkbox
             v-if="!isLocked"
-            :set="search.is_deleted = search.is_deleted ?? 0"
             v-model="search.is_deleted"
+            :set="search.is_deleted = search.is_deleted ?? 0"
             :false-value="0"
             :true-value="1"
             @change="recycleChg"
@@ -493,8 +493,8 @@
         row-key="id"
         :default-sort="defaultSort"
         :empty-text="inited ? undefined : ns('加载中...')"
-        @select="selectChg"
-        @select-all="selectChg"
+        @select="onSelect"
+        @select-all="onSelect"
         @row-click="onRow"
         @sort-change="onSortChange"
         @header-dragend="headerDragend"
@@ -756,7 +756,7 @@
   
   <ImportPercentageDialog
     :percentage="importPercentage"
-    :dialog_visible="isImporting"
+    :dialog-visible="isImporting"
     @stop="stopImport"
   ></ImportPercentageDialog>
   
@@ -830,7 +830,7 @@ const props = defineProps<{
   isListSelectDialog?: string;
   ids?: string[]; //ids
   selectedIds?: WxoAppId[]; //已选择行的id列表
-  isMultiple?: Boolean; //是否多选
+  isMultiple?: boolean; //是否多选
   id?: WxoAppId; // ID
   code?: string; // 原始ID
   code_like?: string; // 原始ID
@@ -899,7 +899,7 @@ const isFocus = $computed(() => props.isFocus !== "0");
 const isListSelectDialog = $computed(() => props.isListSelectDialog === "1");
 
 /** 表格 */
-let tableRef = $ref<InstanceType<typeof ElTable>>();
+const tableRef = $ref<InstanceType<typeof ElTable>>();
 
 /** 查询 */
 function initSearch() {
@@ -909,6 +909,7 @@ function initSearch() {
   if (props.propsNotReset && props.propsNotReset.length > 0) {
     for (let i = 0; i < props.propsNotReset.length; i++) {
       const key = props.propsNotReset[i];
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (search as any)[key] = (builtInSearch as any)[key];
     }
   }
@@ -990,7 +991,7 @@ async function onIdsChecked() {
 }
 
 /** 分页功能 */
-let {
+const {
   page,
   pageSizes,
   pgSizeChg,
@@ -1005,9 +1006,16 @@ let {
 ));
 
 /** 表格选择功能 */
-let {
-  selectedIds,
-  selectChg,
+const tableSelected = useSelect<WxoAppModel, WxoAppId>(
+  $$(tableRef),
+  {
+    multiple: $$(multiple),
+    isListSelectDialog,
+  },
+);
+
+const {
+  onSelect,
   rowClassName,
   onRow,
   onRowUp,
@@ -1017,13 +1025,9 @@ let {
   onRowHome,
   onRowEnd,
   tableFocus,
-} = $(useSelect<WxoAppModel, WxoAppId>(
-  $$(tableRef),
-  {
-    multiple: $$(multiple),
-    isListSelectDialog,
-  },
-));
+} = tableSelected;
+
+let selectedIds = $(tableSelected.selectedIds);
 
 watch(
   () => selectedIds,
@@ -1230,7 +1234,7 @@ function getTableColumns(): ColumnType[] {
 }
 
 /** 表格列 */
-let tableColumns = $ref<ColumnType[]>(getTableColumns());
+const tableColumns = $ref<ColumnType[]>(getTableColumns());
 
 /** 表格列标签国际化 */
 watchEffect(() => {
@@ -1245,7 +1249,7 @@ watchEffect(() => {
 });
 
 /** 表格列 */
-let {
+const {
   headerDragend,
   resetColumns,
   storeColumns,
@@ -1257,7 +1261,7 @@ let {
   },
 ));
 
-let detailRef = $ref<InstanceType<typeof Detail>>();
+const detailRef = $ref<InstanceType<typeof Detail>>();
 
 /** 刷新表格 */
 async function dataGrid(
@@ -1379,7 +1383,7 @@ async function onSortChange(
   await dataGrid();
 }
 
-let exportExcel = $ref(useExportExcel(pagePath));
+const exportExcel = $ref(useExportExcel(pagePath));
 
 /** 导出Excel */
 async function onExport() {
@@ -1475,7 +1479,7 @@ async function onInsert() {
   await openAdd();
 }
 
-let uploadFileDialogRef = $ref<InstanceType<typeof UploadFileDialog>>();
+const uploadFileDialogRef = $ref<InstanceType<typeof UploadFileDialog>>();
 
 let importPercentage = $ref(0);
 let isImporting = $ref(false);
