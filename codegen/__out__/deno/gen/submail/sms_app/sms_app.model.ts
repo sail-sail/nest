e873736b87d3757d82f317f135682061d88636c7
Sail <151263555@qq.com>
2025-01-02 10:04:57 +0800

    pkg

--- codegen/__out__/deno/gen/submail/sms_app/sms_app.model.ts
+++ codegen/__out__/deno/gen/submail/sms_app/sms_app.model.ts
@@ -85,7 +85,9 @@ export const canSortInApiSmsApp = {
 
 /** 短信应用 检测字段是否允许前端排序 */
 export function checkSortSmsApp(sort?: SortInput[]) {
-  if (!sort) return;
+  if (!sort) {
+    return;
+  }
   for (const item of sort) {
     const order = item.order;
     if (
