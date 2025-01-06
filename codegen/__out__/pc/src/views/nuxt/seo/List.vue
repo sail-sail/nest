c45e0864319f04a20e447799f8dd6d09e4b368bb
Sail <151263555@qq.com>
2025-01-06 10:29:50 +0800

    remove fieldPermitStore

--- codegen/__out__/pc/src/views/nuxt/seo/List.vue
+++ codegen/__out__/pc/src/views/nuxt/seo/List.vue
@@ -709,7 +709,6 @@ const {
 } = useI18n(pagePath);
 
 const permitStore = usePermitStore();
-const fieldPermitStore = useFieldPermitStore();
 const dirtyStore = useDirtyStore();
 
 const clearDirty = dirtyStore.onDirty(onRefresh, pageName);
