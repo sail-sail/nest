e3f0af12cf6777474a75dcac9d9ff5d2f1274375
Sail <151263555@qq.com>
2025-01-19 22:35:56 +0800

    isUseI18n

--- codegen/src/tables/tables.ts
+++ codegen/src/tables/tables.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "../config";
 
-/** 是否使用国际化 */
-export const isUseI18n = false;
-
 import base from "./base/base";
 
 // 电子档案
 import eams from "./eams/eams";
+
+/** 是否使用国际化 */
+export const isUseI18n = false;
 
 export default defineConfig({
   
--- pc/src/views/eams/archive/List.vue
+++ pc/src/views/eams/archive/List.vue
@@ -31,12 +31,12 @@
       
       <template v-if="(builtInSearch?.code == null && (showBuildIn || builtInSearch?.code_like == null))">
         <el-form-item
-          :label="n('编号')"
+          label="编号"
           prop="code_like"
         >
           <CustomInput
             v-model="search.code_like"
-            :placeholder="`${ ns('请输入') } ${ n('编号') }`"
+            placeholder="编号"
             @clear="onSearchClear"
           ></CustomInput>
         </el-form-item>
@@ -44,12 +44,12 @@
       
       <template v-if="(builtInSearch?.lbl == null && (showBuildIn || builtInSearch?.lbl_like == null))">
         <el-form-item
-          :label="n('名称')"
+          label="名称"
           prop="lbl_like"
         >
           <CustomInput
             v-model="search.lbl_like"
-            :placeholder="`${ ns('请输入') } ${ n('名称') }`"
+            placeholder="名称"
             @clear="onSearchClear"
           ></CustomInput>
         </el-form-item>
@@ -77,7 +77,7 @@
               :disabled="selectedIds.length === 0"
               @change="onIdsChecked"
             >
-              <span>{{ ns('已选择') }}</span>
+              <span>已选择</span>
               <span
                 v-if="selectedIds.length > 0"
                 un-m="l-0.5"
@@ -88,7 +88,7 @@
             </el-checkbox>
             <el-icon
               v-show="selectedIds.length > 0"
-              :title="ns('清空已选择')"
+              title="清空已选择"
               un-cursor-pointer
               un-text="hover:red"
               @click="onEmptySelected"
@@ -105,7 +105,7 @@
             :true-value="1"
             @change="recycleChg"
           >
-            <span>{{ ns('回收站') }}</span>
+            <span>回收站</span>
           </el-checkbox>
         </div>
       </el-form-item>
@@ -123,7 +123,7 @@
           <template #icon>
             <ElIconSearch />
           </template>
-          <span>{{ ns('查询') }}</span>
+          <span>查询</span>
         </el-button>
         
         <el-button
@@ -133,7 +133,7 @@
           <template #icon>
             <ElIconDelete />
           </template>
-          <span>{{ ns('重置') }}</span>
+          <span>重置</span>
         </el-button>
         
         <div
@@ -172,7 +172,7 @@
         <template #icon>
           <ElIconCirclePlus />
         </template>
-        <span>{{ ns('新增') }}</span>
+        <span>新增</span>
       </el-button>
       
       <el-button
@@ -184,7 +184,7 @@
         <template #icon>
           <ElIconCopyDocument />
         </template>
-        <span>{{ ns('复制') }}</span>
+        <span>复制</span>
       </el-button>
       
       <el-button
@@ -196,7 +196,7 @@
         <template #icon>
           <ElIconEdit />
         </template>
-        <span>{{ ns('编辑') }}</span>
+        <span>编辑</span>
       </el-button>
       
       <el-button
@@ -208,7 +208,7 @@
         <template #icon>
           <ElIconCircleClose />
         </template>
-        <span>{{ ns('删除') }}</span>
+        <span>删除</span>
       </el-button>
       
       <el-button
@@ -218,7 +218,7 @@
         <template #icon>
           <ElIconReading />
         </template>
-        <span>{{ ns('查看') }}</span>
+        <span>查看</span>
       </el-button>
       
       <el-button
@@ -228,7 +228,7 @@
         <template #icon>
           <ElIconRefresh />
         </template>
-        <span>{{ ns('刷新') }}</span>
+        <span>刷新</span>
       </el-button>
       
       <el-dropdown
@@ -243,18 +243,18 @@
             v-if="exportExcel.workerStatus === 'RUNNING'"
             un-text="red"
           >
-            {{ ns('正在导出') }}
+            正在导出
           </span>
           <span
             v-else-if="exportExcel.loading"
             un-text="red"
           >
-            {{ ns('正在为导出加载数据') }}
+            正在为导出加载数据
           </span>
           <span
             v-else
           >
-            {{ ns('更多操作') }}
+            更多操作
           </span>
           <el-icon>
             <ElIconArrowDown />
@@ -271,7 +271,7 @@
               un-justify-center
               @click="onExport"
             >
-              <span>{{ ns('导出') }}</span>
+              <span>导出</span>
             </el-dropdown-item>
             
             <el-dropdown-item
@@ -279,7 +279,7 @@
               un-justify-center
               @click="onCancelExport"
             >
-              <span un-text="red">{{ ns('取消导出') }}</span>
+              <span un-text="red">取消导出</span>
             </el-dropdown-item>
             
             <el-dropdown-item
@@ -287,7 +287,7 @@
               un-justify-center
               @click="onImportExcel"
             >
-              <span>{{ ns('导入') }}</span>
+              <span>导入</span>
             </el-dropdown-item>
             
           </el-dropdown-menu>
@@ -307,7 +307,7 @@
         <template #icon>
           <ElIconCircleCheck />
         </template>
-        <span>{{ ns('还原') }}</span>
+        <span>还原</span>
       </el-button>
       
       <el-button
@@ -319,7 +319,7 @@
         <template #icon>
           <ElIconCircleClose />
         </template>
-        <span>{{ ns('彻底删除') }}</span>
+        <span>彻底删除</span>
       </el-button>
       
       <el-button
@@ -329,7 +329,7 @@
         <template #icon>
           <ElIconReading />
         </template>
-        <span>{{ ns('查看') }}</span>
+        <span>查看</span>
       </el-button>
       
       <el-button
@@ -339,7 +339,7 @@
         <template #icon>
           <ElIconRefresh />
         </template>
-        <span>{{ ns('刷新') }}</span>
+        <span>刷新</span>
       </el-button>
       
       <el-dropdown
@@ -353,18 +353,18 @@
           <span
             v-if="exportExcel.workerStatus === 'RUNNING'"
           >
-            {{ ns('正在导出') }}
+            正在导出
           </span>
           <span
             v-else-if="exportExcel.loading"
             un-text="red"
           >
-            {{ ns('正在为导出加载数据') }}
+            正在为导出加载数据
           </span>
           <span
             v-else
           >
-            {{ ns('更多操作') }}
+            更多操作
           </span>
           <el-icon>
             <ElIconArrowDown />
@@ -381,7 +381,7 @@
               un-justify-center
               @click="onExport"
             >
-              <span>{{ ns('导出') }}</span>
+              <span>导出</span>
             </el-dropdown-item>
             
             <el-dropdown-item
@@ -389,7 +389,7 @@
               un-justify-center
               @click="onCancelExport"
             >
-              <span un-text="red">{{ ns('取消导出') }}</span>
+              <span un-text="red">取消导出</span>
             </el-dropdown-item>
             
           </el-dropdown-menu>
@@ -409,7 +409,7 @@
       @reset-columns="resetColumns"
       @store-columns="storeColumns"
     >
-      {{ ns('列操作') }}
+      列操作
     </TableShowColumns>
     
   </div>
@@ -432,7 +432,7 @@
         height="100%"
         row-key="id"
         :default-sort="defaultSort"
-        :empty-text="inited ? undefined : ns('加载中...')"
+        :empty-text="inited ? undefined : '加载中...'"
         @select="onSelect"
         @select-all="onSelect"
         @row-click="onRow"
@@ -1001,18 +1001,6 @@ function getTableColumns(): ColumnType[] {
 /** 表格列 */
 const tableColumns = $ref<ColumnType[]>(getTableColumns());
 
-/** 表格列标签国际化 */
-watchEffect(() => {
-  const tableColumns2 = getTableColumns();
-  for (let i = 0; i < tableColumns2.length; i++) {
-    const column2 = tableColumns2[i];
-    const column = tableColumns.find((item) => item.prop === column2.prop);
-    if (column) {
-      column.label = n(column2.label);
-    }
-  }
-});
-
 /** 表格列 */
 const {
   headerDragend,
@@ -1174,13 +1162,13 @@ async function openAdd() {
     return;
   }
   if (!permit("add")) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   const {
     changedIds,
   } = await detailRef.showDialog({
-    title: await nsAsync("新增") + " " + await nsAsync("全宗设置"),
+    title: "新增 全宗设置",
     action: "add",
     builtInModel,
     showBuildIn: $$(showBuildIn),
@@ -1206,11 +1194,11 @@ async function openCopy() {
     return;
   }
   if (!permit("add")) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要 复制 的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要 复制 的 全宗设置");
     return;
   }
   const id = selectedIds[selectedIds.length - 1];
@@ -1218,7 +1206,7 @@ async function openCopy() {
   const {
     changedIds,
   } = await detailRef.showDialog({
-    title: await nsAsync("复制") + " " + await nsAsync("全宗设置"),
+    title: "复制 全宗设置",
     action: "copy",
     builtInModel,
     showBuildIn: $$(showBuildIn),
@@ -1270,14 +1258,14 @@ async function onImportExcel() {
     return;
   }
   const header: { [key: string]: string } = {
-    [ await nAsync("编号") ]: "code",
-    [ await nAsync("名称") ]: "lbl",
-    [ await nAsync("关联单位") ]: "company_id_lbl",
-    [ await nAsync("排序") ]: "order_by",
-    [ await nAsync("备注") ]: "rem",
+    [ "编号" ]: "code",
+    [ "名称" ]: "lbl",
+    [ "关联单位" ]: "company_id_lbl",
+    [ "排序" ]: "order_by",
+    [ "备注" ]: "rem",
   };
   const file = await uploadFileDialogRef.showDialog({
-    title: await nsAsync("批量导入"),
+    title: "批量导入",
     accept: ".xlsx",
   });
   tableFocus();
@@ -1290,7 +1278,7 @@ async function onImportExcel() {
   let msg: VNode | undefined = undefined;
   let succNum = 0;
   try {
-    const messageHandler = ElMessage.info(await nsAsync("正在导入..."));
+    const messageHandler = ElMessage.info("正在导入...");
     const models = await getExcelData<ArchiveInput>(
       file,
       header,
@@ -1339,18 +1327,18 @@ async function openEdit() {
     return;
   }
   if (!permit("edit")) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要编辑的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要编辑的 全宗设置");
     return;
   }
   const ids = selectedIds;
   const {
     changedIds,
   } = await detailRef.showDialog({
-    title: await nsAsync("编辑") + " " + await nsAsync("全宗设置"),
+    title: "编辑 全宗设置",
     action: "edit",
     builtInModel,
     showBuildIn: $$(showBuildIn),
@@ -1409,7 +1397,7 @@ async function openView() {
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要查看的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要查看的 全宗设置");
     return;
   }
   const search = getDataSearch();
@@ -1418,7 +1406,7 @@ async function openView() {
   const {
     changedIds,
   } = await detailRef.showDialog({
-    title: await nsAsync("查看") + " " + await nsAsync("全宗设置"),
+    title: "查看 全宗设置",
     action: "view",
     builtInModel,
     showBuildIn: $$(showBuildIn),
@@ -1444,17 +1432,17 @@ async function onDeleteByIds() {
     return;
   }
   if (!permit("delete")) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要删除的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要删除的 全宗设置");
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定删除已选择的 {0} {1}", selectedIds.length, await nsAsync("全宗设置")) }?`, {
-      confirmButtonText: await nsAsync("确定"),
-      cancelButtonText: await nsAsync("取消"),
+    await ElMessageBox.confirm(`确定删除已选择的 ${ selectedIds.length } 全宗设置?`, {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
       type: "warning",
     });
   } catch (err) {
@@ -1466,7 +1454,7 @@ async function onDeleteByIds() {
     selectedIds = [ ];
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
-    ElMessage.success(await nsAsync("删除 {0} {1} 成功", num, await nsAsync("全宗设置")));
+    ElMessage.success(`删除 ${ num } 全宗设置 成功`);
     emit("remove", num);
   }
 }
@@ -1478,17 +1466,17 @@ async function onForceDeleteByIds() {
     return;
   }
   if (!permit("forceDelete")) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要 彻底删除 的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要 彻底删除 的 全宗设置");
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定 彻底删除 已选择的 {0} {1}", selectedIds.length, await nsAsync("全宗设置")) }?`, {
-      confirmButtonText: await nsAsync("确定"),
-      cancelButtonText: await nsAsync("取消"),
+    await ElMessageBox.confirm(`确定 彻底删除 已选择的 ${ selectedIds.length } 全宗设置?`, {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
       type: "warning",
     });
   } catch (err) {
@@ -1497,7 +1485,7 @@ async function onForceDeleteByIds() {
   const num = await forceDeleteByIds(selectedIds);
   if (num) {
     selectedIds = [ ];
-    ElMessage.success(await nsAsync("彻底删除 {0} {1} 成功", num, await nsAsync("全宗设置")));
+    ElMessage.success(`彻底删除 ${ num } 全宗设置 成功`);
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
   }
@@ -1510,17 +1498,17 @@ async function onRevertByIds() {
     return;
   }
   if (permit("delete") === false) {
-    ElMessage.warning(await nsAsync("无权限"));
+    ElMessage.warning("无权限");
     return;
   }
   if (selectedIds.length === 0) {
-    ElMessage.warning(await nsAsync("请选择需要还原的 {0}", await nsAsync("全宗设置")));
+    ElMessage.warning("请选择需要还原的 全宗设置");
     return;
   }
   try {
-    await ElMessageBox.confirm(`${ await nsAsync("确定还原已选择的 {0} {1}", selectedIds.length, await nsAsync("全宗设置")) }?`, {
-      confirmButtonText: await nsAsync("确定"),
-      cancelButtonText: await nsAsync("取消"),
+    await ElMessageBox.confirm(`确定还原已选择的 ${ selectedIds.length } 全宗设置?`, {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
       type: "warning",
     });
   } catch (err) {
@@ -1531,28 +1519,9 @@ async function onRevertByIds() {
     search.is_deleted = 0;
     dirtyStore.fireDirty(pageName);
     await dataGrid(true);
-    ElMessage.success(await nsAsync("还原 {0} {1} 成功", num, await nsAsync("全宗设置")));
+    ElMessage.success(`还原 ${ num } 全宗设置 成功`);
     emit("revert", num);
   }
-}
-
-/** 初始化ts中的国际化信息 */
-async function initI18nsEfc() {
-  const codes: string[] = [
-    "编号",
-    "名称",
-    "关联单位",
-    "排序",
-    "备注",
-    "创建人",
-    "创建时间",
-    "更新人",
-    "更新时间",
-  ];
-  await Promise.all([
-    initListI18ns(),
-    initI18ns(codes),
-  ]);
 }
 
 async function focus() {
@@ -1578,7 +1547,6 @@ watch(
 async function initFrame() {
   initColumns(tableColumns);
   await Promise.all([
-    initI18nsEfc(),
     dataGrid(true),
   ]);
   inited = true;
