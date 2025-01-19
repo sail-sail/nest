c4fbaec740129c5366a0e9b831329ff3925b2411
Sail <151263555@qq.com>
2025-01-19 12:50:15 +0800

    isUseI18n

--- codegen/__out__/pc/src/views/base/i18n/Detail.vue
+++ codegen/__out__/pc/src/views/base/i18n/Detail.vue
@@ -16,7 +16,7 @@
 >
   <template #extra_header>
     <div
-      :title="ns('重置')"
+      title="重置"
     >
       <ElIconRefresh
         class="reset_but"
@@ -26,7 +26,7 @@
     <template v-if="!isLocked && !is_deleted && (dialogAction === 'edit' || dialogAction === 'view')">
       <div
         v-if="!isReadonly"
-        :title="ns('锁定')"
+        title="锁定"
       >
         <ElIconUnlock
           class="unlock_but"
@@ -36,7 +36,7 @@
       </div>
       <div
         v-else
-        :title="ns('解锁')"
+        title="解锉"
       >
         <ElIconLock
           class="lock_but"
@@ -77,7 +77,7 @@
         
         <template v-if="(showBuildIn || builtInModel?.lang_id == null)">
           <el-form-item
-            :label="n('语言')"
+            label="语言"
             prop="lang_id"
           >
             <CustomSelect
@@ -89,7 +89,7 @@
                   value: item.id,
                 };
               })"
-              :placeholder="`${ ns('请选择') } ${ n('语言') }`"
+              placeholder="请选择 语言"
               :readonly="isLocked || isReadonly"
             ></CustomSelect>
           </el-form-item>
@@ -97,13 +97,13 @@
         
         <template v-if="(showBuildIn || builtInModel?.menu_id == null)">
           <el-form-item
-            :label="n('菜单')"
+            label="菜单"
             prop="menu_id"
           >
             <CustomTreeSelect
               v-model="dialogModel.menu_id"
               :method="getMenuTree"
-              :placeholder="`${ ns('请选择') } ${ n('菜单') }`"
+              placeholder="请选择 菜单"
               :readonly="isLocked || isReadonly"
             ></CustomTreeSelect>
           </el-form-item>
@@ -111,12 +111,12 @@
         
         <template v-if="(showBuildIn || builtInModel?.code == null)">
           <el-form-item
-            :label="n('编码')"
+            label="编码"
             prop="code"
           >
             <CustomInput
               v-model="dialogModel.code"
-              :placeholder="`${ ns('请输入') } ${ n('编码') }`"
+              placeholder="请输入 编码"
               :readonly="isLocked || isReadonly"
             ></CustomInput>
           </el-form-item>
@@ -124,12 +124,12 @@
         
         <template v-if="(showBuildIn || builtInModel?.lbl == null)">
           <el-form-item
-            :label="n('名称')"
+            label="名称"
             prop="lbl"
           >
             <CustomInput
               v-model="dialogModel.lbl"
-              :placeholder="`${ ns('请输入') } ${ n('名称') }`"
+              placeholder="请输入 名称"
               :readonly="isLocked || isReadonly"
             ></CustomInput>
           </el-form-item>
@@ -137,7 +137,7 @@
         
         <template v-if="(showBuildIn || builtInModel?.rem == null)">
           <el-form-item
-            :label="n('备注')"
+            label="备注"
             prop="rem"
             un-grid="col-span-full"
           >
@@ -145,7 +145,7 @@
               v-model="dialogModel.rem"
               type="textarea"
               :autosize="{ minRows: 2, maxRows: 5 }"
-              :placeholder="`${ ns('请输入') } ${ n('备注') }`"
+              placeholder="请输入 备注"
               :readonly="isLocked || isReadonly"
               @keyup.enter.stop
             ></CustomInput>
@@ -169,7 +169,7 @@
         <template #icon>
           <ElIconCircleClose />
         </template>
-        <span>{{ ns('关闭') }}</span>
+        <span>关闭</span>
       </el-button>
       
       <el-button
@@ -181,7 +181,7 @@
         <template #icon>
           <ElIconCircleCheck />
         </template>
-        <span>{{ ns('保存并继续') }}</span>
+        <span>保存并继续</span>
       </el-button>
       
       <el-button
@@ -193,7 +193,7 @@
         <template #icon>
           <ElIconCircleCheck />
         </template>
-        <span>{{ ns('保存') }}</span>
+        <span>保存</span>
       </el-button>
       
       <el-button
@@ -205,7 +205,7 @@
         <template #icon>
           <ElIconCircleCheck />
         </template>
-        <span>{{ ns('保存') }}</span>
+        <span>保存</span>
       </el-button>
       
       <div
@@ -331,31 +331,31 @@ watchEffect(async () => {
     lang_id: [
       {
         required: true,
-        message: `${ await nsAsync("请选择") } ${ n("语言") }`,
+        message: "请选择 语言",
       },
     ],
     // 编码
     code: [
       {
         required: true,
-        message: `${ await nsAsync("请输入") } ${ n("编码") }`,
+        message: "请输入 编码",
       },
       {
         type: "string",
         max: 500,
-        message: `${ n("编码") } ${ await nsAsync("长度不能超过 {0}", 500) }`,
+        message: "编码 长度不能超过 500",
       },
     ],
     // 名称
     lbl: [
       {
         required: true,
-        message: `${ await nsAsync("请输入") } ${ n("名称") }`,
+        message: "请输入 名称",
       },
       {
         type: "string",
         max: 500,
-        message: `${ n("名称") } ${ await nsAsync("长度不能超过 {0}", 500) }`,
+        message: "名称 长度不能超过 500",
       },
     ],
   };
@@ -520,10 +520,10 @@ async function onReset() {
   if (!isReadonly && !isLocked) {
     try {
       await ElMessageBox.confirm(
-        await nsAsync("确定要重置表单吗"),
+        "确定要重置表单吗",
         {
-          confirmButtonText: await nsAsync("确定"),
-          cancelButtonText: await nsAsync("取消"),
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
           type: "warning",
         },
       );
@@ -546,7 +546,7 @@ async function onReset() {
     await onRefresh();
   }
   ElMessage({
-    message: await nsAsync("表单重置完毕"),
+    message: "表单重置完毕",
     type: "success",
   });
 }
@@ -581,7 +581,7 @@ async function onPageUp(e?: KeyboardEvent) {
   }
   const isSucc = await prevId();
   if (!isSucc) {
-    ElMessage.warning(await nsAsync("已经是第一项了"));
+    ElMessage.warning("已经是第一项了");
   }
 }
 
@@ -624,7 +624,7 @@ async function onPageDown(e?: KeyboardEvent) {
   }
   const isSucc = await nextId();
   if (!isSucc) {
-    ElMessage.warning(await nsAsync("已经是最后一项了"));
+    ElMessage.warning("已经是最后一项了");
   }
 }
 
@@ -728,7 +728,7 @@ async function save() {
     Object.assign(dialogModel2, { is_deleted: undefined });
     id = await create(dialogModel2);
     dialogModel.id = id;
-    msg = await nsAsync("新增成功");
+    msg = "新增成功";
   } else if (dialogAction === "edit" || dialogAction === "view") {
     if (!dialogModel.id) {
       return;
@@ -745,7 +745,7 @@ async function save() {
       dialogModel.id,
       dialogModel2,
     );
-    msg = await nsAsync("编辑成功");
+    msg = "编辑成功";
   }
   if (id) {
     if (!changedIds.includes(id)) {
@@ -833,26 +833,6 @@ async function beforeClose(done: (cancel: boolean) => void) {
     changedIds,
   });
 }
-
-/** 初始化ts中的国际化信息 */
-async function onInitI18ns() {
-  const codes: string[] = [
-    "语言",
-    "菜单",
-    "编码",
-    "名称",
-    "备注",
-    "创建人",
-    "创建时间",
-    "更新人",
-    "更新时间",
-  ];
-  await Promise.all([
-    initDetailI18ns(),
-    initI18ns(codes),
-  ]);
-}
-onInitI18ns();
 
 defineExpose({
   showDialog,
--- pc/src/views/base/dict_detail/Api.ts
+++ pc/src/views/base/dict_detail/Api.ts
@@ -401,9 +401,6 @@ export async function getDictList() {
  */
 export function useDownloadImportTemplate(routePath: string) {
   const {
-    nsAsync,
-  } = useI18n(routePath);
-  const {
     workerFn,
     workerStatus,
     workerTerminate,
@@ -429,7 +426,7 @@ export function useDownloadImportTemplate(routePath: string) {
       },
     });
     try {
-      const sheetName = await nsAsync("系统字典明细");
+      const sheetName = "系统字典明细";
       const buffer = await workerFn(
         `${ location.origin }/import_template/base/dict_detail.xlsx`,
         {
@@ -437,9 +434,9 @@ export function useDownloadImportTemplate(routePath: string) {
           data,
         },
       );
-      saveAsExcel(buffer, `${ sheetName }${ await nsAsync("导入") }`);
+      saveAsExcel(buffer, `${ sheetName}导入`);
     } catch (err) {
-      ElMessage.error(await nsAsync("下载失败"));
+      ElMessage.error("下载失败");
       throw err;
     }
   }
@@ -454,9 +451,6 @@ export function useDownloadImportTemplate(routePath: string) {
  * 导出Excel
  */
 export function useExportExcel(routePath: string) {
-  const {
-    nsAsync,
-  } = useI18n(routePath);
   const {
     workerFn,
     workerStatus,
@@ -503,7 +497,7 @@ export function useExportExcel(routePath: string) {
         await setLblById(model, true);
       }
       try {
-        const sheetName = await nsAsync("系统字典明细");
+        const sheetName = "系统字典明细";
         const buffer = await workerFn(
           `${ location.origin }/excel_template/base/dict_detail.xlsx`,
           {
@@ -514,7 +508,7 @@ export function useExportExcel(routePath: string) {
         );
         saveAsExcel(buffer, sheetName);
       } catch (err) {
-        ElMessage.error(await nsAsync("导出失败"));
+        ElMessage.error("导出失败");
         throw err;
       }
     } finally {
@@ -538,10 +532,6 @@ export async function importModels(
   isCancel: Ref<boolean>,
   opt?: GqlOpt,
 ) {
-  const {
-    nsAsync,
-  } = useI18n();
-  
   opt = opt || { };
   opt.showErrMsg = false;
   opt.notLoading = true;
@@ -571,7 +561,7 @@ export async function importModels(
       succNum += inputs.length;
     } catch (err) {
       failNum += inputs.length;
-      failErrMsgs.push(await nsAsync(`批量导入第 {0} 至 {1} 行时失败: {1}`, i + 1 - inputs.length, i + 1, err));
+      failErrMsgs.push(`批量导入第 ${ i + 1 - inputs.length } 至 ${ i + 1 } 行时失败: ${ err }`);
     }
     
     percentage.value = Math.floor((i + 1) / len * 100);
