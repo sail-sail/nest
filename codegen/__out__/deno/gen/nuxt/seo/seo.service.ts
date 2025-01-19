e68b565300d27a4fe24e3f2f048ab42a15f0fff0
Sail <151263555@qq.com>
2025-01-19 22:32:41 +0800

    isUseI18n

--- codegen/__out__/deno/gen/nuxt/seo/seo.service.ts
+++ codegen/__out__/deno/gen/nuxt/seo/seo.service.ts
@@ -4,10 +4,6 @@ import type {
   SortInput,
 } from "/gen/types.ts";
 
-import {
-  ns,
-} from "/src/base/i18n/i18n.ts";
-
 import * as seoDao from "./seo.dao.ts";
 
 async function setSearchQuery(
@@ -142,7 +138,7 @@ export async function updateById(
   
   const is_locked = await seoDao.getIsLockedById(id);
   if (is_locked) {
-    throw await ns("不能修改已经锁定的数据");
+    throw "不能修改已经锁定的 SEO优化";
   }
   
   const id2 = await seoDao.updateById(id, input);
@@ -162,7 +158,7 @@ export async function deleteByIds(
     });
     for (const model of models) {
       if (model.is_locked === 1) {
-        throw await ns("不能删除已经锁定的 {0}", "SEO优化");
+        throw "不能删除已经锁定的 SEO优化";
       }
     }
   }
