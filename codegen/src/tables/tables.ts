d904d60b6a3d3141e2f79ff955a7a0064310e839
Sail <151263555@qq.com>
2025-01-20 14:56:47 +0800

    n: isUseI18n

--- codegen/src/tables/tables.ts
+++ codegen/src/tables/tables.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "../config";
 
-/** 是否使用国际化 */
-export const isUseI18n = false;
-
 import base from "./base/base";
 
 // 网站
 import nuxt from "./nuxt/nuxt";
+
+/** 是否使用国际化 */
+export const isUseI18n = false;
 
 export default defineConfig({
   
