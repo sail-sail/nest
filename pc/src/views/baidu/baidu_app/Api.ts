cfa102c3fe469866be3ff8412c909637fa845877
Sail <151263555@qq.com>
2025-01-22 09:44:08 +0800

    onlyCodegenDenoButApi

--- pc/src/views/baidu/baidu_app/Api.ts
+++ pc/src/views/baidu/baidu_app/Api.ts
@@ -356,7 +356,7 @@ export async function forceDeleteByIds(
 /**
  * 下载百度应用导入模板
  */
-export function useDownloadImportTemplate(routePath: string) {
+export function useDownloadImportTemplate() {
   const {
     workerFn,
     workerStatus,
@@ -405,7 +405,7 @@ export function useDownloadImportTemplate(routePath: string) {
 /**
  * 导出Excel
  */
-export function useExportExcel(routePath: string) {
+export function useExportExcel() {
   const {
     workerFn,
     workerStatus,
