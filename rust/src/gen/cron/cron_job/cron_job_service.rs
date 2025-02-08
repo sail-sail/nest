bcdcbefe9533a4f542930e361839f859279a85fe
Sail <151263555@qq.com>
2025-02-08 09:46:49 +0800

    value-format

--- rust/src/gen/cron/cron_job/cron_job_service.rs
+++ rust/src/gen/cron/cron_job/cron_job_service.rs
@@ -144,8 +144,7 @@ pub async fn update_tenant_by_id(
 }
 
 /// 根据 id 修改定时任务
-#[allow(dead_code)]
-#[allow(unused_mut)]
+#[allow(dead_code, unused_mut)]
 pub async fn update_by_id(
   id: CronJobId,
   mut input: CronJobInput,
