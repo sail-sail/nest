2fa79a9dfcf53b8844eb3247e081f0f13d72cb02
Sail <151263555@qq.com>
2025-02-24 17:17:36 +0800

    log

--- codegen/__out__/rust/src/gen/wxwork/wxw_app_token/wxw_app_token_resolver.rs
+++ codegen/__out__/rust/src/gen/wxwork/wxw_app_token/wxw_app_token_resolver.rs
@@ -2,8 +2,13 @@
 use std::time::Instant;
 
 use color_eyre::eyre::Result;
+use tracing::info;
 
-use crate::common::context::Options;
+use crate::common::context::{
+  get_req_id,
+  Options,
+};
+
 use crate::common::gql::model::{PageInput, SortInput};
 use crate::src::base::permit::permit_service::use_permit;
 
@@ -13,12 +18,19 @@ use super::wxw_app_token_service;
 use crate::r#gen::base::tenant::tenant_model::TenantId;
 
 /// 根据搜索条件和分页查找企微应用接口凭据列表
+#[function_name::named]
 pub async fn find_all(
   search: Option<WxwAppTokenSearch>,
   page: Option<PageInput>,
   sort: Option<Vec<SortInput>>,
   options: Option<Options>,
 ) -> Result<Vec<WxwAppTokenModel>> {
+  
+  info!(
+    "{req_id} {function_name}: search: {search:?} page: {page:?} sort: {sort:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   check_sort_wxw_app_token(sort.as_deref())?;
   
@@ -33,10 +45,17 @@ pub async fn find_all(
 }
 
 /// 根据条件查找企微应用接口凭据总数
+#[function_name::named]
 pub async fn find_count(
   search: Option<WxwAppTokenSearch>,
   options: Option<Options>,
 ) -> Result<u64> {
+  
+  info!(
+    "{req_id} {function_name}: search: {search:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let num = wxw_app_token_service::find_count(
     search,
@@ -47,11 +66,18 @@ pub async fn find_count(
 }
 
 /// 根据条件查找第一个企微应用接口凭据
+#[function_name::named]
 pub async fn find_one(
   search: Option<WxwAppTokenSearch>,
   sort: Option<Vec<SortInput>>,
   options: Option<Options>,
 ) -> Result<Option<WxwAppTokenModel>> {
+  
+  info!(
+    "{req_id} {function_name}: search: {search:?} sort: {sort:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   check_sort_wxw_app_token(sort.as_deref())?;
   
@@ -65,10 +91,17 @@ pub async fn find_one(
 }
 
 /// 根据 id 查找企微应用接口凭据
+#[function_name::named]
 pub async fn find_by_id(
   id: WxwAppTokenId,
   options: Option<Options>,
 ) -> Result<Option<WxwAppTokenModel>> {
+  
+  info!(
+    "{req_id} {function_name}: id: {id:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let model = wxw_app_token_service::find_by_id(
     id,
@@ -80,10 +113,17 @@ pub async fn find_by_id(
 
 /// 创建企微应用接口凭据
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn creates(
   inputs: Vec<WxwAppTokenInput>,
   options: Option<Options>,
 ) -> Result<Vec<WxwAppTokenId>> {
+  
+  info!(
+    "{req_id} {function_name}: inputs: {inputs:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let mut inputs = inputs;
   for input in &mut inputs {
@@ -115,11 +155,18 @@ pub async fn creates(
 
 /// 企微应用接口凭据根据id修改租户id
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn update_tenant_by_id(
   id: WxwAppTokenId,
   tenant_id: TenantId,
   options: Option<Options>,
 ) -> Result<u64> {
+  
+  info!(
+    "{req_id} {function_name}: id: {id:?} tenant_id: {tenant_id:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let num = wxw_app_token_service::update_tenant_by_id(
     id,
@@ -132,11 +179,18 @@ pub async fn update_tenant_by_id(
 
 /// 根据 id 修改企微应用接口凭据
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn update_by_id(
   id: WxwAppTokenId,
   input: WxwAppTokenInput,
   options: Option<Options>,
 ) -> Result<WxwAppTokenId> {
+  
+  info!(
+    "{req_id} {function_name}: id: {id:?} input: {input:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let mut input = input;
   input.id = None;
@@ -162,10 +216,17 @@ pub async fn update_by_id(
 
 /// 根据 ids 删除企微应用接口凭据
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn delete_by_ids(
   ids: Vec<WxwAppTokenId>,
   options: Option<Options>,
 ) -> Result<u64> {
+  
+  info!(
+    "{req_id} {function_name}: ids: {ids:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   use_permit(
     get_route_path_wxw_app_token(),
@@ -181,9 +242,16 @@ pub async fn delete_by_ids(
 }
 
 /// 获取企微应用接口凭据字段注释
+#[function_name::named]
 pub async fn get_field_comments(
   options: Option<Options>,
 ) -> Result<WxwAppTokenFieldComment> {
+  
+  info!(
+    "{req_id} {function_name}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   let comments = wxw_app_token_service::get_field_comments(
     options,
@@ -194,10 +262,17 @@ pub async fn get_field_comments(
 
 /// 根据 ids 还原企微应用接口凭据
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn revert_by_ids(
   ids: Vec<WxwAppTokenId>,
   options: Option<Options>,
 ) -> Result<u64> {
+  
+  info!(
+    "{req_id} {function_name}: ids: {ids:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   use_permit(
     get_route_path_wxw_app_token(),
@@ -214,10 +289,17 @@ pub async fn revert_by_ids(
 
 /// 根据 ids 彻底删除企微应用接口凭据
 #[allow(dead_code)]
+#[function_name::named]
 pub async fn force_delete_by_ids(
   ids: Vec<WxwAppTokenId>,
   options: Option<Options>,
 ) -> Result<u64> {
+  
+  info!(
+    "{req_id} {function_name}: ids: {ids:?}",
+    req_id = get_req_id(),
+    function_name = function_name!(),
+  );
   
   use_permit(
     get_route_path_wxw_app_token(),
