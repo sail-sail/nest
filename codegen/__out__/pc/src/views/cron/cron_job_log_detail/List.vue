a7f11be5a0405b0324dd9263408aad217943af33
Sail <151263555@qq.com>
2025-03-05 17:34:56 +0800

    search-ids-checked

--- codegen/__out__/pc/src/views/cron/cron_job_log_detail/List.vue
+++ codegen/__out__/pc/src/views/cron/cron_job_log_detail/List.vue
@@ -58,9 +58,8 @@
         </el-form-item>
       </template>
       
-      <el-form-item
-        label=""
-        prop="idsChecked"
+      <div
+        class="search-ids-checked"
       >
         <div
           un-flex="~ nowrap"
@@ -111,10 +110,10 @@
             <span>回收站</span>
           </el-checkbox>
         </div>
-      </el-form-item>
+      </div>
       
-      <el-form-item
-        label=""
+      <div
+        class="search-buttons"
       >
         
         <el-button
@@ -156,7 +155,7 @@
           
         </div>
         
-      </el-form-item>
+      </div>
       
     </el-form>
   </div>
@@ -738,8 +737,11 @@ let selectedIds = $(tableSelected.selectedIds);
 
 watch(
   () => selectedIds,
-  () => {
+  (oldVal, newVal) => {
     if (!inited) {
+      return;
+    }
+    if (oldVal.length === newVal.length && oldVal.every((v, i) => v === newVal[i])) {
       return;
     }
     emit("selectedIdsChg", selectedIds);
