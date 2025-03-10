c49412bc78a183248c0902fa738ee38d1b9f7fef
Sail <151263555@qq.com>
2025-03-10 11:35:50 +0800

    findAll date, datetime转为字符串

--- pc/src/views/eams/archive/Detail.vue
+++ pc/src/views/eams/archive/Detail.vue
@@ -438,7 +438,9 @@ async function showDialog(
       order_by,
     ] = await Promise.all([
       getDefaultInput(),
-      findLastOrderBy(),
+      findLastOrderBy({
+        notLoading: !inited,
+      }),
     ]);
     dialogModel = {
       ...defaultModel,
@@ -459,7 +461,9 @@ async function showDialog(
         id,
         is_deleted,
       }),
-      findLastOrderBy(),
+      findLastOrderBy({
+        notLoading: !inited,
+      }),
     ]);
     if (data) {
       dialogModel = {
@@ -525,7 +529,9 @@ async function onReset() {
       order_by,
     ] = await Promise.all([
       getDefaultInput(),
-      findLastOrderBy(),
+      findLastOrderBy({
+        notLoading: !inited,
+      }),
     ]);
     dialogModel = {
       ...defaultModel,
