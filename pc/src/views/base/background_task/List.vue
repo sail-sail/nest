bd3ae3e0fc51f19308c2fab76bc1609227bcdf18
Sail <151263555@qq.com>
2025-03-12 10:14:38 +0800

    builtInSearch

--- pc/src/views/base/background_task/List.vue
+++ pc/src/views/base/background_task/List.vue
@@ -955,20 +955,25 @@ const {
 
 const detailRef = $(useTemplateRef<InstanceType<typeof Detail>>("detailRef"));
 
+/** 当前表格数据对应的搜索条件 */
+let currentSearch = $ref<BackgroundTaskSearch>({ });
+
 /** 刷新表格 */
 async function dataGrid(
   isCount = false,
   opt?: GqlOpt,
 ) {
   clearDirty();
+  const search = getDataSearch();
+  currentSearch = search;
   if (isCount) {
     await Promise.all([
-      useFindAll(opt),
-      useFindCount(opt),
+      useFindAll(search, opt),
+      useFindCount(search, opt),
     ]);
   } else {
     await Promise.all([
-      useFindAll(opt),
+      useFindAll(search, opt),
     ]);
   }
 }
@@ -990,14 +995,14 @@ function getDataSearch() {
 }
 
 async function useFindAll(
+  search: BackgroundTaskSearch,
   opt?: GqlOpt,
 ) {
   if (isPagination) {
     const pgSize = page.size;
     const pgOffset = (page.current - 1) * page.size;
-    const search2 = getDataSearch();
     tableData = await findAll(
-      search2,
+      search,
       {
         pgSize,
         pgOffset,
@@ -1008,9 +1013,8 @@ async function useFindAll(
       opt,
     );
   } else {
-    const search2 = getDataSearch();
     tableData = await findAll(
-      search2,
+      search,
       undefined,
       [
         sort,
@@ -1021,6 +1025,7 @@ async function useFindAll(
 }
 
 async function useFindCount(
+  search: BackgroundTaskSearch,
   opt?: GqlOpt,
 ) {
   const search2 = getDataSearch();
@@ -1270,12 +1275,15 @@ watch(
     if (isSearchReset) {
       return;
     }
-    search.is_deleted = builtInSearch.is_deleted;
-    if (deepCompare(builtInSearch, search, undefined, [ "selectedIds" ])) {
-      return;
+    if (builtInSearch.is_deleted != null) {
+      search.is_deleted = builtInSearch.is_deleted;
     }
     if (showBuildIn) {
       Object.assign(search, builtInSearch);
+    }
+    const search2 = getDataSearch();
+    if (deepCompare(currentSearch, search2, undefined, [ "selectedIds" ])) {
+      return;
     }
     await dataGrid(true);
   },
--- pc/src/views/base/dict_detail/List.vue
+++ pc/src/views/base/dict_detail/List.vue
@@ -1119,20 +1119,25 @@ const {
 
 const detailRef = $(useTemplateRef<InstanceType<typeof Detail>>("detailRef"));
 
+/** 当前表格数据对应的搜索条件 */
+let currentSearch = $ref<DictDetailSearch>({ });
+
 /** 刷新表格 */
 async function dataGrid(
   isCount = false,
   opt?: GqlOpt,
 ) {
   clearDirty();
+  const search = getDataSearch();
+  currentSearch = search;
   if (isCount) {
     await Promise.all([
-      useFindAll(opt),
-      useFindCount(opt),
+      useFindAll(search, opt),
+      useFindCount(search, opt),
     ]);
   } else {
     await Promise.all([
-      useFindAll(opt),
+      useFindAll(search, opt),
     ]);
   }
 }
@@ -1154,14 +1159,14 @@ function getDataSearch() {
 }
 
 async function useFindAll(
+  search: DictDetailSearch,
   opt?: GqlOpt,
 ) {
   if (isPagination) {
     const pgSize = page.size;
     const pgOffset = (page.current - 1) * page.size;
-    const search2 = getDataSearch();
     tableData = await findAll(
-      search2,
+      search,
       {
         pgSize,
         pgOffset,
@@ -1172,9 +1177,8 @@ async function useFindAll(
       opt,
     );
   } else {
-    const search2 = getDataSearch();
     tableData = await findAll(
-      search2,
+      search,
       undefined,
       [
         sort,
@@ -1185,6 +1189,7 @@ async function useFindAll(
 }
 
 async function useFindCount(
+  search: DictDetailSearch,
   opt?: GqlOpt,
 ) {
   const search2 = getDataSearch();
@@ -1710,12 +1715,15 @@ watch(
     if (isSearchReset) {
       return;
     }
-    search.is_deleted = builtInSearch.is_deleted;
-    if (deepCompare(builtInSearch, search, undefined, [ "selectedIds" ])) {
-      return;
+    if (builtInSearch.is_deleted != null) {
+      search.is_deleted = builtInSearch.is_deleted;
     }
     if (showBuildIn) {
       Object.assign(search, builtInSearch);
+    }
+    const search2 = getDataSearch();
+    if (deepCompare(currentSearch, search2, undefined, [ "selectedIds" ])) {
+      return;
     }
     await dataGrid(true);
   },
