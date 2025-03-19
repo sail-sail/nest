555023857bfd18e6b67868fcdea34b2476540681
root <151263555@qq.com>
2025-03-19 09:18:32 +0800

    n:Bearer

--- rust/src/common/auth/auth_dao.rs
+++ rust/src/common/auth/auth_dao.rs
@@ -28,7 +28,7 @@ pub fn get_token_by_auth_model(
     return Err(eyre!("token过期时间不能为空"));
   }
   let key: hmac::Hmac<sha2::Sha256> = hmac::Hmac::new_from_slice(SECRET_KEY.as_bytes())?;
-  Ok(SignWithKey::sign_with_key(auth_model, &key))
+  Ok(SignWithKey::sign_with_key(auth_model, &key)?.to_string())
 }
 
 pub fn get_password(str: String) -> Result<String> {
