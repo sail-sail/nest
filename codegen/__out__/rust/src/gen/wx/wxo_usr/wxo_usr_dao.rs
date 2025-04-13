8cb8b37722b796b49330a64f2ce57fefeaa49818
Sail <151263555@qq.com>
2025-04-13 21:30:20 +0800

    find_by_id_ok

--- codegen/__out__/rust/src/gen/wx/wxo_usr/wxo_usr_dao.rs
+++ codegen/__out__/rust/src/gen/wx/wxo_usr/wxo_usr_dao.rs
@@ -924,8 +924,55 @@ pub async fn get_field_comments_wxo_usr(
   Ok(field_comments)
 }
 
+// MARK: find_one_ok_wxo_usr
+/// 根据条件查找第一个公众号用户
+#[allow(dead_code)]
+pub async fn find_one_ok_wxo_usr(
+  search: Option<WxoUsrSearch>,
+  sort: Option<Vec<SortInput>>,
+  options: Option<Options>,
+) -> Result<WxoUsrModel> {
+  
+  let table = "wx_wxo_usr";
+  let method = "find_one_ok_wxo_usr";
+  
+  let is_debug = get_is_debug(options.as_ref());
+  
+  if is_debug {
+    let mut msg = format!("{table}.{method}:");
+    if let Some(search) = &search {
+      msg += &format!(" search: {:?}", &search);
+    }
+    if let Some(sort) = &sort {
+      msg += &format!(" sort: {:?}", &sort);
+    }
+    if let Some(options) = &options {
+      msg += &format!(" options: {:?}", &options);
+    }
+    info!(
+      "{req_id} {msg}",
+      req_id = get_req_id(),
+    );
+  }
+  
+  let options = Options::from(options)
+    .set_is_debug(Some(false));
+  let options = Some(options);
+  
+  let wxo_usr_model = validate_option_wxo_usr(
+    find_one_wxo_usr(
+      search,
+      sort,
+      options,
+    ).await?,
+  ).await?;
+  
+  Ok(wxo_usr_model)
+}
+
 // MARK: find_one_wxo_usr
 /// 根据条件查找第一个公众号用户
+#[allow(dead_code)]
 pub async fn find_one_wxo_usr(
   search: Option<WxoUsrSearch>,
   sort: Option<Vec<SortInput>>,
@@ -979,6 +1026,45 @@ pub async fn find_one_wxo_usr(
   let model: Option<WxoUsrModel> = res.into_iter().next();
   
   Ok(model)
+}
+
+// MARK: find_by_id_ok_wxo_usr
+/// 根据 id 查找公众号用户
+#[allow(dead_code)]
+pub async fn find_by_id_ok_wxo_usr(
+  id: WxoUsrId,
+  options: Option<Options>,
+) -> Result<WxoUsrModel> {
+  
+  let table = "wx_wxo_usr";
+  let method = "find_by_id_ok_wxo_usr";
+  
+  let is_debug = get_is_debug(options.as_ref());
+  
+  if is_debug {
+    let mut msg = format!("{table}.{method}:");
+    msg += &format!(" id: {:?}", &id);
+    if let Some(options) = &options {
+      msg += &format!(" options: {:?}", &options);
+    }
+    info!(
+      "{req_id} {msg}",
+      req_id = get_req_id(),
+    );
+  }
+  
+  let options = Options::from(options)
+    .set_is_debug(Some(false));
+  let options = Some(options);
+  
+  let wxo_usr_model = validate_option_wxo_usr(
+    find_by_id_wxo_usr(
+      id,
+      options,
+    ).await?,
+  ).await?;
+  
+  Ok(wxo_usr_model)
 }
 
 // MARK: find_by_id_wxo_usr
