b60aecc1860b1e86d983bb7f63a9e04fe5d17945
Sail <151263555@qq.com>
2025-04-17 18:26:58 +0800

    presetWind4 删除 @apply

--- pc/uno.config.ts
+++ pc/uno.config.ts
@@ -6,10 +6,9 @@ import {
   defineConfig,
   presetAttributify,
   presetIcons,
-  presetWind3,
+  presetWind4,
   transformerAttributifyJsx,
   transformerCompileClass,
-  transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
 
@@ -36,7 +35,7 @@ export default defineConfig({
     ],
   ],
   presets: [
-    presetWind3(),
+    presetWind4(),
     presetRemToPx(),
     presetIcons({
       warn: true,
@@ -51,7 +50,6 @@ export default defineConfig({
   ],
   transformers: [
     transformerCompileClass(),
-    transformerDirectives(),
     transformerVariantGroup(),
     transformerAttributifyJsx(),
   ],
