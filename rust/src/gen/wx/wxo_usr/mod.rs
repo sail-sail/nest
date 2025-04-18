f63f8e58673253e312415682bea51ad64200ea12
Sail <151263555@qq.com>
2025-04-18 16:25:20 +0800

    app

--- rust/src/gen/wx/wxo_usr/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod wxo_usr_model;
-pub mod wxo_usr_resolver;
-pub mod wxo_usr_graphql;
-pub mod wxo_usr_service;
-pub mod wxo_usr_dao;
--- rust/src/src/wx/wx_usr/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod wx_usr_router;
-pub mod wx_usr_resful;
-pub mod wx_usr_service;
-pub mod wx_usr_model;
