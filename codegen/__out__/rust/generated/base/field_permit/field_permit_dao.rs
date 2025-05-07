c1dcf6717ce4859f2727eb027b920966f69f4a14
Sail <151263555@qq.com>
2025-05-07 15:02:00 +0800

    secondSorts 固定的二级排序

--- codegen/__out__/rust/generated/base/field_permit/field_permit_dao.rs
+++ codegen/__out__/rust/generated/base/field_permit/field_permit_dao.rs
@@ -323,9 +323,7 @@ pub async fn find_all_field_permit(
     });
   }
   
-  let sort = sort.into();
-  
-  let order_by_query = get_order_by_query(sort);
+  let order_by_query = get_order_by_query(Some(sort));
   let page_query = get_page_query(page);
   
   let sql = format!(r#"select f.* from (select t.*
