6096fd93b633518a851e29a04ffaa01bbdb8abc4
Sail <151263555@qq.com>
2025-06-06 00:36:01 +0800

    find_by_ids 根据 ids 排序

--- codegen/__out__/deno/gen/wxwork/wxw_app/wxw_app.dao.ts
+++ codegen/__out__/deno/gen/wxwork/wxw_app/wxw_app.dao.ts
@@ -1030,7 +1030,11 @@ export async function findByIdsWxwApp(
     options,
   );
   
-  return models;
+  const models2 = ids
+    .map((id) => models.find((item) => item.id === id))
+    .filter((item) => !!item);
+  
+  return models2;
 }
 
 // MARK: findByIdsOkWxwApp
--- deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
+++ deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
@@ -927,7 +927,11 @@ export async function findByIdsWxwUsr(
     options,
   );
   
-  return models;
+  const models2 = ids
+    .map((id) => models.find((item) => item.id === id))
+    .filter((item) => !!item);
+  
+  return models2;
 }
 
 // MARK: findByIdsOkWxwUsr
