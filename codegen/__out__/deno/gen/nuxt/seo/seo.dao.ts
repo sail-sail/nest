a0d6add086219d01da8cb00590cf0eba2363639b
Sail <151263555@qq.com>
2025-06-06 00:37:14 +0800

    find_by_ids 根据 ids 排序

--- codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
+++ codegen/__out__/deno/gen/nuxt/seo/seo.dao.ts
@@ -906,7 +906,11 @@ export async function findByIdsSeo(
     options,
   );
   
-  return models;
+  const models2 = ids
+    .map((id) => models.find((item) => item.id === id))
+    .filter((item) => !!item);
+  
+  return models2;
 }
 
 // MARK: findByIdsOkSeo
