55e19c22dbdf749415e4e53a6b5ea39ac7240280
Sail <151263555@qq.com>
2025-06-06 00:37:59 +0800

    find_by_ids 根据 ids 排序

--- codegen/__out__/deno/gen/baidu/baidu_app/baidu_app.dao.ts
+++ codegen/__out__/deno/gen/baidu/baidu_app/baidu_app.dao.ts
@@ -939,7 +939,11 @@ export async function findByIdsBaiduApp(
     options,
   );
   
-  return models;
+  const models2 = ids
+    .map((id) => models.find((item) => item.id === id))
+    .filter((item) => !!item);
+  
+  return models2;
 }
 
 // MARK: findByIdsOkBaiduApp
