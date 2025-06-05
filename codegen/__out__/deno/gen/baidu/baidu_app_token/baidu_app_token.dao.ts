aab1e9b9b69158926a84266459845a8feb6f1cf3
Sail <151263555@qq.com>
2025-06-06 00:37:59 +0800

    find_by_ids 根据 ids 排序

--- codegen/__out__/deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
+++ codegen/__out__/deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
@@ -876,7 +876,11 @@ export async function findByIdsBaiduAppToken(
     options,
   );
   
-  return models;
+  const models2 = ids
+    .map((id) => models.find((item) => item.id === id))
+    .filter((item) => !!item);
+  
+  return models2;
 }
 
 // MARK: findByIdsOkBaiduAppToken
