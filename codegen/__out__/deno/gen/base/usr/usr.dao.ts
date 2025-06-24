80924adb6767e083ed22857051b4d0ecc25feaf4
Sail <151263555@qq.com>
2025-06-25 00:46:39 +0800

    findByIdOk error

--- codegen/__out__/deno/gen/base/usr/usr.dao.ts
+++ codegen/__out__/deno/gen/base/usr/usr.dao.ts
@@ -1312,6 +1312,7 @@ export async function findByIdOkUsr(
   
   if (!usr_model) {
     const err_msg = "此 用户 已被删除";
+    console.error(`${ err_msg } id: ${ id }`);
     throw new Error(err_msg);
   }
   
