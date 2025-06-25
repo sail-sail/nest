e7570a148827f687108f8b4ddc16706d7acd157d
Sail <151263555@qq.com>
2025-06-25 09:25:44 +0800

    findByIdOk error

--- deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
+++ deno/gen/wxwork/wxw_usr/wxw_usr.dao.ts
@@ -881,6 +881,7 @@ export async function findByIdOkWxwUsr(
   
   if (!wxw_usr_model) {
     const err_msg = "此 企微用户 已被删除";
+    console.error(`${ err_msg } id: ${ id }`);
     throw new Error(err_msg);
   }
   
