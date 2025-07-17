80c0fdd090de0baddd280d1efa3507e8be81c2d0
Sail <151263555@qq.com>
2025-07-17 17:55:33 +0800

    route_path

--- codegen/__out__/deno/gen/wxwork/wxw_msg/wxw_msg.dao.ts
+++ codegen/__out__/deno/gen/wxwork/wxw_msg/wxw_msg.dao.ts
@@ -77,10 +77,6 @@ import {
   findByIdUsr,
 } from "/gen/base/usr/usr.dao.ts";
 
-import {
-  route_path,
-} from "./wxw_msg.model.ts";
-
 async function getWhereQuery(
   args: QueryArgs,
   search?: Readonly<WxwMsgSearch>,
