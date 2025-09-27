ad5d034abc3cc868aae0b2e485bfaffd3ac5d44d
Sail <151263555@qq.com>
2025-09-27 19:02:24 +0800

    ts-node

--- codegen/src/tables/tables.ts
+++ codegen/src/tables/tables.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "../config.ts";
 
 import base from "./base/base.ts";
 
-import submail from "./submail/submail";
+import submail from "./submail/submail.ts";
 
 /** 是否使用国际化 */
 export const isUseI18n = false;
