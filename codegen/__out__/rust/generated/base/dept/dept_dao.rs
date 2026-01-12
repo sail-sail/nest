59324dd8b815468c7de5bb16129c1954202bfc27
Sail <151263555@qq.com>
2026-01-12 17:08:43 +0800

    del_cache_

--- codegen/__out__/rust/generated/base/dept/dept_dao.rs
+++ codegen/__out__/rust/generated/base/dept/dept_dao.rs
@@ -2757,6 +2757,8 @@ fn get_cache_tables() -> Vec<&'static str> {
   let table = get_table_name_dept();
   vec![
     table,
+    "dept",
+    "org",
   ]
 }
 
@@ -2764,10 +2766,25 @@ fn get_cache_tables() -> Vec<&'static str> {
 /// 清空缓存
 #[allow(dead_code)]
 pub async fn del_cache_dept() -> Result<()> {
+  
   let cache_key1s = get_cache_tables();
+  
+  let cache_key1s = cache_key1s
+    .into_iter()
+    .map(|x|
+      format!("dao.sql.{x}")
+    )
+    .collect::<Vec<String>>();
+  
+  let cache_key1s_str = cache_key1s
+    .iter()
+    .map(|item| item.as_str())
+    .collect::<Vec<&str>>();
+  
   del_caches(
-    cache_key1s.as_slice(),
+    cache_key1s_str.as_slice(),
   ).await?;
+  
   Ok(())
 }
 
