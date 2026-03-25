a23cffed0780dae334fde78850cf21a15267b12b
Sail <151263555@qq.com>
2026-03-25 19:13:00 +0800

    async_graphql MergedObject

--- codegen/src/template/rust/generated/[[mod]]/mod.rs
+++ codegen/src/template/rust/generated/[[mod]]/mod.rs
@@ -3,10 +3,7 @@ for (let i = 0; i < allTables.length; i++) {
   const record = allTables[i];
   let table_name = record.TABLE_NAME;
   if (!optTables[table_name]) continue;
-  let table_comment = record.TABLE_COMMENT;
-  if (table_comment.includes("[")) {
-    table_comment = table_comment.substring(0, table_comment.indexOf("["));
-  }
+  const table_comment = record.TABLE_COMMENT;
   const mod2 = table_name.substring(0, table_name.indexOf("_"));
   if (mod !== mod2) continue;
   table_name = table_name.substring(table_name.indexOf("_") + 1);
@@ -18,3 +15,46 @@ for (let i = 0; i < allTables.length; i++) {
 <#
 }
 #>
+use async_graphql::MergedObject;<#
+const modUp = mod.substring(0, 1).toUpperCase() + mod.substring(1);
+#>
+
+#[derive(MergedObject, Default)]
+pub struct <#=modUp#>Query(<#
+  for (let i = 0; i < allTables.length; i++) {
+    const record = allTables[i];
+    let table_name = record.TABLE_NAME;
+    if (!optTables[table_name]) continue;
+    const table_comment = record.TABLE_COMMENT;
+    const mod2 = table_name.substring(0, table_name.indexOf("_"));
+    if (mod !== mod2) continue;
+    table_name = table_name.substring(table_name.indexOf("_") + 1);
+    const table_nameUp = table_name.substring(0, 1).toUpperCase() + table_name.substring(1);
+    const table_nameUP = table_nameUp.split("_").map(function(item) {
+      return item.substring(0, 1).toUpperCase() + item.substring(1);
+    }).join("");
+  #>
+  self::<#=table_name#>::<#=table_name#>_graphql::<#=table_nameUP#>GenQuery,<#
+  }
+  #>
+);
+
+#[derive(MergedObject, Default)]
+pub struct <#=modUp#>Mutation(<#
+  for (let i = 0; i < allTables.length; i++) {
+    const record = allTables[i];
+    let table_name = record.TABLE_NAME;
+    if (!optTables[table_name]) continue;
+    const table_comment = record.TABLE_COMMENT;
+    const mod2 = table_name.substring(0, table_name.indexOf("_"));
+    if (mod !== mod2) continue;
+    table_name = table_name.substring(table_name.indexOf("_") + 1);
+    const table_nameUp = table_name.substring(0, 1).toUpperCase() + table_name.substring(1);
+    const table_nameUP = table_nameUp.split("_").map(function(item) {
+      return item.substring(0, 1).toUpperCase() + item.substring(1);
+    }).join("");
+  #>
+  self::<#=table_name#>::<#=table_name#>_graphql::<#=table_nameUP#>GenMutation,<#
+  }
+  #>
+);
--- codegen/src/template/rust/generated/lib.rs
+++ codegen/src/template/rust/generated/lib.rs
@@ -7,10 +7,7 @@ for (let i = 0; i < allTables.length; i++) {
   const record = allTables[i];
   let table_name = record.TABLE_NAME;
   if (!optTables[table_name]) continue;
-  let table_comment = record.TABLE_COMMENT;
-  if (table_comment.includes("[")) {
-    table_comment = table_comment.substring(0, table_comment.indexOf("["));
-  }
+  const table_comment = record.TABLE_COMMENT;
   const mod = table_name.substring(0, table_name.indexOf("_"));
   table_name = table_name.substring(table_name.indexOf("_") + 1);
   const table_nameUp = table_name.substring(0, 1).toUpperCase() + table_name.substring(1);
@@ -25,46 +22,22 @@ use async_graphql::MergedObject;
 
 #[derive(MergedObject, Default)]
 pub struct GenQuery(<#
-  for (let i = 0; i < allTables.length; i++) {
-    const record = allTables[i];
-    let table_name = record.TABLE_NAME;
-    if (!optTables[table_name]) continue;
-    let table_comment = record.TABLE_COMMENT;
-    if (table_comment.includes("[")) {
-      table_comment = table_comment.substring(0, table_comment.indexOf("["));
-    }
-    const mod2 = table_name.substring(0, table_name.indexOf("_"));
-    // if (mod !== mod2) continue;
-    table_name = table_name.substring(table_name.indexOf("_") + 1);
-    const table_nameUp = table_name.substring(0, 1).toUpperCase() + table_name.substring(1);
-    const table_nameUP = table_nameUp.split("_").map(function(item) {
-      return item.substring(0, 1).toUpperCase() + item.substring(1);
-    }).join("");
+  for (let i = 0; i < mods.length; i++) {
+    const mod2 = mods[i];
+    const mod2Up = mod2.substring(0, 1).toUpperCase() + mod2.substring(1);
   #>
-  <#=mod2#>::<#=table_name#>::<#=table_name#>_graphql::<#=table_nameUP#>GenQuery,<#
+  <#=mod2#>::<#=mod2Up#>Query,<#
   }
   #>
 );
 
 #[derive(MergedObject, Default)]
 pub struct GenMutation(<#
-  for (let i = 0; i < allTables.length; i++) {
-    const record = allTables[i];
-    let table_name = record.TABLE_NAME;
-    if (!optTables[table_name]) continue;
-    let table_comment = record.TABLE_COMMENT;
-    if (table_comment.includes("[")) {
-      table_comment = table_comment.substring(0, table_comment.indexOf("["));
-    }
-    const mod2 = table_name.substring(0, table_name.indexOf("_"));
-    // if (mod !== mod2) continue;
-    table_name = table_name.substring(table_name.indexOf("_") + 1);
-    const table_nameUp = table_name.substring(0, 1).toUpperCase() + table_name.substring(1);
-    const table_nameUP = table_nameUp.split("_").map(function(item) {
-      return item.substring(0, 1).toUpperCase() + item.substring(1);
-    }).join("");
+  for (let i = 0; i < mods.length; i++) {
+    const mod2 = mods[i];
+    const mod2Up = mod2.substring(0, 1).toUpperCase() + mod2.substring(1);
   #>
-  <#=mod2#>::<#=table_name#>::<#=table_name#>_graphql::<#=table_nameUP#>GenMutation,<#
+  <#=mod2#>::<#=mod2Up#>Mutation,<#
   }
   #>
 );
