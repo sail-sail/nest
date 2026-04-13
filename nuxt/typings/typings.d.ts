731c902ff60ca3f006341f98e7f8d0cbf2925b17
Sail <151263555@qq.com>
2026-04-13 11:27:12 +0800

    types

--- nuxt/typings/typings.d.ts
+++ nuxt/typings/typings.d.ts
@@ -10,7 +10,13 @@ declare global {
   type PartialNull<T> = { [P in keyof T]?: T[P] | null | undefined; }
   
   type Distinct<T, DistinctName> = T & { __TYPE__: DistinctName };
-
+  
+  type TableSortOrder = 'ascending' | 'descending';
+  
+  interface Sort {
+    prop: string;
+    order: TableSortOrder;
+  }
   
   interface ColumnType {
     prop: string;
