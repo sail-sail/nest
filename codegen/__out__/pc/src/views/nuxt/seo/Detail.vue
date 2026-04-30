6cf82e30f95cae187af850db407680139d7ee951
Sail <151263555@qq.com>
2026-05-01 05:22:20 +0800

    findByUnique

--- codegen/__out__/pc/src/views/nuxt/seo/Detail.vue
+++ codegen/__out__/pc/src/views/nuxt/seo/Detail.vue
@@ -227,6 +227,7 @@
         v-if="(dialogAction === 'add' || dialogAction === 'copy') && permit('add', '新增') && !isLocked && !isReadonly"
         plain
         type="primary"
+        :disabled="is_form_hydrating"
         @click="onSave"
       >
         <template #icon>
@@ -239,6 +240,7 @@
         v-if="(dialogAction === 'edit' || dialogAction === 'view') && permit('edit', '编辑') && !isLocked && !isReadonly"
         plain
         type="primary"
+        :disabled="is_form_hydrating"
         @click="onSave"
       >
         <template #icon>
@@ -325,6 +327,7 @@ const permitStore = usePermitStore();
 const permit = permitStore.getPermit(pagePath);
 
 let inited = $ref(false);
+let is_form_hydrating = $ref(false);
 
 type DialogAction = "add" | "copy" | "edit" | "view";
 let dialogAction = $ref<DialogAction>("add");
@@ -587,42 +590,49 @@ async function onReset() {
 
 /** 刷新 */
 async function onRefresh() {
-  const id = dialogModel.id;
-  if (!id) {
+  is_form_hydrating = true;
+  try {
+    const id = dialogModel.id;
+    if (!id) {
+      const [
+        defaultModel,
+        order_by,
+      ] = await Promise.all([
+        getDefaultInputSeo(),
+        findLastOrderBySeo(
+          undefined,
+          {
+            notLoading: !inited,
+          },
+        ),
+      ]);
+      dialogModel = {
+        ...defaultModel,
+        ...builtInModel,
+        order_by: order_by + 1,
+      };
+      is_form_hydrating = false;
+      return;
+    }
     const [
-      defaultModel,
-      order_by,
+      data,
     ] = await Promise.all([
-      getDefaultInputSeo(),
-      findLastOrderBySeo(
-        undefined,
-        {
-          notLoading: !inited,
-        },
-      ),
+      findOneModel({
+        id,
+        is_deleted,
+      }),
     ]);
-    dialogModel = {
-      ...defaultModel,
-      ...builtInModel,
-      order_by: order_by + 1,
-    };
-    return;
+    if (data) {
+      dialogModel = intoInputSeo({
+        ...data,
+      });
+      dialogTitle = `${ oldDialogTitle } - ${ dialogModel.lbl }`;
+    }
+    seo_model = data;
+  } finally {
+    await nextTick();
+    is_form_hydrating = false;
   }
-  const [
-    data,
-  ] = await Promise.all([
-    findOneModel({
-      id,
-      is_deleted,
-    }),
-  ]);
-  if (data) {
-    dialogModel = intoInputSeo({
-      ...data,
-    });
-    dialogTitle = `${ oldDialogTitle } - ${ dialogModel.lbl }`;
-  }
-  seo_model = data;
 }
 
 /** 键盘按 PageUp */
@@ -723,7 +733,7 @@ async function onSaveKeydown(e: KeyboardEvent) {
 
 /** 保存并返回id */
 async function save() {
-  if (!inited || isReadonly) {
+  if (!inited || isReadonly || is_form_hydrating) {
     return;
   }
   if (!formRef) {
