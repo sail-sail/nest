43e91f836e14a5e39d9fa6ff2db3b64d109169c4
Sail <151263555@qq.com>
2026-05-04 02:37:28 +0800

    del_cache_<#=table#>()

--- codegen/__out__/rust/generated/cron/cron_job_log/cron_job_log_dao.rs
+++ codegen/__out__/rust/generated/cron/cron_job_log/cron_job_log_dao.rs
@@ -2296,42 +2296,6 @@ pub async fn update_by_id_return_cron_job_log(
   }
 }
 
-/// 获取需要清空缓存的表名
-#[allow(dead_code)]
-fn get_cache_tables() -> Vec<&'static str> {
-  let table = get_table_name_cron_job_log();
-  vec![
-    table,
-    "cron_cron_job",
-  ]
-}
-
-// MARK: del_cache_cron_job_log
-/// 清空缓存
-#[allow(dead_code)]
-pub async fn del_cache_cron_job_log() -> Result<()> {
-  
-  let cache_key1s = get_cache_tables();
-  
-  let cache_key1s = cache_key1s
-    .into_iter()
-    .map(|x|
-      SmolStr::new(format!("dao.sql.{x}"))
-    )
-    .collect::<Vec<SmolStr>>();
-  
-  let cache_key1s_str = cache_key1s
-    .iter()
-    .map(|item| item.as_str())
-    .collect::<Vec<&str>>();
-  
-  del_caches(
-    cache_key1s_str.as_slice(),
-  ).await?;
-  
-  Ok(())
-}
-
 // MARK: delete_by_ids_cron_job_log
 /// 根据 ids 删除定时任务日志
 #[allow(unused_variables)]
--- codegen/__out__/rust/generated/cron/cron_job_log_detail/cron_job_log_detail_dao.rs
+++ codegen/__out__/rust/generated/cron/cron_job_log_detail/cron_job_log_detail_dao.rs
@@ -1936,42 +1936,6 @@ pub async fn update_by_id_return_cron_job_log_detail(
   }
 }
 
-/// 获取需要清空缓存的表名
-#[allow(dead_code)]
-fn get_cache_tables() -> Vec<&'static str> {
-  let table = get_table_name_cron_job_log_detail();
-  vec![
-    table,
-    "cron_cron_job_log",
-  ]
-}
-
-// MARK: del_cache_cron_job_log_detail
-/// 清空缓存
-#[allow(dead_code)]
-pub async fn del_cache_cron_job_log_detail() -> Result<()> {
-  
-  let cache_key1s = get_cache_tables();
-  
-  let cache_key1s = cache_key1s
-    .into_iter()
-    .map(|x|
-      SmolStr::new(format!("dao.sql.{x}"))
-    )
-    .collect::<Vec<SmolStr>>();
-  
-  let cache_key1s_str = cache_key1s
-    .iter()
-    .map(|item| item.as_str())
-    .collect::<Vec<&str>>();
-  
-  del_caches(
-    cache_key1s_str.as_slice(),
-  ).await?;
-  
-  Ok(())
-}
-
 // MARK: delete_by_ids_cron_job_log_detail
 /// 根据 ids 删除定时任务日志明细
 #[allow(unused_variables)]
