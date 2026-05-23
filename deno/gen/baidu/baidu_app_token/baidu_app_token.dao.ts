38ca2151ceb6930f9af5703c740dbc8a17c6b542
Sail <151263555@qq.com>
2026-05-23 12:39:57 +0800

    syncUsrLblByUsrId

--- deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
+++ deno/gen/baidu/baidu_app_token/baidu_app_token.dao.ts
@@ -599,6 +599,7 @@ export async function findByUniqueBaiduAppToken(
     if (canFind) {
       const modelTmps = await findAllBaiduAppToken(
         {
+          tenant_id: search0.tenant_id,
           baidu_app_id,
         },
         undefined,
@@ -982,7 +983,7 @@ export async function existBaiduAppToken(
 // MARK: existByIdBaiduAppToken
 /** 根据id判断百度接口凭据是否存在 */
 export async function existByIdBaiduAppToken(
-  id?: Readonly<BaiduAppTokenId | null>,
+  id?: BaiduAppTokenId | null,
   options?: {
     is_debug?: boolean;
   },
@@ -1452,7 +1453,7 @@ export async function delCacheBaiduAppToken() {
 /** 百度接口凭据 根据 id 修改 租户id */
 export async function updateTenantByIdBaiduAppToken(
   id: BaiduAppTokenId,
-  tenant_id: Readonly<TenantId>,
+  tenant_id: TenantId,
   options?: {
     is_debug?: boolean;
   },
@@ -1490,6 +1491,73 @@ export async function updateTenantByIdBaiduAppToken(
   const affectedRows = res.affectedRows;
   
   await delCacheBaiduAppToken();
+  return affectedRows;
+}
+
+// MARK: syncUsrLblByUsrIdBaiduAppToken
+/** 根据 usr_id 同步创建人/更新人/删除人标签 */
+export async function syncUsrLblByUsrIdBaiduAppToken(
+  usr_id: UsrId,
+  options?: {
+    is_debug?: boolean;
+  },
+): Promise<number> {
+  
+  const table = getTableNameBaiduAppToken();
+  const method = "syncUsrLblByUsrIdBaiduAppToken";
+  
+  const is_debug = get_is_debug(options?.is_debug);
+  
+  if (is_debug !== false) {
+    let msg = `${ table }.${ method }:`;
+    if (usr_id) {
+      msg += ` usr_id:${ usr_id }`;
+    }
+    if (options && Object.keys(options).length > 0) {
+      msg += ` options:${ JSON.stringify(options) }`;
+    }
+    log(msg);
+  }
+  
+  if (!usr_id) {
+    return 0;
+  }
+  
+  const findOptions = {
+    ...(options ?? { }),
+    is_debug: false,
+  };
+  const usr_model = await findByIdUsr(usr_id, findOptions);
+  if (!usr_model) {
+    return 0;
+  }
+  
+  const usr_lbl = usr_model.lbl;
+  const sqlFields = [ ];
+  const whereQuerys = [ ];
+  const args = new QueryArgs();
+  
+  sqlFields.push(`create_usr_id_lbl=case when create_usr_id=${ args.push(usr_id) } then ${ args.push(usr_lbl) } else create_usr_id_lbl end`);
+  whereQuerys.push(`create_usr_id=${ args.push(usr_id) }`);
+  
+  sqlFields.push(`update_usr_id_lbl=case when update_usr_id=${ args.push(usr_id) } then ${ args.push(usr_lbl) } else update_usr_id_lbl end`);
+  whereQuerys.push(`update_usr_id=${ args.push(usr_id) }`);
+  
+  sqlFields.push(`delete_usr_id_lbl=case when delete_usr_id=${ args.push(usr_id) } then ${ args.push(usr_lbl) } else delete_usr_id_lbl end`);
+  whereQuerys.push(`delete_usr_id=${ args.push(usr_id) }`);
+  
+  if (sqlFields.length === 0 || whereQuerys.length === 0) {
+    return 0;
+  }
+  
+  const sql = `update baidu_baidu_app_token set ${ sqlFields.join(",") } where ${ whereQuerys.join(" or ") }`;
+  const res = await execute(sql, args);
+  const affectedRows = res.affectedRows;
+  
+  if (affectedRows > 0) {
+    await delCacheBaiduAppToken();
+  }
+  
   return affectedRows;
 }
 
--- deno/gen/base/usr/usr_sync.dao.ts
+++ deno/gen/base/usr/usr_sync.dao.ts
@@ -99,6 +99,14 @@ import {
   syncUsrLblByUsrIdDynPageData,
 } from "/gen/base/dyn_page_data/dyn_page_data.dao.ts";
 
+import {
+  syncUsrLblByUsrIdBaiduApp,
+} from "/gen/baidu/baidu_app/baidu_app.dao.ts";
+
+import {
+  syncUsrLblByUsrIdBaiduAppToken,
+} from "/gen/baidu/baidu_app_token/baidu_app_token.dao.ts";
+
 /** 根据 usr_id 同步所有表中的创建人/更新人/删除人标签 */
 export async function syncUsrLblByUsrId(
   usr_id: UsrId,
@@ -244,6 +252,16 @@ export async function syncUsrLblByUsrId(
   );
   
   affectedRows += await syncUsrLblByUsrIdDynPageData(
+    usr_id,
+    syncOptions,
+  );
+  
+  affectedRows += await syncUsrLblByUsrIdBaiduApp(
+    usr_id,
+    syncOptions,
+  );
+  
+  affectedRows += await syncUsrLblByUsrIdBaiduAppToken(
     usr_id,
     syncOptions,
   );
